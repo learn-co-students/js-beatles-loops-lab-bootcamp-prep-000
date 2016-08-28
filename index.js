2b9de1441704b6ad292c37f66ed105e88c8d04a1
Aaron Miller <amiller719@gmail.com>
2016-08-28 21:23:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function theBeatlesPlay(musicians, instruments) {
+    var beatles = [];
+    for (var i = 0; i < musicians.length; i++) {
+        beatles.push(`${musicians[i]} plays ${instruments[i]}`);
+    }
+
+    return beatles;
+}
+
+function johnLennonFacts(array) {
+    var i = 0;
+    while (i < array.length) {
+        array[i] = array[i] + '!!!';
+        i++;
+    }
+
+    return array;
+}
+
+function iLoveTheBeatles(n) {
+    var beatleFan = [];
+
+    do {
+        beatleFan.push('I love the Beatles!');
+        n++;
+    } while (n < 15);
+
+    return beatleFan;
+}
