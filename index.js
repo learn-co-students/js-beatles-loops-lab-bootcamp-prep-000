2a65e93d548147e79031cb827c455439d7adecc8
Alejandro Coca <amcocarojas@gmail.com>
2016-09-09 16:22:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 function theBeatlesPlay(musicians, instruments) {
-  var array = Array.new();
+  var array = new Array();
 
   for(let i=0; i<musicians.length; i++){
-    
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
   }
+
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  var c = 0;
+  while (c<facts.length) {
+    facts[c] = facts[c] + "!!!";
+    c++;
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(number) {
+  var array = new Array();
+
+  do {
+    array.push("I love the Beatles!");
+    number++;
+  } while (number < 15);
+  
+  return array;
 }
