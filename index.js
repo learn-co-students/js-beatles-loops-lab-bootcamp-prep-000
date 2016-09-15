34b10c2a2d9afc441f6800180eeef3cb5894e3b0
Neggme Elreda <neggme@gmail.com>
2016-09-15 16:03:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+function theBeatlesPlay (musicians, instruments, array){
+var array = []
+  for (let i = 0; i <= 6; i++) {
+
+    array.push (`musicians[i] + 'plays' + instruments[i]`)
+  }
+  return array
+}
