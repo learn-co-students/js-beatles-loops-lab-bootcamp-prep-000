8d1b300d6ed0b1cf93c3f88fe930474124d19aa7
Kendall Holland <holland.kd@gmail.com>
2016-09-15 02:21:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+var newArr = [];
+
+function theBeatlesPlay(musicians, instruments) {
+  for (var n = 0; n <= musicians.length-1; n++) {
+   newArr.push(musicians[n] + " plays " + instruments[n]);
+}
+	return newArr;
+}
+function johnLennonFacts(x) {
+var i=0;
+var factsArr = [];
+  while (i < x.length) {
+    factsArr.push(x[i] + "!!!");
+    i++;
+  }
+  return factsArr;
+}
+
+function iLoveTheBeatles(i) {
+  var Arr = [];
+  do {
+    push.Arr("I love the Beatles!")
+    i++
+  }
+  while (i <15);
+}
+
+function iLoveTheBeatles(i) {
+  var Arr = [];
+  do {
+    Arr.push("I love the Beatles!");
+    i++;
+  }
+  while (i<15);
+  return Arr;
+}
