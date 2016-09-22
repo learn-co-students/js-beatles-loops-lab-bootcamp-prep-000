1074ef949df98c8f5c5a18465b92f4cf4c1505e2
Frank Faustino <nawzyah@gmail.com>
2016-09-22 01:50:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function theBeatlesPlay(musicians, instruments) {
+  const arr = []
+
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return arr
+}
+
+function johnLennonFacts(facts) {
+  // const shoutedFacts = []
+
+  let i = 0
+  while (i < facts.length) {
+    facts.push(`${facts[i]}!!!`)
+    i++
+  }
+
+  return facts
+}
