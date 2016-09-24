c0c0ebdd3829c87563e3ca27b8b44eb9baa64631
Neil Rajendran <rajendranneil19@gmail.com>
2016-09-24 20:33:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,7 @@
+function theBeatlesPlay(musicians,instruments){
+  var emptyarray = [],
+  for (let i = 0; i < musicians.length; i++){
+    emptyarray.push(`${musicians[i]}`)
+  }
+  return emptyarray
+}
