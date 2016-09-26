239348fb7e0ef12244af845eb4fbc6ecd4c792ed
Neil Rajendran <rajendranneil19@gmail.com>
2016-09-26 01:24:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 function theBeatlesPlay(musicians,instruments){
-  var emptyarray = [],
+  var thebeatlesplayball = [];
   for (let i = 0; i < musicians.length; i++){
-    emptyarray.push(`${musicians[i]}`)
-  }
-  return emptyarray
+  thebeatlesplayball.push(`${musicians[i]} plays ${instruments[i]}`)
+    }
+  return thebeatlesplayball
+}
+
+
+function johnLennonFacts(facts){
+var i = 0
+  while (i < facts.length){
+facts[i] = facts[i] + "!!!"
+i++}
+return facts
+}
+
+function iLoveTheBeatles(n){
+  var beatlejuice = [];
+  do {
+  beatlejuice.push("I love the Beatles!")
+ n++}
+ while (n < 15)
+  return beatlejuice
 }
