c636eaf5a03f5b5b3521ffab759bf542a311f9e0
Senad Hadrovic <senadhadrovic@gmail.com>
2016-09-29 13:04:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function theBeatlesPlay(musicians, instruments) {
+  var combo_arr = [];
+  for (i = 0; i < musicians.length; i++)
+{
+  combo_arr.push(`${musicians[i]} plays ${instruments[i]}`)
+}
+    return combo_arr;
+}
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  while (i < facts.length)
+  {
+  facts[i] = `${facts[i]}!!!`;
+    i++;
+  }
+  return facts
+}
+
+function iLoveTheBeatles(n) {
+  var new_arr = [];
+
+  do {
+    new_arr.push("I love the Beatles!");
+    i++;
+  }
+while (n < 15);
+    return array;
+}
