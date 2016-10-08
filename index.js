14c8019993bba052c1d0f702acdd7ffec61cc51d
Shineka Barnett <shineka1barnett@gmail.com>
2016-10-08 00:39:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function theBeatlesPlay(musicians, instruments){
+    const emptyArray = []; //create an empty arr
+    for(var m = 0; m.length; m++){
+      arr.push(`${musicians[i]} plays ${instruments[i]}`)
+    }
+    return arr
+}
+
+function johnLennonFacts(facts) {
+  const shoutedFacts = []
+
+  let i = 0
+  while (i < facts.length) {
+    shoutedFacts.push(`${facts[i]}!!!`)
+    i++
+  }
+
+  return shoutedFacts
+}
+
+function iLoveTheBeatles(n) {
+  const arr = []
+
+  do {
+    arr.push('I love the Beatles!')
+    n++
+  } while (n < 15)
+
+  return arr
+}
