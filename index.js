8013dd7c94f6e1761e884d4ba9507a89d36c85fd
Leonna Hill <leonnahill@gmail.com>
2016-10-12 20:57:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function theBeatlesPlay(musiciansArray, instrummentArray) {
+  var newArray = [];
+  for(var i = musiciansArray[0]; i <= musiciansArray.length; i++) {
+    //do something
+    newArray.push(`${musiciansArray[i]} plays ${instrummentArray[i]}`)
+  }
+  return newArray
+}
+
+
+
+
+function johnLennonFacts(array) {
+  while(condition) {
+      //do something
+  }
+}
+
+
+
+
+function iLoveTheBeatles (number) {
+  var myArray = [];
+  do {
+    //do something
+  } while (condition)
+  return myArray
+}
