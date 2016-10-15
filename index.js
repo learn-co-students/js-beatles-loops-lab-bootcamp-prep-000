5d6cb727ba6d2c6017d7047678c04513a7f801c6
colombianito66 <colombianito66@gmail.com>
2016-10-15 21:55:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,7 @@
+function theBeatlesPlay(musicians, instruments){
+  var array = [];
+  for(let i = 0; i < musicians.length; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+return array;
+}
