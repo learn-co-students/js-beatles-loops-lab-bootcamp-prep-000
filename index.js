9cfd957a92a6dd956e34fa0cf1f7fb8c3d0997cf
Zhanxi Ni <ni.zhanxi@gmail.com>
2016-10-20 18:35:50 +0000

    Done.

--- index.js
+++ index.js
@@ -20,12 +20,9 @@ function johnLennonFacts(array) {
 
 function iLoveTheBeatles (number) {
   var array = [];
-  var i = 0;
   do {
     array.push("I love the Beatles!");
-  } while (i<number && i<15) {
-    array.push("I love the Beatles!");
-    i++;
-  }
+    number++;
+  } while (number<15);
   return array;
 }
