443fb6677e08a1e2c65bf2d8b497e1a9db228668
Vanessa Jimenez <vjimenez127@gmail.com>
2016-10-26 05:29:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+function theBeatlesPlay(musicians, instruments){
+var musicians=[];
+var instruments=[];
+var beatles=[];
+
+musicians.push(musicians);
+instruments.push(instruments);
+
+for(var i=0; i< musicians.length; i++){
+  beatles.push( ${musicians[i]} + " plays " + ${instruments[i]})
+}
+
+return beatles
+}
