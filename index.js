3d548d2cea802e78289d217c391980252daf2345
Laziza Kodirova <kodirova.laziza@gmail.com>
2016-10-27 18:14:18 +0000

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,26 @@ array.push(musicians[i] +  " plays "  + instruments[i])
 }
 return array;
 };
+
+
+function johnLennonFacts(facts){
+var arr=[];
+var i=0
+while (i<facts.length) {
+  arr.push(facts[i] + "!!!")
+  i++;
+}
+  return arr;
+};
+
+
+
+function iLoveTheBeatles(number){
+  var empty_array=[];
+  do {
+    empty_array.push('I love the Beatles!');
+    number++
+}
+while (number<15) ;
+return empty_array;
+};
