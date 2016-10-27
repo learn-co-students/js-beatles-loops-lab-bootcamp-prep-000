7b78b5b1ee5bc04871b158d9345f9bc925cb45a9
Bharath Kumar <bharathkumar.a20@gmail.com>
2016-10-27 23:06:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function theBeatlesPlay(musicians, instruments){
+  var result = []
+
+  for(var i = 0; i < musicians.length; i++){
+    result.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return result
+}
+
+function johnLennonFacts(facts){
+  var i = 0;
+  while(facts[i]){
+    facts[i] = `${facts[i]}!!!`
+    i++
+  }
+  return facts
+}
+
+function iLoveTheBeatles(n){
+  var result = []
+  do{
+    result.push("I love the Beatles!")
+    n++
+  }while(n < 15)
+  return result
+}
