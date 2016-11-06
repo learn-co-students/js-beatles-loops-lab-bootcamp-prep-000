5307a8403a795d08740c77d034efc3cb5ade4a8d
erika1066 <hastings.erika@gmail.com>
2016-11-06 17:24:48 +0000

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(){
   var array = [
     'John Lennon plays Guitar',
     'Paul McCartney plays Bass Guitar',
-    'Georget Harrison plays Lead Guitar',
+    'George Harrison plays Lead Guitar',
     'Ringo Starr plays Drums'
   ];
   return array;
