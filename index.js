004be3b2b4a646bb171597e9062c8fe4fd6ebca9
erika1066 <hastings.erika@gmail.com>
2016-11-06 18:04:36 +0000

    Done.

--- index.js
+++ index.js
@@ -8,12 +8,22 @@ function theBeatlesPlay(){
   return Beatles;
 }
 
-function johnLennonFacts(){
-  var facts = [
-    'He was the last Beatle to learn to drive',
-    'He was never a vegetarian',
-    'He was a choir boy and a boy scout',
-    'He hated the sound of his own voice'
-  ];
+var facts = [
+  'He was the last Beatle to learn to drive',
+  'He was never a vegetarian',
+  'He was a choir boy and a boy scout',
+  'He hated the sound of his own voice'
+];
+
+function johnLennonFacts(facts){
+//  var facts = [
+//    'He was the last Beatle to learn to drive',
+//    'He was never a vegetarian',
+//    'He was a choir boy and a boy scout',
+//    'He hated the sound of his own voice'
+//  ];
+  for (let facts = i){
+    johnLennonFacts.push( i + '!!!')
+  }
   return facts;
 }
