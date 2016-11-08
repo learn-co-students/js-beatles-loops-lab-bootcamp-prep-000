52220f862141b83c49bf9aa253db193a6f91c3e5
jason brown <j.grant.brown@gmail.com>
2016-11-08 14:12:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+function theBeatlesPlay(musicians,instruments){
+  var musicians= new Array();
+  var instruments=new Array();
+  for(var i=0,i<=musicians.length,i++){
+    musicians[0]="John Lennon plays guitar";
+  }
+}
+
+
+johnLenninFacts
+
+iLoveTheBeatles
