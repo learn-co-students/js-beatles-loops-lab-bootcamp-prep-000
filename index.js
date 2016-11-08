7925e066de56a8b377419b5f6c173c2076eb9c3b
jason brown <j.grant.brown@gmail.com>
2016-11-08 14:41:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function theBeatlesPlay(musicians,instruments){
 var theBand = new Array ();
   for(var i=0,i<=musicians.length,i++){
-  return  musicians[0]="John Lennon ";
-  return instruments[0]="plays guitar";
-  
+  musicians[0]="John Lennon ";
+  instruments[0]="plays guitar";
+  return theBand("musicians[i] "+ "instruments[i]");
   }
 }
 
