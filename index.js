b5681d9e8d3fc273bd8eae31e2de39bb9425cc88
jason brown <j.grant.brown@gmail.com>
2016-11-09 16:17:23 +0000

    Done.

--- index.js
+++ index.js
@@ -23,9 +23,12 @@ return newFacts;
 
 function iLoveTheBeatles(num){
   var lovethem= new Array();
+ for(i=0;num<i;i++){
   do{
+
   lovethem.push("I love the Beatles!");
 }
-while(0<num--<15)
+while(num<15)
     return lovethem;
   }
+}
