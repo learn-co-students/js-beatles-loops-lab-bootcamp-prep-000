fc1f0591bcd69cc1518de08cdfeedddd93a76514
Brittany Hill <brit.hill10@gmail.com>
2016-11-14 16:10:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 function theBeatlesPlay(arrMusicians, arrInstruments) {
-  const arr=[];
-  for (let i=1, i < arrMusicians.length;i++) {
-    arr.push("${arrMusicians[i]} plays ${arrInstruments[i]}");
+  const arr = []
+  for (let i = 0, l = arrMusicians.length; i < l; i++) {
+    arr.push(`${arrMusicians[i]} plays ${arrInstruments[i]}`)
   }
-  return arr;
+  return arr
 }
 
 function johnLennonFacts(array) {
-  const shout=[]
-  let i=0
-  while (i<shout.length) {
-    shout.push("${shout[i]}!!!");
-  } return shout;
+  const newArr = []
+  let i = 0
+  while(i < array.length) {
+    newArr.push(`${array[i]}!!!`)
+    i++
+  }
+  return newArr
 }
 
 function iLoveTheBeatles(n) {
-  const arr=[];
+  const arr = []
   do {
-    arr.push("I love the Beatles");
-    n++;
-  } while (n<15)
-  return arr;
+    arr.push("I love the Beatles!")
+    n++
+  } while(n<15)
+  return arr
 }
