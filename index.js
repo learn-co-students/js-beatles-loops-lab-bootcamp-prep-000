d856cead174235fa1813869079c3e7790705323f
Lauren Albritton <dancerlma@yahoo.com>
2016-11-16 19:17:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function theBeatlesPlay(musicians, instruments) {
+  const arr = []
+
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return arr
+}
+
+
+function johnLennonFacts(facts) {
+  const loudFacts = []
+
+  let i =0
+  while (i < facts.length) {
+    loudFacts.push(`${facts[i]}!!!`)
+    i++
+  }
+
+  return loudFacts
+}
+
+
+function iLoveTheBeatles(n) {
+  const arr = []
+
+  do {
+    arr.push('I love the Beatles!')
+    n++
+  }
+  while (n < 15)
+
+  return arr
+}
