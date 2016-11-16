78e2ace739339638c50f3271b2e868bbc7ae3b18
Elizabeth Hall <elizabethhall205@gmail.com>
2016-11-16 04:30:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+//create a function theBeatlesPlay accepts two parameters-an array
+//musicians and instruments. the body stores an empty array stored in a variable
+function theBeatlesPlay(musicians, instruments) {
+  var emptyArray = []
+  for(musicians[0];)
+    emptyArray.push( musicians ; instruments)
+  }
+    return \\the array of new strings
+}
