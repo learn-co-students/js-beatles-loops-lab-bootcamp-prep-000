3662a87a3d132eb92b4657eec8b1ea483415ff39
J Se <seraficaj@gmail.com>
2016-12-27 17:16:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function theBeatlesPlay(musicians, instruments){
+  var plays = [];
+  for (var i=0; i < musicians.length; i++) {
+    plays.push(musicians[i] + " plays " + instruments[i]);
+  } return plays;
+}
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  var newFacts = [];
+  while (i < facts.length) {
+    newFacts.push(facts[i] + "!!!");
+    i++;
+  } return newFacts;
+}
+
+function iLoveTheBeatles(n) {
+  var empty = [];
+  do {
+    empty.push("I love the Beatles!");
+    n++;
+  }
+  while ( n < 15) {
+    return empty;
+  };
+}
