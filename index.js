f1051d14fbbeb34ee01b14c624921d98182f5698
Amanda Tjan <amanda.tjan@gmail.com>
2016-12-29 07:15:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+var musicians = ['John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Starr'];
+
+var instruments = ['Guitar', 'Bass Guitar', 'Lead Guitar', 'Drums'];
+
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for ( var i = 0; i < musicians.length; i ++) {
+    array.push(musicians[i] + ' plays ' + instruments[i])
+  }
+  return array;
+}
+
+
+const facts = [
+"He was the last Beatle to learn to drive",
+"He was never a vegetarian",
+"He was a choir boy and boy scout",
+"He hated the sound of his own voice"
+];
+
+function johnLennonFacts (facts) {
+  var newFacts = [];
+  var i = 0;
+  while (i < facts.length) {
+    newFacts.push(facts[i] + "!!!");
+    i++;
+  }
+
+  return newFacts;
+}
+
+function iLoveTheBeatles(n) {
+  var beatlesArray = [];
+
+  do {beatlesArray.push('I love the Beatles!');
+  n++;
+
+} while(n < 15);
+  return beatlesArray;
+}
