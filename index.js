de27527b404e3cb4f103f86848a780bb2b723d2d
Grant MacManus <flatiron@macdav.net>
2016-12-31 20:11:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  for ( var i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  let i = 0
+  while (i < facts.length) {
+    facts[i] = facts[i] + "!!!"
+    i++
+  }
+  return facts
+}
+
+function iLoveTheBeatles(n) {
+  var array = []
+  do {
+    array.push("I love the Beatles!")
+    n++
+  } while (n < 15)
+  return array
+}
