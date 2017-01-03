c2140286f4f52d5c0016b6a8c8f5ee906a79e5ad
Elizabeth Hall <elizabethhall205@gmail.com>
2017-01-03 02:51:04 +0000

    Done.

--- index.js
+++ index.js
@@ -5,30 +5,25 @@ function theBeatlesPlay(musicians,instruments) {
   }
     return emptyArray
 }
+/*ran learn test using var str = "!!!" and gave error*/
+function johnLennonFacts (arr)  {
+  var str = "!!!"; //changed from var str = "!!!"
+  let i = 0;
 
-function johnLennonFacts(facts) {
-  const exclamedFacts = []
-
-  let i = 0
-  while(i < facts.length) {
-
-    exclamedFacts.push(`${facts[i]}!!!`)
-    i ++
-  }
-  return exclamedFacts
+  while (i < arr.length) {
+    str.push(array[i], str );
+    i++
+    }
+    return str
 }
 
- function iLoveTheBeatles(n) {
-   var array = []
-   //starts counting at 0
-   do {
-  // it("returns an array of 'I love the Beatles!' once when passed the parameter 17"/this means nothing
-  // expect(iLoveTheBeatles(17)).toEqual(["I love the Beatles!"]);
-  //8 + 7 = 15
-     array.push("I love the Beatles!")
-     n++
-     ///iterates
-   } while ( n < 15)
-// n< 15 outputs 14 times; and 17 is 15+2
-    return array
- }
+function iLoveTheBeatles ( n ) {
+  var arr =[]
+  var str = "I love the Beatles!"
+do {
+  n++
+  arr.push(str)
+}
+    while ( n < 15 )
+    return arr
+}
