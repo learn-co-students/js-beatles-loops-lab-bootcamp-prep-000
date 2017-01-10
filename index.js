864bb34870e0fb85eb74fc2612b3fc0560d463da
Daniel Humphries <danrhumphries@gmail.com>
2017-01-10 01:29:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function theBeatlesPlay(musicians, instruments) {
+    var newArray = []
+    for (var i = 0; i < musicians.length; i++ ) {
+        newArray.push(`${musicians[i]} plays ${instruments[i]}`)
+    }
+    return newArray
+}
+
+function johnLennonFacts(array) {
+    var newArray = []
+    for (var i=0; i < array.length; i++) {
+        newArray.push(`${array[i]}!!!`)
+    }
+    return newArray
+}
+
+function iLoveTheBeatles(n) {
+    var newArray = []
+    do {
+        newArray.push("I love the Beatles!")
+        n++
+    }
+    while (n < 15)
+    return newArray
+}
