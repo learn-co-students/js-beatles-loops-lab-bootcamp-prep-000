6d93624996884cb7543fb4fc1cdfc41233d0dcb7
Isaac Peifer <iwpeifer@gmail.com>
2017-01-12 02:28:55 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function theBeatlesPlay(musicians, instruments){
+  var array = [];
+  for (var i = 0; i < 4; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+}
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+]
+
+function johnLennonFacts(facts){
+  var array = [];
+  var i = 0;
+  while (i < facts.length){
+    array.push(`${facts[i]}!!!`);
+    i++
+  }
+  return array
+}
+
+function iLoveTheBeatles(n){
+  var array = [];
+  var i = 0;
+  do {
+    array.push("I love the Beatles!");
+    n++;
+  }
+  while (n < 15);
+  return array
+}
