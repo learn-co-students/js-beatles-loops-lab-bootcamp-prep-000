89fbe64e41b4b47b08eed8dbd6e24d7f5d7e4f26
Juha  Bach <cdalsoniii@gmail.com>
2017-01-16 15:36:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function theBeatlesPlay(musicians,instruments) {
+   var phrases = []
+   for (let i = 0; i < musicians.length; i++) {
+     phrases.push(`${musicians[i]} plays ${instruments[i]}`)
+   }
+   return phrases
+ }
+
+ function johnLennonFacts(facts) {
+   var factsplus = []
+   while (facts.length > 0) {
+     factsplus.push(facts[0]+'!!!')
+     facts.shift()
+   }
+   return factsplus
+ }
+
+ function iLoveTheBeatles(times) {
+   var beatlemania = []
+   do {
+     beatlemania.push("I love the Beatles!")
+     times++
+   } while (times < 15)
+   return beatlemania
+ } 
