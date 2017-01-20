3dcb5af9bf050bb3de50e1a20021c92396a05d57
Randi Sterenberg <randi.sterenberg@gmail.com>
2017-01-20 17:45:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  for (var i = 0; i < musicians.length; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  var x = 0
+  var array = []
+  while (facts[x]) {
+    array[x] = facts[x] + "!!!";
+    x++
+  }
+  return array
+}
+
+function iLoveTheBeatles(number) {
+  var array = []
+  do {
+    array.push("I love the Beatles!")
+  } while (number < 15);
+  return array
+}
