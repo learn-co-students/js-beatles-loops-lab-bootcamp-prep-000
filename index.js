bcf1b8c08a2dced06004a946d4e99a9624aae3a4
Charisa Lackrone <charisaanne@gmail.com>
2017-01-25 03:25:55 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function theBeatlesPlay(musician, instrument) {
+  var array = []
+
+  for (var i = 0; i < musician.length; i++) {
+    array.push(musician[i] + ' plays ' + instrument[i])
+  }
+
+  return array
+}
+
+function johnLennonFacts(facts) {
+  var array = []
+  var i = 0
+  while (i < facts.length) {
+    array.push(facts[i] +'!!!')
+    i++
+
+  }
+  return array
+}
+
+function iLoveTheBeatles(n) {
+  var array = []
+  do {
+    array.push('I love the Beatles!');
+    n++
+  } while (n < 15);
+  return array
+}
