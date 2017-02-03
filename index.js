7c1bee306629db44aebde7c1df66b15eb64a072e
Phillip Fine <phillipfine@yahoo.com>
2017-02-03 00:18:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+function theBeatlesPlay (a1,a2) {var newArrB=[]; for (let i=0; i<a1.length; i++) {newArrB[i]= `${a1[i]} plays ${a2[i]}`} return newArrB}
+function johnLennonFacts(a1) {let i=0; while (i<a1.length) {a1[i]=a1[i] + "!!!"; i++} return a1}
+function iLoveTheBeatles(n) {var eA=[]; if (n>=15) {eA[0]="I love the Beatles!"; return eA} else {let i=0; do {eA[i]="I love the Beatles!"; i++} while (i<=n) return eA}}
