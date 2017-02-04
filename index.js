bc101468fe255e3afe54a388fe04814724d36d26
Drew Hart <drew@drewhart.com>
2017-02-04 17:43:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function theBeatlesPlay( musicians, instruments ) {
+  var array = []
+
+    for (var i = 0; i < musicians.length; i++){
+      array.push(musicians[i] + " plays " + instruments[i])
+    }
+
+    return array
+}
+
+function johnLennonFacts( facts ) {
+  var i = 0;
+  while (i < facts.length) {
+    facts[i] = facts[i] + "!!!"
+    i++
+  }
+  return facts
+}
+
+/*
+The condition of the loop should check to see that the parameter number is less than 15. The function should return the array with the strings "I love the Beatles!".
+*/
+
+function iLoveTheBeatles( number ){
+  var array = []
+  
+  do {
+      array.push("I love the Beatles!")
+      number++
+  } while ( number < 15 );
+
+  return array
+}
