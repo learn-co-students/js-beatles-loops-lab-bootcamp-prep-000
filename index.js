3f29fd1c7f4e98b84fb4515e7b6810da756b0252
Lev Craig <levbaus@gmail.com>
2017-02-12 02:58:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < musicians.length; i++) {
+    array.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  for (var i = 0; i < facts.length; i++) {
+    facts[i] = facts[i] + "!!!";
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(n) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!");
+    n++;
+  } while (n < 15);
+  return array;
+}
