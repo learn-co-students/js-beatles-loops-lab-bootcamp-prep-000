59fbf5907f200893fff7078ffd2367f63a086acb
Heather Mason <heatem@gmail.com>
2017-02-13 01:59:03 +0000

    Done.

--- index.js
+++ index.js
@@ -26,9 +26,3 @@ function iLoveTheBeatles(num) {
   while (num < 15);
   return array;
 }
-/* Create a function iLoveTheBeatles which accepts a number as a parameter.
-The body of the function should create a variable that stores an empty array.
-Then, implement a do-while loop inside the function that adds "I love the Beatles!" to the empty array.
-Then the loop should increment the number passed in as a parameter.
-The condition of the loop should check to see that the parameter number is less than 15.
-The function should return the array with the strings "I love the Beatles!". */
