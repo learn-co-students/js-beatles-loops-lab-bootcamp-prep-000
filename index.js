f8054d047413e3c42f30a478fb62d7007bd944e8
Jimmy Delgrosso <jimmydg16@gmail.com>
2017-02-16 15:25:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function theBeatlesPlay(musicians, instruments) {
+  const array = []
+
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+}
+
+
+function johnLennonFacts(facts) {
+  const exclaimedFacts = [];
+
+  let i = 0
+  while (i < facts.length) {
+    exclaimedFacts.push(`${facts[i]}!!!`)
+    i++
+  }
+  return exclaimedFacts
+}
+
+function iLoveTheBeatles(n) {
+  const array = []
+
+  do {
+    array.push("I love the Beatles!")
+    n++
+  }
+  while (n < 15)
+
+  return array
+}
