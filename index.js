1005ddfc0b7f96837fc9f7a5734193a703eb41f2
subash karki <skarki35205@hotmail.com>
2017-02-20 22:16:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-function theBeatlesPlay(musicians, instruments){
+function theBeatlesPlay(musicians, instruments) {
     var beattlesList = [];
     for (; i < musicians.length; i++) {
         musicians[i]
     }
+}
+
+function johnLennonFacts(x) {
+    var i = 0;
+    var gothruArray = x.length;
+    var emptyArray = [];
+    while ([i] < gothruArray) {
+        emptyArray.push(x[i++] + '!!!');
+    }
+    return emptyArray;
 }
