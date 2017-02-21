7b4d61cb082cb008db7a060fd0e3d523f9d315fd
subash karki <skarki35205@hotmail.com>
2017-02-21 20:18:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function theBeatlesPlay(musicians, instruments) {
-    var beattlesList = [];
-    for (; i < musicians.length; i++) {
-        musicians[i]
+    var array = [];
+    for (let i = 0; i < 4; i++) {
+        array.push(musicians[i] + " plays " + instruments[i]);
     }
+    return array;
 }
 
 function johnLennonFacts(x) {
@@ -13,4 +14,15 @@ function johnLennonFacts(x) {
         emptyArray.push(x[i++] + '!!!');
     }
     return emptyArray;
+}
+
+function iLoveTheBeatles(n) {
+    const arr = []
+
+    do {
+        arr.push('I love the Beatles!')
+        n++
+    } while (n < 15)
+
+    return arr
 }
