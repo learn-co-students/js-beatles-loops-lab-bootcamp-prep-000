7c57f4ba701d973f30fe430f76028f3ca104fba9
Leah Tabush <leahtabush@gmail.com>
2017-02-26 00:51:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,40 +1,15 @@
 function theBeatlesPlay(musicians, instruments) {
-  const myArray = [];
-  for(var i = 0; i < musicians.length; i++) {
-    myArray.push(musicians[i] + " plays " + instruments[i]);
+  var array = [];
+  for (let i = 0; i < musicians.length; i++) {
+    array.push(musicians[i] + " plays " + instruments[i]);
   }
-  return myArray;
+  return array;
 }
-
-var musicians = ["John Lennon", "Ringo Starr", "George Harrison", "Paul McCartney"];
-var instruments = ["guitar", "drums", "leadGuitar", "bass"];
-theBeatlesPlay(musicians, instruments);
-
-function johnLennonFacts(aboutJohn) {
-  var newJohn = [];
+function johnLennonFacts(facts) {
   var i = 0;
-  while(i < aboutJohn.length) {
-    newJohn.push(aboutJohn[i] + "!!!")
+  while (i < facts.length;) {
+    facts[i] = facts[i] + "!!!";
     i++;
   }
-  return newJohn;
+  return facts;
 }
-
-const aboutJohn = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
-johnLennonFacts(aboutJohn);
-
-function iLoveTheBeatles(num) {
-  var beatlesLove = [];
-  do  {
-    beatlesLove.push("I love the Beatles!");
-    num++;
-  } while (num < 15);
-  return beatlesLove;
-}
-iLoveTheBeatles(num);
-var num = 0;
