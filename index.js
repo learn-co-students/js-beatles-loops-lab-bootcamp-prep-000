16e55f69b3144f15042a6f1cb6bd864b3faec82a
Justin Ramirez <justinjlramirez@gmail.com>
2017-03-12 17:55:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function theBeatlesPlay(musicians, instruments) {
+  var playedInstrument=[];
+  for (var i=0;i<musicians.length;i++) {
+    playedInstrument.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return playedInstrument;
+}
+
+function johnLennonFacts(facts) {
+  var i=0;
+  while (i<facts.length) {
+    facts[i]=`${facts[i]}!!!`
+    i++;
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(num) {
+  var tempArray=[];
+  do {
+    tempArray.push("I love the Beatles!");
+    num++;
+  } while (num<15);
+  return tempArray;
+}
