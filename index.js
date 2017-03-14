c7586702e2600a1663b76d553fbbe78e2c69d611
pia smith <pips@piasmith.com>
2017-03-14 21:57:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+var theBeatlesPlay = function(arr1, arr2){
+  var newArr = [];
+  for (var i = 0 ; i < arr1.length ; i++) {
+      newArr = newArr.concat(arr1[i] + " plays " + arr2[i]);
+  }
+  return newArr;
+}
+
+var johnLennonFacts = function(array){
+   var newArr = [];
+  //do ()
+}
+
+var iLoveTheBeatles = function(number) {
+    newArr = [];
+}
