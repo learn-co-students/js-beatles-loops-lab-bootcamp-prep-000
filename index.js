96adb1dbcad8f089e36cb13625b834cea2856d90
Anish Kumar Shaw <anishshaw100@gmail.com>
2017-03-14 13:24:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function theBeatlesPlay(musicians,instruments){
+  var empty = []
+  for (var i = 0; i < musicians.length; i++) {
+    empty[i]=musicians[i]+' plays '+instruments[i]
+  }
+  return empty
+}
+
+function johnLennonFacts(facts){
+//  len = facts.length
+  var i = 0
+  var empty = []
+  while (i < facts.length) {
+    empty[i] = facts[i] + '!!!'
+    i++
+  }
+  return empty
+}
+
+function iLoveTheBeatles(number){
+  var empty = []
+  do {
+    empty.push('I love the Beatles!')
+    number++
+  } while (number < 15);
+  return empty
+}
