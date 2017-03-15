607578574fb15ce3848b383076eaaa2c2ecb50a0
Trey Boyer <wwboyer14@mail.strakejesuit.org>
2017-03-15 03:12:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  for(let i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+}
+
+function johnLennonFacts(array) {
+  var counter = 0
+  while(counter < array.length) {
+    array[counter] = array[counter] + '!!!'
+    counter++
+  }
+  return array
+}
+
+function iLoveTheBeatles(n) {
+  var array = []
+  do{
+    array.push("I love the Beatles!")
+    n++
+  } while(n < 15)
+  return array
+}
