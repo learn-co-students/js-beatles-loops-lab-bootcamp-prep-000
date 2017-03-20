99f6bfc75815969c2d1f0a68bc096a4bcf67c655
Shalin Scupham <shalin.scupham@gmail.com>
2017-03-20 08:08:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,6 @@
+function theBeatlesPlay(musicians, instruments) {
+  Beatles = []
+  (let i = 0, l < musicians.length; i < l ; i++)
+  Beatles.push (`${musicians[i]} plays ${instrumnets[i]}`)
+  return Beatles
+  }
