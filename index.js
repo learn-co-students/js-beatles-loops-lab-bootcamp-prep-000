8b3a025a48543b1023921e40a549c2c7c513bbce
Brian Bytnar <bytnarb2@gmail.com>
2017-03-26 22:39:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+function theBeatlesPlay(musicians, instruments) {
+  const array = []
+
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    array.push('${musicians[i]} plays ${instruments[i]}')
+  }
+  return array
+}
