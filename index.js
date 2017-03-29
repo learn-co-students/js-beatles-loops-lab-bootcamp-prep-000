6f12942582fcffd64c607e0803cb7832457f8c2c
Chris Wood <woodcr5@gmail.com>
2017-03-29 15:26:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 function theBeatlesPlay(musicians, instruments) {
-  const arr = [];
+  const arr = []
 
   for (let i = 0, l = musicians.length; i < l; i++) {
-    arr.push(`${musicians[i]} plays ${instruments[i]}`);
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   return arr;
 }
 
 function johnLennonFacts(factsArray) {
-  const arr = [];
-  let i = 0;
+  const arr = []
+  let i = 0
 
   while (i < factsArray.length) {
-    arr.push(`${factsArray[i]}!!!`);
+    arr.push(`${factsArray[i]}!!!`)
     i++;
   }
   return arr;
 }
 
 function iLoveTheBeatles(n) {
-  var arr = [];
+  var arr = []
 
   do {
-    arr.push("I love the Beatles!");
-    n++;
+    arr.push("I love the Beatles!")
+    n++
   } while (n < 15);
   return arr;
 }
