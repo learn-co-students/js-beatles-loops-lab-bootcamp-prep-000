3728fc624050d75f5596b3d93a2adef752e60a16
Chris Wood <woodcr5@gmail.com>
2017-03-29 15:24:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function theBeatlesPlay(musicians, instruments) {
+  const arr = [];
+
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return arr;
+}
+
+function johnLennonFacts(factsArray) {
+  const arr = [];
+  let i = 0;
+
+  while (i < factsArray.length) {
+    arr.push(`${factsArray[i]}!!!`);
+    i++;
+  }
+  return arr;
+}
+
+function iLoveTheBeatles(n) {
+  var arr = [];
+
+  do {
+    arr.push("I love the Beatles!");
+    n++;
+  } while (n < 15);
+  return arr;
+}
