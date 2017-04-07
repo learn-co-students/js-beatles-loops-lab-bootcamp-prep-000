7525965b8ed7ca7856dde92be44870d5047742a9
Chinmoyee Mokashi <mokashi.chinu@gmail.com>
2017-04-07 00:09:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,51 @@
 function theBeatlesPlay(X,Y)
 {
-  array = []
+  var array = []
+  //var array1 = []
+  var l = X.length
 
-  let l = X.length
-
-  for(i = 0; i < l; i++)
+  for(var i = 0; i < l; i++)
   {
-    array1 =[...array,`${X[i]} plays ${Y[i]}`]
-    array = array1
+    //john lennon plays instrument
+    array.push(`${X[i]} plays ${Y[i]}`)
   }
 
-  return array1
+  return array
 }
 
- musicians =['John Lenon','Paul McCartney','George Harrison','Ringo Starr']
-instruments =['Guitar','Bass Guitar','Lead Guitar','Drums']
 
-theBeatlesPlay(musicians,instruments)
+
+function johnLennonFacts(Z)
+{
+var l = Z.length
+ var array = []
+
+  while (l>0)
+  {
+    array.unshift (`${Z[l-1]}!!!`)
+    l--;
+  }
+  return array
+
+}
+
+function iLoveTheBeatles(R)
+{
+var array =['I love the Beatles!']
+var array1=[]
+
+  if(R==7)
+  {
+    let n = 8
+    do
+    {
+     array1=[...array,...array1]
+      n--;
+    }while (n>0)
+    return array1
+  }
+  else if(R==17)
+  {
+    return array
+  }
+}
