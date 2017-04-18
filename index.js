e0194a0dc93e543b318a447d748648bc309b8e9c
Anthony Verdesca <verdesca2@gmail.com>
2017-04-18 02:18:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 function theBeatlesPlay(musicos,instruments) {
 var Lista = new Array()
   for (var i = 0; i < musicos.length; i++) {
-    Lista[i]=(musicos[i] + " plays " + instruments[i])    
+    Lista[i]=(musicos[i] + " plays " + instruments[i])
   }
   return Lista
+}
+
+function johnLennonFacts(facts){
+var a=0
+while (a<facts.length) {
+  facts[a]=facts[a]+"!!!"
+a=a+1
+}
+return facts
+}
+
+function iLoveTheBeatles(number){
+var a=0
+var Lista = new Array()
+if (number>14) {
+return "I love the Beatles!"
+}
+else {
+do {
+Lista[a]="I love the Beatles!"
+a=a+1
+} while (a<=number);
+return Lista
+}
 }
