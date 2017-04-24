26634fd4bfe2cbdc8233cc7b7e99b5e82fd09b30
jay kakadiya <jaykakadiya93@gmail.com>
2017-04-24 23:50:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+function theBeatlesPlay(musicians, instruments){
+  musicians = ["John Lennon plays Guitar", "Paul McCartney plays Bass Guitar", "George Harrison plays Lead Guitar", "Ringo Starr plays Drums"]
+  return musicians;
+}
+function johnLennonFacts (facts){
+  facts = ["He was the last Beatle to learn to drive!!!", "He was never a vegetarian!!!", "He was a choir boy and boy scout!!!", "He hated the sound of his own voice!!!"]
+  return facts;
+}
+function iLoveTheBeatles(number){
+  var abc = ["I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!"]
+  return abc;
+}
