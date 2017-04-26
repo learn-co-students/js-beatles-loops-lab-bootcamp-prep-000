016e16131c39c8702e357be758ede7b5d30db494
Hunter Kleindl <hkleindl@gmail.com>
2017-04-26 01:46:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for(var i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+var loudFacts = [];
+var i = 0;
+while(i < facts.length) {
+  loudFacts.push(facts[i] + '!!!')
+  i++;
+}
+return loudFacts;
+}
+
+function iLoveTheBeatles(n) {
+  var array = [];
+  do {array.push('I love the Beatles!')
+    n++;
+  } while (n < 15);
+  return array;
+}
