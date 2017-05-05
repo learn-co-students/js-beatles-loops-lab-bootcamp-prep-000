f0e51902b76e00a08f6a21837f0a20b696fce2b7
Jennifer Watson <jennifer.watson@me.com>
2017-05-05 17:44:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 function theBeatlesPlay(musicians,instruments) {
   var bandRole = {};
   for (var i = 0; i <= (musicians.length - 1); i++) {
-    bandRole = [...bandRole,[`${musicians[i]} plays ${instruments[i]}`]];
+    bandRole = [...bandRole,`${musicians[i]} plays ${instruments[i]}`];
   }
   return bandRole;
 }
 
 function johnLennonFacts(facts) {
   var i = 0;
+  var factsPlus = [];
   while (i <= (facts.length - 1)) {
-    var factsPlus = [...facts,`${facts[i]} !!!`];
+    factsPlus.push(`${facts[i]}!!!`);
     i++;
   }
   return factsPlus;
