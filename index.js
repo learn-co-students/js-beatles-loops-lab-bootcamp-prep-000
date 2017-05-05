3c0b2ed71c6df939def81a257ccc009c3e81f824
Jennifer Watson <jennifer.watson@me.com>
2017-05-05 02:58:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 function theBeatlesPlay(musicians,instruments) {
   var bandRole = {};
-  for (var i = 0; i <= musicians.length; i++) {
-    bandRole = [...bandRole,[`${musicians[i]} + "plays " + ${instruments[i]}"`]];
+  for (var i = 0; i <= (musicians.length - 1); i++) {
+    bandRole = [...bandRole,[`${musicians[i]} plays ${instruments[i]}`]];
   }
   return bandRole;
 }
 
 function johnLennonFacts(facts) {
   var i = 0;
-  while (i <= facts.length) {
-    facts.push(`${facts[i]} + "!!!"`);
+  while (i <= (facts.length - 1)) {
+    var factsPlus = [...facts,`${facts[i]} !!!`];
     i++;
   }
-  return facts;
+  return factsPlus;
 }
 
 function iLoveTheBeatles(n) {
