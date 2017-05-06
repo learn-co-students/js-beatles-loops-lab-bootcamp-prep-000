6613fcb77190ef1f22286db9c285e96d24398012
Sean Denison <sean.denison26@gmail.com>
2017-05-06 17:21:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function theBeatlesPlay(musicians, instruments) {
+  return musicians.map((beatle) => {
+    return beatle + " plays " + instruments[musicians.indexOf(beatle)];
+  });
+}
+
+function johnLennonFacts(array) {
+  return array.map((str) => str + "!!!");
+}
+
+function iLoveTheBeatles(n) {
+  let times = 15 - n;
+  if (times < 0) {
+    return ["I love the Beatles!"];
+  }
+  else {
+    let arr = new Array(times).join(".").split(".");
+    return arr.map((a) => "I love the Beatles!");
+  }
+}
