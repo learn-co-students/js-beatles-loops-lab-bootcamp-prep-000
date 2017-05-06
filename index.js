45c706caf544ab4cb8e74735d903f3ebe1684275
Arthur Miguel <retiredinfantry80@outlook.com>
2017-05-06 19:36:33 +0000

    Done.

--- index.js
+++ index.js
@@ -4,15 +4,14 @@ var facts = [];
 function theBeatlesPlay(musicians, instruments){
   for (var i = 0; i < 4; i++){
     beatles.push(musicians[i] + " plays " + instruments[i]);
-  }  
+  }
   return beatles
 }
 
 function johnLennonFacts(facts){
   var i = 0;
-  facts[i] = facts[i] +"!!!"
-  while (facts.length > 1){
-    facts.push(facts);
+  while (facts.length > i){
+    console.log(facts[i] + "!!!");
   }
-  return facts 
+  return facts
 }
