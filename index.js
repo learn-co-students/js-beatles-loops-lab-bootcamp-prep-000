4a44d1c29f1899798ee3d34d6a980671d37fed45
Shande Mengisteab <shandemengisteab@gmail.com>
2017-05-12 00:34:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function theBeatlesPlay(musicians, instruments) {
+  var emptyArray = [];
+  for (var i = 0; i < musicians.length; i++) {
+    emptyArray.push(musicians[i] + " plays " + instruments[i])
+  }
+  return emptyArray
+}
+function johnLennonFacts(facts) {
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+  var newFacts = [];
+  var i = 0
+  while (i < facts.length) {
+   newFacts.push(facts[i] + "!!!");
+   i++;
+ }
+ return newFacts;
+}
+function iLoveTheBeatles(num) {
+  var newBeatles = [];
+  do {
+    newBeatles.push("I love the Beatles!");
+    num++
+  } while (num < 15);
+  return newBeatles
+}
