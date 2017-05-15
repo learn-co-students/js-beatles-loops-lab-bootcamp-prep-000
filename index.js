d8635cdee97d660903d52e68c1553a34caf75c39
heidi lernihan <pelagia143@yahoo.com>
2017-05-15 05:37:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function theBeatlesPlay (musicians, instruments) {
+  var info = [];
+  for (var i = 0; i < musicians.length; i++) {
+    info.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return info;
+}
+
+
+function johnLennonFacts(facts) {
+  var i = 0
+  while (i < facts.length) {
+    facts[i]= facts[i] + `!!!`;
+    i++;
+  }
+  return facts;
+}
+
+
+function iLoveTheBeatles (number) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!")
+    number +=1;
+  } while (number < 15);
+  return array;
+}
