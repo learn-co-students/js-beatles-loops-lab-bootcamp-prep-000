b8de57d01eb9836cfb454ff2285e56754139b846
Desmond Drakes <desmond.drakes@gmail.com>
2017-05-23 02:28:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  for ( var i = 0; i < musicians.length; i ++ ) {
+    array.push(musicians[i] + " plays " + instruments[i])
+  }
+  return array
+}
+
+function johnLennonFacts(array) {
+  var i = 0
+  while (i < array.length) {
+    array[i] += "!!!"
+    i++
+  }
+  return array
+}
+
+function iLoveTheBeatles(number) {
+  var array = []
+  var i = 0
+
+  do {
+    array.push("I love the Beatles!")
+    number++
+  } while (number < 15);
+    return array;
+
+  // do {
+  //   array.push("I love the Beatles!")
+  //   i++
+  // } while (i < 8);
+  // return array;
+}
