993609a504d02afb35681d2ad8ea5ca1dd0155cd
Nancy Chan <nancychannc@gmail.com>
2017-05-25 18:16:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function theBeatlesPlay(musicians, instruments) {
+ var array = [];
+  
+  for (let i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return array
+}
+
+function johnLennonFacts(array) {
+  let i = 0
+  while (i < array.length) {
+    array[i++] += '!!!'
+  }
+
+  return array
+}
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  
+  do {
+    array.push("I love the Beatles!")
+    number++
+  } while(number < 15);
+
+  return array
+}
