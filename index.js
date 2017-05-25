2a4e872e8d4ef07beb3a2b826bedb15b91173a14
elle lee <eelzil77@gmail.com>
2017-05-25 04:38:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function theBeatlesPlay(musicians, instruments){
+  var array = [];
+  for(var i = 0; i < musicians.length; i++){
+    array.push(musicians[i] + ' plays ' + instruments[i]);
+  }
+  return array;
+}
+function johnLennonFacts(facts){
+  var results = [];
+  var count = 0;
+  while(count < facts.length){
+    results.push(facts[count] + '!!!');
+    count++;
+  }
+  return results;
+}
+function iLoveTheBeatles(n){
+  var array = [];
+  do{
+    array.push("I love the Beatles!");
+    n++;
+  }
+  while (n < 15);
+  return array;
+}
