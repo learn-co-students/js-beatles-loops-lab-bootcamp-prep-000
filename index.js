2db936bdd026d2dc9e9e5960dc2b8845426f7e56
Carol Greene <carolgreene3@gmail.com>
2017-05-31 01:07:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+var musicians = ['John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Starr']
+var instruments = ['Guitar', 'Bass Guitar', 'Lead Guitar', 'Drums']
+function theBeatlesPlay(musicians, instruments) {
+  var theyPlay = []
+  for( let i = 0; i < musicians.length; i++ ) {
+  theyPlay.push(`${musicians[i]} plays ${instruments[i]}`)
+}
+return theyPlay
+}
+
+function johnLennonFacts(array) {
+
+  return array
+}
