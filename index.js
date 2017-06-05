ac56c72c2f3dea6fdf51f0df28450e492ef5517e
sochetanie <allagorik2901@gmail.com>
2017-06-05 16:47:49 -0400

    done

--- index.js
+++ index.js
@@ -1,28 +1,43 @@
-function theBeatlesPlay(musicians, instruments){
-   var array = [];
-   for (var i=0; i < musicians.length; i ++) {
-     array.push(`${musicians[i]} plays ${instruments[i]}`)
-   }
-   return array;
- }
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+]
 
- var johnLennonFacts = function(facts) {
-   var newArray = [];
-   let i = 0;
-   while ( i < facts.length) {
-     var string = facts[i] + "!!!";
-     newArray.push(string);
-     i++;
-       }
-   return newArray
- }
+function theBeatlesPlay(musicians, instruments) {
+  var newArr = []
+  for (let i=0; i<musicians.length; i++) {
+    newArr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return newArr
+}
 
- function iLoveTheBeatles(n){
-   var array = [];
-   do {
-     array.push('I love the Beatles!');
-     n++;
-   }
-   while(n < 15);
-   return array;
- }
+
+function johnLennonFacts(facts) {
+  var newArr = []
+  for (let i=0; i<facts.length; i++) {
+    newArr.push(`${facts[i]}!!!`)
+  }
+  return newArr
+}
+
+
+// function iLoveTheBeatles(number) {
+//   var newArr = []
+//   while (number < 15) {
+//     newArr.push("I love the Beatles!")
+//     number++
+//   }
+//   return newArr
+// }
+
+// the same thing
+
+function iLoveTheBeatles(number) {
+  var newArr = []
+  do {newArr.push('I love the Beatles!')
+  number++
+  } while (number<15)
+  return newArr
+}
