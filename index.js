9cfd6d17357c9aa846f50513e36498df3b3c22d5
Sikander Chowhan <sikanderchowhan@gmail.com>
2017-06-15 17:14:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  for (var i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  var array2 = []
+  let i = 0
+  while (i < facts.length) {
+    array2.push(facts[i] + "!!!")
+    i++
+  }
+  return array2
+}
+
+function iLoveTheBeatles(n) {
+  var array3 = []
+  do {
+    array3.push("I love the Beatles!")
+    let n = 1
+  } while (n++ && n < 15);
+  return array3
+}
