7d81b2fabb0f318a1fea78d85f0b22c7845e9a60
Harry Ehrlich <harry@shadoobie.com>
2017-06-21 22:37:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+function theBeatlesPlay(players, instruments) {
+   var allPlayers = [];
+    for (var i=0; i < players.length; i++) {
+        allPlayers.push(players[i] + " plays " + instruments[i]);
+    }
+
+function johnLennonFacts(facts){
+  var newFacts= []
+  (while i< facts.length){
+    newFacts.push(facts[i] + "!!!")
+    i++
+  }
+   return newFacts
+}
+
+function iLoveTheBeatles (n) {
+  n=1
+  var phrase= []
+  do{
+  phrase.push "I Love The Beatles"
+  n++
+  }
+  while (n <15)
+  return phrase 
+}
