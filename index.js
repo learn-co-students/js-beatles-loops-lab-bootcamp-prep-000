499641f9f7896650a8c7730ca01159a8c024dddf
Rick Nilon <r.nilon92@gmail.com>
2017-06-27 20:49:51 +0000

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function theBeatlesPlay(musicians, instruments){
   for (let i =  0, l = musicians.length; i < l; i++){
   arr.push(`${musicians[i]} plays ${instruments[i]}`)
 }
+
   return arr
 }
 
@@ -11,10 +12,21 @@ function johnLennonFacts(facts){
 const LennonFacts = []
 
 let i = 0
-
-while([i < facts.length]) {
-LennonFacts.push(`${facts}!!!`)
+while(i < facts.length) {
+LennonFacts.push(`${facts[i]}!!!`)
 i++
 }
+
 return LennonFacts
 }
+
+function iLoveTheBeatles(n){
+  const arr = []
+
+  do{
+    arr.push("I love the Beatles!")
+  n++
+} while (n < 15)
+
+    return arr
+  }
