8255b37ca05c6f82251e867c5f51bc35cc75ea3b
Rick Nilon <r.nilon92@gmail.com>
2017-06-27 20:35:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function theBeatlesPlay(musicians, instruments){
+  const arr = []
+
+  for (let i =  0, l = musicians.length; i < l; i++){
+  arr.push(`${musicians[i]} plays ${instruments[i]}`)
+}
+  return arr
+}
+
+function johnLennonFacts(facts){
+const LennonFacts = []
+
+let i = 0
+
+while([i < facts.length]) {
+LennonFacts.push(`${facts}!!!`)
+i++
+}
+return LennonFacts
+}
