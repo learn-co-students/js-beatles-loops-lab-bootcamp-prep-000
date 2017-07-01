9285f21edd117bc191694bc5d36f0c41946d2d76
Harry Ehrlich <harry@shadoobie.com>
2017-07-01 21:06:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-'use strict';
-function theBeatlesPlay(players, instruments) {
-   var allPlayers = [];
-    for (var i=0; i < players.length; i++) {
-        allPlayers.push(players[i] + " plays " + instruments[i]);
-    }
-
-function johnLennonFacts(facts){
-  var newFacts= []
-  (while i< facts.length){
-    newFacts.push(facts[i] + "!!!")
-    i++
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+ for (var i = 0; i < musicians.length; i++) {
+    array.push(musicians[i] + " plays " + instruments[i]);
   }
-   return newFacts
+  return array;
 }
 
-function iLoveTheBeatles (n) {
-  n=1
-  var phrase= []
-  do{
-  phrase.push "I Love The Beatles"
-  n++
-  }
-  while (n <15)
-  return phrase 
+function johnLennonFacts(array) {
+  var i = 0;
+  while (i < array.length) {
+    array[i] = array[i] + "!!!";
+    i++;
+   }
+   return array;
+}
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!");
+    number += 1
+ } while (number < 15);
+ return array;
 }
