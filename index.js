1d3823fdbc55e844da0857b5f3148768a91a2a91
Will Middlebrook <will.d.middlebrook@gmail.com>
2017-07-03 09:51:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+var musicians = {"George", "John", "Paul", "Ringo"}
+var instruments = {"plays guitar", "sings", "plays bass", "plays drums"}
+function theBeatlesPlay (musicians, instruments) {
+  var emptyarray = [];
+  for (var musicians = 0; musicians <4; musicians++) {
+    var emptyarray = [theBeatlesPlay]
+  }
+
+}
