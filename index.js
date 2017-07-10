343d20cddaa95ad7cc422fce79a5e10bbe7547dd
Derek Strang <strang.derek@gmail.com>
2017-07-10 20:07:51 +0000

    Done.

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ function theBeatlesPlay(musicians, instruments) {
   return allPlayers
 }
 function johnLennonFacts() {
-    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-    var newFacts = [];
-    var i = 0;
+    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"]
+    var newFacts = ["foo", "Bar!"]
+    var i = 0
     while (i < facts.length) {
-        newFacts.push(facts[i] +"!!!");
-        i++;
+        newFacts.push(facts[i] + "!!!")
+        i++
     }
-    return newFacts;
+    return newFacts
 }
 function iLoveTheBeatles(num) {
    var love = [];
