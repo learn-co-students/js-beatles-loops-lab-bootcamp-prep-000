94dd6004ffdeaf2e79d8a3f31072d77b49679948
Stephen Decker <sdeckerm@gmail.com>
2017-07-15 21:38:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  for (var i = 0; i< musicians.length; i++){
+    array.push(musicians[i] + ' plays ' + instruments[i]);
+  }
+  return array
+}
+
+function johnLennonFacts(array) {
+  const facts = [];
+  let i = 0
+  while (i < array.length) {
+    facts.push(array[i] + '!!!')
+    i++
+  }
+  return facts
+}
+
+function iLoveTheBeatles(n){
+  var array = [];
+  do {
+    array.push('I love the Beatles!')
+    n++
+  } while (n < 15);
+  return array
+}
