1e51b4a483097053f6d179cb179680c378e50e7e
beardshadow <beardshadow@gmail.com>
2017-07-26 13:57:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,6 @@
+function theBeatlesPlay(musicians, instruments){
+  for (var i = 0; i < musicians.length; i++)
+  var play = []
+  play.push(`${musicians[i]} plays ${instruments[i]}`);
+  return play;
+}
