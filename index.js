55ec49374473e1117eb8d446f65c8b742c705486
beardshadow <beardshadow@gmail.com>
2017-07-26 20:07:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 function theBeatlesPlay(musicians, instruments){
-  for (var i = 0; i < musicians.length; i++)
-  var play = []
-  play.push(`${musicians[i]} plays ${instruments[i]}`);
+  var play = [];
+  for (var i = 0; i < musicians.length; i++){
+    play.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
   return play;
+}
+
+function johnLennonFacts(array){
+  var i = 0;
+  while (i < array.length){
+    array[i] += '!!!';
+    i++;
+  }
+  return array;
+}
+
+function iLoveTheBeatles(num){
+  var array = []
+  do {
+    array.push('I love the Beatles!');
+    num--;
+  } while (array.length < num);
+  return array;
 }
