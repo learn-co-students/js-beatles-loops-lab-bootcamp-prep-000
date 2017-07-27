360d9f71e2392e576be4a86abb2117a237249788
Daniel Wiehoff <daniel.wiehoff+flatiron@gmail.com>
2017-07-27 15:02:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function theBeatlesPlay(musicians, instruments) {
-  let tmpArr = [];
-  for (var i=0; i < musicians.length; i++) {
-    let str = musicians[i] + " plays " + instruments[i];
+  let tmpArr = []; // const arr = []
+  for (let i=0; i < musicians.length; i++) { // let i = 0, l = musicians.length; i < l
+    let str = musicians[i] + " plays " + instruments[i]; // `${mus[i]} plays ${ins[i]}`
     tmpArr.push(str);
   }
   return tmpArr;
 }
 
 function johnLennonFacts(facts) {
-  let n = 0
+  let n = 0 // const shoutedFacts = [] << make copy to fill
   while (n < facts.length) {
     facts[n] = facts[n] + "!!!"
     n++;
@@ -17,7 +17,7 @@ function johnLennonFacts(facts) {
 }
 
 function iLoveTheBeatles(n) {
-  let arr = [];
+  let arr = []; // const arr = []
   do {
     arr.push("I love the Beatles!")
     n++;
