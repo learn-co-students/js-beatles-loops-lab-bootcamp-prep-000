cf9a6cabecbc463ae649570b61ae4d03513d9caf
Araba Taylor <araba.taylor20@gmail.com>
2017-07-29 00:42:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function theBeatlesPlay(musicians,instruments){
+  var array =[];
+  for(var i=0; i<musicians.length ;i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array;
+}
+
+function johnLennonFacts(facts){
+  var excitedFacts = []
+  var i=0
+while(facts.length > i) {
+  excitedFacts.push(`${facts[i]}!!!`);
+  i++
+}
+return excitedFacts;
+}
