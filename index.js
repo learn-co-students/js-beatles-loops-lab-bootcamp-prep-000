57b8ad24326310d985224dafc1a81c08fd7dffb6
ff18qd <ff18qd@gmail.com>
2017-07-30 20:31:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function theBeatlesPlay(musicians, instruments) {
-  var results = new Array();
+  var results = {};
   for (let i = 0; i<musicians.length; i++) {
     results[i] = musicians[i]+" plays " + instruments[i]
   }
