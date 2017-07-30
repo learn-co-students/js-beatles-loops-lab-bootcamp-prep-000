c3a5f39350d5cbbe67eeb4c7464010d4056afb7e
Rick Nilon <r.nilon92@gmail.com>
2017-07-30 03:27:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
+var musicians = [];
+var instruments = []
+
 function theBeatlesPlay(musicians, instruments){
-  const arr = []
-
-  for (let i =  0, l = musicians.length; i < l; i++){
-  arr.push(`${musicians[i]} plays ${instruments[i]}`)
+var array = [];
+for (var i = 0; i < musicians.length; i++){
+array.push (musicians[i] + " plays " + instruments[i]);
+  //console.log (musicians[i] + " plays " + instruments[i]); didnt work because we need to add to arr, then return arr
+  //return doesnt work with loop. use console.log "return (musicians[i] " plays " instruments[i])""
+}
+return array;
 }
 
-  return arr
-}
 
 function johnLennonFacts(facts){
-const LennonFacts = []
+/*while (var i = 0; i < facts.length){
+console.log (facts[i])};*/
+var lennonFacts = [];
+var i = 0;
+while (i < facts.length){
+  lennonFacts.push (facts[i] + "!!!");
+  i++;
+}
+return lennonFacts;
 
-let i = 0
-while(i < facts.length) {
-LennonFacts.push(`${facts[i]}!!!`)
-i++
 }
 
-return LennonFacts
-}
 
 function iLoveTheBeatles(n){
-  const arr = []
-
-  do{
-    arr.push("I love the Beatles!")
-  n++
-} while (n < 15)
-
-    return arr
-  }
+var number = 0;
+var beatlesLove = [];
+do{
+  beatlesLove.push ('I love the Beatles!')
+  n++;
+}
+while (n < 15)
+return beatlesLove;
+}
