15c58a913b0234dd08bc30eb24ee0f4079b994e3
Michelle Wiraydi <miwiryadi@gmail.com>
2017-08-02 04:40:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 
 
+
+
 function theBeatlesPlay(musicians, instruments){
-  array = [];
+  var array = [ ];
   for (var i=0; i < musicians.length; i++){
-    array.push(musicians[i]+ " plays " + instrument[i]);
+    array.push(musicians[i]+ " plays " + instruments[i]);
   }
   return array;
 }
 
+var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+
 function johnLennonFacts(facts){
-  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
   var newFacts = [];
-  var i =0;
+  var i = 0;
   while (i < facts.length){
-    newFacts.push(facts[i] + " !!!");
+    newFacts.push(facts[i] + "!!!");
+    i++;
   }
   return newFacts;
 }
