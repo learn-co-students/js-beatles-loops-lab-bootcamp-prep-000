55bada3673bf8c18ae995fc3d9150e7f7f5a8138
Robin Pahlman <robin.pahlman@gmail.com>
2017-08-11 02:46:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < musicians.length; i++) {
+    array.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  var count = 0;
+  var array = []
+  while (count < facts.length) {
+    array.push(facts[count] + "!!!");
+    count++;
+    }
+    return array;
+}
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!")
+    number++;
+  } while (number < 15);
+  return array;
+}
