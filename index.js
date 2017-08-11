989c793619905451fd63bf1de5ba6dbc2461c7e1
Dustin Astrin <dustastrin@gmail.com>
2017-08-11 13:26:19 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function theBeatlesPlay(musicians,instruments) {
+  var array = [];
+
+  for (var i=0; i<musicians.length; i++ ){
+
+array[i] = (musicians[i] + " plays " + instruments[i])
+
+}
+  return array
+}
+
+function johnLennonFacts(facts) {
+  var i = 0
+  while (i < facts.length) {
+   facts[i] = facts[i] + "!!!";
+  i++;
+}
+return facts
+}
+
+function iLoveTheBeatles(num) {
+  var array = []
+  do {
+    array.push('I love the Beatles!')
+    num++}
+    while (num < 15);
+
+  return array
+
+}
