947d8f4ea7c88d86b96123e4ae804d56b53216fe
Zhipeng Wen <learnor@gmail.com>
2017-08-15 03:10:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function theBeatlesPlay (musicians, instruments) {
+  var newArray = []
+  for (var i = 0; i < musicians.length; i++) {
+    newArray.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return newArray
+}
+
+function johnLennonFacts(facts) {
+  var i = 0
+  while (facts[i]) {
+    facts[i] += '!!!'
+    i++
+  }
+  return facts
+}
+
+function iLoveTheBeatles(n) {
+  var newArray = []
+  do {
+    newArray.push('I love the Beatles!')
+    n++
+  } while (n<15);
+  return newArray
+}
