ef9084c0aacab4646af41c90164f3ca5aac14af2
nitchan parnitch <nitchan.pn@gmail.com>
2017-08-29 02:49:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function theBeatlesPlay(musicians,instruments){
+  var empty = new Array();
+  for( var i = 0; i < musicians.length; i++){
+    empty.push(musicians[i] + " plays " + instruments[i])
+  }
+  return empty;
+}
+
+function johnLennonFacts(facts){
+  var newFacts = new Array();
+  var i = 0;
+  while( i < facts.length){
+    newFacts.push(facts[i] + '!!!');
+    i++;
+  }
+  return newFacts;
+}
+
+function iLoveTheBeatles(number){
+  var newArry = new Array();
+  do{
+    newArry.push("I love the Beatles!");
+    number++;
+  }
+  while(number < 15);
+  return newArry;
+}
