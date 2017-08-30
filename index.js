6754fafaee8e45ae0e3b986864d83913ed6f3a02
Mark Fowler <fowlermark488@gmail.com>
2017-08-30 18:26:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
 function theBeatlesPlay(musicians, instruments) {
-  var newArray = [];
-  for (var i = 0; i < musicians.length; i++) {
-    newArray.push(musicians[i] + " plays " + instruments[i])
-  }
-  return newArray;
+	var arr = [];
+
+  	for (var i = 0; i < musicians.length; i++) {
+    	arr.push(`${musicians[i]} plays ${instruments[i]}`)
+    }
+  return arr;
 }
 
 function johnLennonFacts(facts) {
-  var newFacts = facts
-  var i = 0
+  var i = 0;
   while (i < facts.length) {
     facts[i] += "!!!";
     i++;
   }
-  return newFacts;
+  return facts;
 }
 
-function iLoveTheBeatles(number) {
-  var array = [];
-  if (number < 15) {
+function iLoveTheBeatles(num) {
+  var arr = [];
+
   do {
-    array.push("I love the Beatles!")
-  } while (array.length < (number + 1));
-  return array;
-}
-else {
-  return "I love the Beatles!";
-}
+    arr.push("I love the Beatles!");
+    num++;
+  } while (num < 15);
+
+return arr;
 }
