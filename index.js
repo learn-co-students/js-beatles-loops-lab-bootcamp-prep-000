53b35d9151f74f29b3be96c5d9d742fc5912e53e
zuriel stanley <zuriel.stanley6@gmail.com>
2017-09-11 21:41:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+var musician = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
+
+var instrument = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
+
+
+function theBeatlesPlay(musician, instrument) {
+
+  var newArr = new Array();
+
+  for (i = 0; i < 6; i++) {
+    console.log('${musician} plays ${instrument}')
+  }
+
+}
