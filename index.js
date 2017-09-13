48e593b0ca526eee1626a6b52633329457bee8d0
Mukhammadayub Siddikov <m.siddikov@gmail.com>
2017-09-13 09:46:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function theBeatlesPlay(artist, intrument){
+  var str = [];
+  for (let i=0, i<artist.length or i<instrument.length){
+    str.push(`${artist[i]} plays ${instrument[i]}`);
+  }
+
+  return str;
+}
+
+function johnLennonFacts(facts){
+  let i = 0;
+  while (i<facts.length) {
+    facts[i] = facts[i]+"!!!"
+  }
+}
+
+function iLoveTheBeatles(number){
+  var array = [];
+  do{
+    array.push("I lova the Beatles!");
+    number++;
+  } while (number<15)
+
+  }
+}
