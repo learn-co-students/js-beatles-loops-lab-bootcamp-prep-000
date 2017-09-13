5d3774e66618f63b04f1eb6c9b5ef66d54826ddf
M V <vahyno+learn@gmail.com>
2017-09-13 06:55:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function theBeatlesPlay(musicians,instruments){
+  var newStrings = [];
+  for (var i = 0; i<=musicians.length-1; i++) {
+  newStrings[i]= (musicians[i]+" plays "+instruments[i]);
+}
+  return newStrings;
+}
+
+function johnLennonFacts(facts){
+  var strings = []
+  let i = 0;
+  while (i <= facts.length-1) {
+  (strings[i] = facts[i]+"!!!");
+  (i++);
+}
+  return strings;
+}
+
+
+
+
+
+
+
+
+
+function iLoveTheBeatles(number){
+  var emptyArray = []
+  do {
+    emptyArray.push("I love the Beatles!");
+    ++number;
+  }
+  while (number<15);
+  return emptyArray;
+}
