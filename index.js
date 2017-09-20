989005505248f2c1908db3245030d34ffc77a0a3
Eric Roginek <eroginek@protonmail.com>
2017-09-20 22:01:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+function theBeatlesPlay(musicians, instruments){
+  var variable = {}
+    for (let i = 0; i <= musicians.length; i++){
+      var variable = {musicians + " plays " + instruments}
+      variable.push()
+    }
+    return variable
+}
