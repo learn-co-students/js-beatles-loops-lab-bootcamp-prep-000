d6685f65af0f6dd565fd117967ec9c96a02b777e
Alex Levine <aalexlevine@gmail.com>
2017-09-23 05:45:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function theBeatlesPlay(musicians, instruments) {
+  var emptyArray = [];
+     for (let i = 0; i < musicians.length; i++) {
+       emptyArray.push(musicians[i] + " plays " + instruments[i]);
+     }
+return emptyArray
+}
+
+function johnLennonFacts(array) {
+   var result= [];
+   let i = 0;
+   while(i < array.length) {
+     result.push(array[i] + '!!!')
+     i++;
+   }
+   return result;
+}
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  var i = 0;
+  do {
+    array.push('I love the Beatles!');
+    i++;
+  } while (i <= number && number < 15);
+   return array;
+ }
