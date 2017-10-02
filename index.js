6561b048b5569a5fbb85d3989dc72c37f482cedf
Jaime Zuspann <jaimeadele@gmail.com>
2017-10-02 19:20:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theBeatlesPlay(musicians, instruments){
+  const arr = [];
+  for (var i = 0; i < musicians.length; i++){
+    arr.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return arr;
+}
+
+function johnLennonFacts(facts){
+  for (var i = 0; i < facts.length; i++){
+    facts[i] += "!!!";
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(num){
+  var arr = [];
+  do {
+    arr.push('I love the Beatles!');
+    num++;
+  } while (num < 15);
+  return arr;
+}
