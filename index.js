930b98a8c5c1589a6117ed0c27c5ac645dc4c5c8
Daaf Bleumink <daaf@semso.nl>
2017-10-04 10:13:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+function theBeatlesPlay (musicians, instruments) {
+  var musicians = [];
+  var musicianslength = musicians.length;
+  for (i = 0; i < musicianslength; i++) {
+    array.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return array
+}
