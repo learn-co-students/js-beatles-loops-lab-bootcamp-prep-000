ba0041947c4d44efed8b769434e9745cedd8163a
jonathan Morton <jmorton4193@gmail.com>
2017-10-08 17:06:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 function theBeatlesPlay(musicians, instruments){
-var muscicians = []
-var instruments= []
 var strings = []
-for (let i=0; i<3; i++) {
-strings.push("'$(musicians[i])' played '$(instruments[i])'")
+for (let i=0; i<musicians.length; i++) {
+strings.push(`${musicians[i]} plays ${instruments[i]}`)
 }
 return strings
+}
+
+function johnLennonFacts(facts){
+  let i=0
+  while (i<facts.length){
+facts[i]+="!!!"
+i++
+}
+  return facts
+}
+
+function iLoveTheBeatles (number) {
+  var array=[]
+do{
+array.push("I love the Beatles!")
+++number
+}
+while(number<15)
+return array
 }
