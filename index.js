87601eb51fe63f11d73a20cfc1e5a4b9d0eb9c6c
Jerry Bennett <jerrysbusiness@gmail.com>
2017-10-10 17:43:06 -0400

    Update index.js
    added comments

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function theBeatlesPlay( musicians, instruments ) {
   var arr = [];
+  // iterates through musicians array
   for (var i = 0; i < musicians.length; i++) {
+    // pushes a string with current value of musicians index and intruments index to arr
     arr.push(musicians[i] + " plays " + instruments[i]);
   }
   return arr;
@@ -9,8 +11,11 @@ function theBeatlesPlay( musicians, instruments ) {
 function johnLennonFacts( facts ) {
   var newFacts = [];
   var i = 0;
+  // while i is < the length of the facts array
   while (i < facts.length) {
+    // pushes string with current value of facts index to newFacts array with "!!!"
     newFacts.push(facts[i] + '!!!');
+    // goes to next index in facts array
     i++;
   }
   return newFacts;
