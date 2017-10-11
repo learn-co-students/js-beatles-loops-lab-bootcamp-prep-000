079aae827743438f779eabce2cfe1909a8b83282
Daaf Bleumink <daaf@semso.nl>
2017-10-11 14:30:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-function theBeatlesPlay (musicians, instruments) {
+function theBeatlesPlay(musicians, instruments) {
   var array = [];
-  for (i = 0; i < musicians.length; i++) {
+  for (var i = 0; i < musicians.length; i++) {
     array.push(musicians[i] + " plays " + instruments[i]);
   }
-  return array
+  return array;
+}
+
+function johnLennonFacts(array) {
+  var i = 0
+  while (i < array.length) {
+    array[i] += '!!!';
+    i++;
+  }
+  return array;
+}
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!");
+    number += 1
+  } while (number < 15);
+  return array;
 }
