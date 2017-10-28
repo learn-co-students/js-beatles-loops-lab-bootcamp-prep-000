04f3dd5515b8e35ec28ebc6a751c065e7e84ff46
Liz Burton <liz.burton147@gmail.com>
2017-10-28 15:27:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,7 @@
+function theBeatlesPlay(musicians,instruments) {
+  var array = [];
+  for (i=0; i < musicians.length; i++) {
+      array.push(musician[i] + " plays " + instruments[i]);
+  }
+  return array;
+}
