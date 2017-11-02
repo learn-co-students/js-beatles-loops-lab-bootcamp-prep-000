05233e52175175dbe32adbdcadc4f138456cc4eb
harenarium <carolinel2610@gmail.com>
2017-11-02 23:54:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,7 @@
+function theBeatlesPlay(arrayMusicians, arrayInstruments){
+  var array = []
+  for(n=0, n<arrayMusicians.length, n++){
+    array.push(`arrayMusicians[n] plays arrayInstruments[n]`)
+  }
+  return array
+}
