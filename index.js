56f3d02e5c60d897cf187466c1792366fe0686c5
Patricia Bardowell <patricia.bardowell@gmail.com>
2017-11-02 00:10:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 function theBeatlesPlay (musiciansArray, instrumentsArray) {
   var newArray = []
-  for (var i=0; i < 5; i++) {
-    newArray[i] = musicianArray[i] + "plays" + instrumentsArray[i]
+  for (var i=0; i < 4; i++) {
+    newArray[i] = musiciansArray[i] + " plays " + instrumentsArray[i]
   }
+  return newArray
+}
+function johnLennonFacts(arrayFacts) {
+  var counter = 0
+  while (counter < arrayFacts.length) {
+    arrayFacts[counter] = arrayFacts[counter] +"!!!"
+    counter ++
+  }
+  return arrayFacts
+}
+function iLoveTheBeatles (number) {
+  var newArray = []
+  do {
+    newArray.push("I love the Beatles!")
+    number ++
+  }
+  while (number < 15)
   return newArray
 }
