c706f45b9ae1fe69b41b562733c94febee3a5da4
harenarium <carolinel2610@gmail.com>
2017-11-03 02:50:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 function theBeatlesPlay(arrayMusicians, arrayInstruments){
   var array = []
-  for(n=0, n<arrayMusicians.length, n++){
-    array.push(`arrayMusicians[n] plays arrayInstruments[n]`)
+ for(var n =0; n<arrayMusicians.length; n++){
+    array.push(`${arrayMusicians[n]} plays ${arrayInstruments[n]}`)
+ }
+  return array
+}
+
+function johnLennonFacts(array){
+  var n =0
+  while(n<array.length){
+    array[n]=array[n]+"!!!"
+    n++
   }
+  return array
+}
+
+function iLoveTheBeatles(number){
+  var array = []
+  do{
+    array.push("I love the Beatles!")
+    number++
+  }while(number<15)
   return array
 }
