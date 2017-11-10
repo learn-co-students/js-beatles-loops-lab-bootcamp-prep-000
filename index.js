581e4491b3abc69aeec8a18fcde93c6d4c5a1c72
Sean Derrow <sderrow@mit.edu>
2017-11-10 17:30:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function theBeatlesPlay(musicians, instruments) {
+  var myArray = []
+  for(var i = 0; i < musicians.length; i++) {
+    myArray.push(musicians[i] + " plays " + instruments[i])
+  }
+  return myArray
+}
+
+function johnLennonFacts(facts) {
+  var myArray = []
+  var i = 0
+  while ( i < facts.length ) {
+    myArray.push(facts[i] + '!!!')
+    i++
+  }
+  return myArray
+}
+
+function iLoveTheBeatles(n) {
+  var myArray = []
+  do {
+    myArray.push('I love the Beatles!')
+    n++
+  } while ( n < 15 )
+  return myArray
+}
