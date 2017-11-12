04d7aa99e5aa330bf60cd49539e4eddaf9cfed23
Mayank Shah <mayank.24shah@gmail.com>
2017-11-12 23:30:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function theBeatlesPlay(array1,array2){
+  var array3 = [];
+  for(var i=0;i<array1.length;i++){
+    array3.push(array1[i]+" plays "+array2[i]);
+  }
+return array3;
+}
+function johnLennonFacts(facts){
+  var i = 0;
+  var array1 = [];
+  while(facts.length>i){
+ array1.push(facts[i]+ "!!!")
+ i++
+  }
+  return array1;
+}
+function iLoveTheBeatles(i){
+  var array1 = [];
+  do{
+    array1.push("I love the Beatles!");
+    i++ 
+  }
+  while (i<15){
+  }
+  return array1; 
+}
