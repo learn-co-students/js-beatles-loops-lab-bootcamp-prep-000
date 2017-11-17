4779eaf39f7b0d1e939e71833c849a22f770b27e
kathleen wang <kathleenchibimaru@gmail.com>
2017-11-17 00:37:19 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+function theBeatlesPlay(musicians, instruments) {
+var empty = [];
+var i;
+for (i=0; i < musicians.length; i++) {
+ var newString = console.log(musicians[i] + ' plays ' + instruments[i])
+ empty.push = newString
+}
+return empty
+}
