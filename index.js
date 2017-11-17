b0f8145c5e769d465439b1d8359239c2c072c6ab
kathleen wang <kathleenchibimaru@gmail.com>
2017-11-17 00:49:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 function theBeatlesPlay(musicians, instruments) {
 var empty = [];
-var i;
-for (i=0; i < musicians.length; i++) {
- var newString = console.log(musicians[i] + ' plays ' + instruments[i])
- empty.push = newString
+
+for (var i=0; i < musicians.length; i++) {
+ var str = musicians[i] + ' plays ' + instruments[i] ;
+ empty.push(str)
 }
 return empty
 }
+
+function johnLennonFacts(facts) {
+ var counter = 0;
+ while (counter < facts.length) {
+   facts[counter]+= '!!!';
+   counter++
+ }
+ return facts ;
+ }
+
+ function iLoveTheBeatles(number){
+   var empty = [];
+   do {
+     empty.push('I love the Beatles!');
+     number++
+   }
+   while (number < 15)
+   return empty
+ }
