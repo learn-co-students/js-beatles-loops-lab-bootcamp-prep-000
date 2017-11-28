92a53bb6a42aae9b24f377d21c39070aeba78b86
Jay G <playerjayho@gmail.com>
2017-11-28 16:59:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function theBeatlesPlay(musicians, instruments) {
+    var empty = [];
+    for(var i=0;i < 4; i++) {
+        //empty = musicians[0] + " plays " + instruments[0];
+        empty.push(musicians[i] + " plays " + instruments[i]);
+
+    }
+
+
+      return empty;
+}
+
+ function johnLennonFacts(facts) {
+        var empty = [];
+        var i =0;
+       while(i<facts.length) {
+         empty.push(facts[i] + "!!!");
+         i++;
+       }
+       return empty;
+ }
+
+
+function iLoveTheBeatles(number) {
+
+}
