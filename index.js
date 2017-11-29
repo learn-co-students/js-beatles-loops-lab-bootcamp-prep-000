2f1dafa2f5fd6332fe1c476ed96f9549575ffb7c
Peter Coughlin <coughlin1966@gmail.com>
2017-11-29 18:40:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+const musicians = ['Jonh Lennon', 'Paul Mcartney', 'George Harrison', 'Ringo Starr']
+const instruments = ['Guitar', 'Guitar', 'Guitar', 'Drums']
+
+function theBeatlesPlay (musicians,instruments) {
+  var theyPlay = []
+
+  for (let i = 0; i < musicians.length; i++) {
+  theyPlay.push (musicians[i] + " plays " + instruments[i])
+  }
+return theyPlay
+}
+theBeatlesPlay()
+
+const facts = ["He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"]
+  var i = 0
+
+  function johnLennonFacts(facts) {
+    var updatedFacts = []
+    while (i <= facts.length) {
+      updatedFacts.push (facts[i] + "!!!")
+    }
+return updatedFacts
+  }
