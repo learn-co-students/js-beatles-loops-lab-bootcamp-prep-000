35831532413e99a4fcad0bf090143a54484b98f4
Tim Shipman <bricklayertim@yahoo.com>
2017-12-01 19:11:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function theBeatlesPlay(musicians, instruments){
-  var array = [];
+  const array = [];
   for (let i = 0, l = musicians.length; i < l; i++) {
     array.push(`${musicians[i]} plays ${instruments[i]}`)
   }
@@ -8,5 +8,19 @@ return array
 
 function johnLennonFacts(facts){
   const yelling = []
+  let i = 0
+  while (i < facts.length){
+    yelling.push(`${facts[i]}!!!`)
+    i++
+  }
+  return yelling
+}
 
+function iLoveTheBeatles(n){
+  var heart = []
+  do {
+    heart.push("I love the Beatles!")
+    n++
+  }while (n < 15)
+  return heart
 }
