1dd01f7b33f92c12f954592f3fb86058ffcf84da
Mariel Schlaefer <mschlaefer00@gmail.com>
2017-12-04 17:46:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = []
   for (let i = 0; i < musicians.length; i++) {
-    array.push(musicians[i] + " plays" + instruments[i]);
+    array.push(musicians[i] + " plays " + instruments[i]);
   }
   return array;
 }
 
 function johnLennonFacts(facts) {
-  while () {
+  while (i < facts.length) {
     facts.push("!!!")
   }
   return facts;
