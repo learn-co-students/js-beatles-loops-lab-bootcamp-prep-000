f14bf6b93b88b25f991b7bbc89d03f9b43e7badd
Azumi Callanan <azucm@hotmail.com>
2017-12-06 18:30:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function theBeatlesPlay(musicians,instruments){
+  var array=[];
+  for(var i=0;i<musicians.length;i++){
+    array.push(musicians[i]+" plays "+ instruments[i]);
+    }
+    return array;
+ }
+
+function johnLennonFacts(array){
+  var i = 0;
+  while (i < array.length) {
+    array[i]=array[i] +"!!!";
+    i++;
+ }
+ return array;
+}
+
+ 
+function iLoveTheBeatles(number){
+  var beatlesArray=[];
+  do{
+    beatlesArray.push("I love the Beatles!");
+    number++;}
+    while (number<15);
+    return beatlesArray;
+  }
+
+
+ 
