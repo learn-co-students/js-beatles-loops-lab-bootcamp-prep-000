333664bbde77fac8fb2821dbbf920f848a64b797
Samuel Comerford <samcomerford1825@gmail.com>
2017-12-10 17:03:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-function theBeatlesPlay (musician, instrument) {
+function theBeatlesPlay(musician, instrument) {
   var array = []
-  for (i = 0; i < musician.length; i++) {
+  for (var i = 0; i < musician.length; i++) {
     array.push(`${musician[i]} plays ${instrument[i]}`)
   }
+  return array
+}
+
+
+function johnLennonFacts (facts) {
+  var i = 0
+  while (i < facts.length) {
+    facts[i] = facts[i].concat("!!!")
+    i++
+  }
+  return facts
+}
+
+
+function iLoveTheBeatles(n) {
+  var array = []
+  do {
+    array.push("I love the Beatles!")
+    n++
+  } while (n < 15)
   return array
 }
