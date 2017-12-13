8a78452b9764edf08bb2e38e4720f3be15571bdb
Jeremy Crichton <jeremycrichton@gmail.com>
2017-12-13 12:28:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+function theBeatlesPlay(musicians, instruments) {
+  var arrayOfString = [];
+
+  for (var i = 0; i < musicians.length; i++) {
+    var sentence = `${musicians[i]} plays ${instruments[i]}`
+  }
+  return arrayOfString;
+}
