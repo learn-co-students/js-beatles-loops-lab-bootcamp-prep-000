10658e2ebaa776f862c904f3eccab7e6c91c30c2
Quentin <q.smith1@yahoo.com>
2017-12-13 04:20:28 +0000

    Done.

--- index.js
+++ index.js
@@ -3,32 +3,6 @@ function theBeatlesPlay(musicians, instruments) {
   for (var i = 0; i < musicians.length; i++)
   for (i = 0; i < instruments.length; i++) {
     myArray.push(musicians[i] + " plays " + instruments[i]);
-  var myArray = newArray()
-  for (i = 0; i<musicians.length; i++)
-  for (i = 0; i<instruments.length; i++) {
-    myArray.push("${musicians[i]} plays + ${instruments[i]}");
-}
+ }
   return myArray
 }
-
-function johnLennonFacts(facts) {
-   var newFacts = []
-   let i = 0
-   while (i < facts.length) {
-     newFacts.push(facts[i] + "!!!")
-     i++
-   }
-   return newFacts
- }
- 
- function iLoveTheBeatles(number) {
-  var phrase = []
-  do {
-     phrase.push("I love the Beatles!");
-     number++
-
-   do {
-     phrase.push("I love the Beatles!");
-   } while (number < 15)
-   return phrase
- }
