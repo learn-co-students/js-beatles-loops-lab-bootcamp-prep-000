18aefde250bb68d103e37f11ec74dda822d5660f
David Perez <davperez89@gmail.com>
2017-12-15 01:50:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 function theBeatlesPlay(musicians,instruments){
-  musicians = var emptyArray()
-  
+  var array = [];
+  for (let i=0; i < musicians.length; i++) {
+      var string = `${musicians[i]} plays ${instruments[i]}`;
+      array.push(string);
+      }
+      return array
+}      
+
+function johnLennonFacts(facts){
+  var array =[];
+  let i=0
+  while (i < facts.length){
+    array.push(`${facts[i]}!!!`);
+    i++
+  } 
+  return array
 }
+
+function iLoveTheBeatles(n){
+  var array = [];
+  do { 
+    array.push("I love the Beatles!");
+    n++;
+  } while (n <15)
+  return array
+}
+
+
+
