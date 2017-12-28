4f10fcf31e5d27ab5335604b8d04496148f7e9bc
Shirley Zhang <shirleyz9402@yahoo.com>
2017-12-28 03:42:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-/*const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-*/
 function theBeatlesPlay(musicians,instruments){
 var array = []
   for (let i = 0; i < 4; i++) {
@@ -8,12 +8,6 @@ var array = []
     }
   return array
 }
-/*const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];*/
 function johnLennonFacts(facts) {
 var shoutFacts = []
 let i=0 
