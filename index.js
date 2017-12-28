aa9b2003372d4e95e3298db988ba7738bb35c2e0
Shirley Zhang <shirleyz9402@yahoo.com>
2017-12-28 04:56:20 +0000

    Done.

--- index.js
+++ index.js
@@ -8,11 +8,26 @@ var array = []
     }
   return array
 }
+
 function johnLennonFacts(facts) {
 var shoutFacts = []
-let i=0 
+let i = 0
  while (i<facts.length)  {
     shoutFacts.push(`${facts[i]}!!!`);
-  }
+  i++
+ }
   return shoutFacts;
+}
+function iLoveTheBeatles(x) {
+  var array = [];
+  if (x > 15)
+  {array.push("I love the Beatles!")
+  return array}
+  let i = 0
+  do {
+   array.push("I love the Beatles!")
+   i++
+  }
+  while (i <= x)
+  return array;
 }
