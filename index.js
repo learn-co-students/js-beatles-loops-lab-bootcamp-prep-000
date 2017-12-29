762a714db8a73cc360857a805cb6d08e826d6159
Stephane <son.trinh.dev@gmail.com>
2017-12-29 05:23:27 +0000

    Done.

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles(num) {
   var result = [];
   do {
-    result.push('I love the Beatles!')
+    result.push('I love the Beatles!');
+    num++;
   } while (num < 15);
 
   return result;
