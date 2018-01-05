2fd35573ea4dcbfd4d152ee8ffd0fe7cbfe83e01
mogon2990 <mogon2990@gmail.com>
2018-01-05 04:38:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function theBeatlesPlay(musicians, instruments) {
+  var newArray = [];
+  for (var i = 0; i < musicians.length; i++) {
+    newArray.push(musicians[i] + " plays " + instruments[i])
+  };
+  return newArray
+}
+
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  while (i < facts.length) {
+    facts[i] = facts[i] + "!!!";
+    i += 1;
+  }
+  return facts
+}
+
+function iLoveTheBeatles(number) {
+  var newArray = [];
+   do {
+     newArray.push("I love the Beatles!");
+     number += 1;
+   } while (number < 15);
+   return newArray
+}
