80ca55236d008b6e9062f0d13397972bd49b90a0
Tim Hillhouse <thillhouse2@gmail.com>
2018-01-06 17:39:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 function theBeatlesPlay(musicians, instruments){
   var newArray = [];
-  for (var i = 0; i < [musicians].length; i++){
+  for (var i = 0; i < musicians.length; i++){
     newArray[i] = musicians[i] + " plays " + instruments[i];
-    console.log(newArray[i])
-    console.log(musicians.length)
   }
+  return newArray;
+}
+
+function johnLennonFacts(facts){
+  var newArray = [];
+  var i = 0;
+  while(i < facts.length){
+    newArray[i] = facts[i] + "!!!";
+    i++;
+  }
+  return newArray;
+}
+
+function iLoveTheBeatles(loopX){
+  var newArray = [];
+  var i = 0;
+  do{
+    newArray[i] = "I love the Beatles!";
+    i++;
+  } while (loopX < 15 && i <= loopX);
+  return newArray;
 }
