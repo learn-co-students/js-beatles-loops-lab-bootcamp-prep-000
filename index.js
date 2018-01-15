2c61c40a541f1c4760f1b8626455675c4ffd73cc
Chris Beyer <beyercorpuz@gmail.com>
2018-01-15 02:07:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,43 @@
+function theBeatlesPlay(musicians, instruments){
+  var text = []
+  for (var i = 0; i < musicians.length; i++){
+  text.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return text
+}
+
+
+function johnLennonFacts(facts){
+  var text=[]
+  let i = 0;
+while (i < facts.length){
+  text.push(`${facts[i]}!!!`)
+i++
+}
+  return text
+}
+
+
+function iLoveTheBeatles(num){
+  //return an array with "I love the Beatles!" inside num number of times
+
+  //var text = []
+  //var i = 0;
+//do {
+  //   i += 1;
+  //   result += i + ' ';
+  //} while (i < 8);
+//  return text
+
+
+//
+
+function iLoveTheBeatles(num){
+   const array = []
+  	do{
+      // do somethng to the array //
+        num++
+      } while (num < 7)
+
+       return array
+  }
