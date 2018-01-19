7b6a8ed57169ab94e9ae85b9d8da741e156b6935
rosie w <rosie.wilt@gmail.com>
2018-01-19 03:07:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+// for (var i = 1; i < 100; i++) {
+//   console.log( "Hello World the " + i + " time" );
+// }
+
+function theBeatlesPlay(instruments,musicians){
+  var instruments = []
+  var musicians = []
+  for (var musicians = 0, musicians > 0, i++) {console.log(`${musicians} plays ${instruments}`)})
+}
