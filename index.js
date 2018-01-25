03bbf9fcf660b04bb1f54c8f31c185e10b2f7f10
Skylar Dunn-Lubin <skylar.dunnlubin@gmail.com>
2018-01-25 03:09:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function theBeatlesPlay(musicians, instruments) {
+  const arr = []
+
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return arr
+}
+
+function johnLennonFacts(facts) {
+  const shoutedFacts = []
+
+  let i = 0
+  while (i < facts.length) {
+    shoutedFacts.push(`${facts[i]}!!!`)
+    i++
+  }
+
+  return shoutedFacts
+}
+
+function iLoveTheBeatles(n){
+  const num = []
+  do { num.push("I love the Beatles!")
+  n++}
+  while ( n<15 )
+  
+  return num
+}
