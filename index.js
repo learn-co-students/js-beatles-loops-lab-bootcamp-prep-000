a4483ee130f29e76e04586971ecd8dd7bba692ed
Jon Osterman <suni.abraham@gmail.com>
2018-01-31 19:37:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,63 @@
+function theBeatlesPlay(musician, instruments){
+  var newarray = [];
+
+  for (var i =0; i<musician.length; i++){
+    newarray.push(musician[i] + " plays " + instruments[i]);
+  }
+
+  return newarray;
+}
+
+/*
+var facts = [
+"He was the last Beatle to learn to drive",
+"He was never a vegetarian",
+"He was a choir boy and boy scout",
+"He hated the sound of his own voice"
+];
+*/
+
+
+function johnLennonFacts(facts){
+//  var facts = [
+//  "He was the last Beatle to learn to drive",
+//  "He was never a vegetarian",
+//  "He was a choir boy and boy scout",
+//  "He hated the sound of his own voice"
+//  ];
+  var newfacts = []
+  let i = 0
+    while (i<facts.length)  {
+      newfacts.push(facts[i] + "!!!")
+      i++;
+    }
+  return newfacts;
+
+}
+
+
+
+function iLoveTheBeatles(num){
+  var emptyarray = [];
+
+  do {
+
+    emptyarray.push("I Love The Beatles!");
+    num++;
+
+  } while (num < 15);
+
+  return emptyarray;
+
+}
+
+/*
+function johnLennonFacts(facts) {
+  let n = 0
+  while (n<=facts.length)  {
+    facts[n] + "!!!";
+    n++
+  }
+    return facts;
+  }
+*/
