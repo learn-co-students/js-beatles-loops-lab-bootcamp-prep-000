3531a9152573a3416614a2e41e486fc136bbc26d
combskyle@gmail.com <combskyle@gmail.com>
2018-02-02 17:19:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < 4; i++) {
+    array.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  var newFacts = []
+  for (var i = 0; i < facts.length; i++) {
+    newFacts.push(facts[i] +"!!!");
+  }
+  return newFacts;
+}
+
+function iLoveTheBeatles(n) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!")
+    n++;
+  } while(n<15);
+  return array;
+}
