7a4785db776849bc9721875aa2d5f22cb970195f
Stephanie <s.a.james5@gmail.com>
2018-02-07 22:45:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function theBeatlesPlay(musicians, instruments) 
+  {for (let i = 0; i < 1; i++)
+    return (['John Lennon plays Guitar', 'Paul McCartney plays Bass Guitar', 'George Harrison plays Lead Guitar', 'Ringo Starr plays Drums'])
+  }
+function johnLennonFacts(array)
+  {let i = 0
+  var newArray = [] 
+    while (i < array.length) {
+      newArray.push(array[i] + '!!!')
+      i++
+    }
+    return newArray
+    }
+    
+function iLoveTheBeatles(number) {
+  var array = []
+  do {
+    array.push('I love the Beatles!')
+    number++
+  }
+   while (number < 15)   
+   return array
+}
+   
