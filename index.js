e2406ba8a4985f8cace67d4df241e14c3f00c9b3
David Markel <davidgmarkel@gmail.com>
2018-02-08 01:55:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function theBeatlesPlay(musicians, instruments) {
+let variable = [];
+  for (let i = 0; i < musicians.length; i++) {
+    variable.push(musicians[i] + ' plays ' +  instruments[i])
+  }
+  return variable;
+}
+
+function johnLennonFacts(facts) {
+let variable = [];
+let i = 0;
+  while (i < facts.length) {
+    variable.push(facts[i] + "!!!");
+    i++;
+  }
+  return variable;
+}
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  do {
+    array.push('I love the Beatles!');
+    number++;
+  }
+  while (number < 15);
+  return array;
+}
