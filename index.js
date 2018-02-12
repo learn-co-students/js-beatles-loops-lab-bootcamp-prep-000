553be3843af5e28ae9c3576e2313979b7f616e17
Babisha Manandhar <babisha.manandhar@gmail.com>
2018-02-12 05:50:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function theBeatlesPlay(musicians, instruments) {
+  const array = []
+  
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  const array = []
+  
+  let i = 0;
+  while(i < facts.length) {
+    array.push(`${facts[i]}!!!`)
+    i++
+  }
+  return array
+}
+
+function iLoveTheBeatles(n) {
+  const arr = []
+  
+  do {
+    arr.push('I love the Beatles!')
+    n++
+  }
+  while (n < 15)
+  
+  return arr
+}
