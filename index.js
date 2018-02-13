a83115f4ef76ada5292256681d999b431d887e6f
exclusiveoranges <35941205+exclusiveoranges@users.noreply.github.com>
2018-02-13 11:09:49 -0500

    Update index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr," ]
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums", ]
+
+
+function theBeatlesPlay (musicians, instruments) {
+  function forLoop(musicians, instruments) {
+    for (i=0; i<4; i++) {
+      var product (console.log(`${musicians}, plays, ${instruments}!`)
+    }
+  var emptyarray [] }
+}
