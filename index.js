3cd498224149602ecb1315f11d2363043ecad055
Ryan O'Toole <otoolerj@gmail.com>
2018-02-13 16:15:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr," ]
 var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums", ]
 
@@ -9,3 +10,16 @@ function theBeatlesPlay (musicians, instruments) {
     }
   var emptyarray [] }
 }
+=======
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr," ]
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums", ]
+
+
+function theBeatlesPlay (musicians, instruments) {
+  function forLoop(musicians, instruments) {
+    for (i=0; i<4; i++) {
+      var product (console.log(`${musicians}, plays, ${instruments}!`)
+    }
+  var emptyarray [] }
+}
+>>>>>>> d696aa5892435ab0ac3109638171664ed18a9f16
