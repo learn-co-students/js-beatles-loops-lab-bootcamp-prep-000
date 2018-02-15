c7fe18036b89c38e9658425906c137d793ad02e7
AlexCodeBigBlue <acschwerin@gmail.com>
2018-02-15 19:41:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function theBeatlesPlay(musicians, instruments) {
+  const array = []
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  const awesomeFacts = []
+  let i = 0
+  while (i < facts.length) {
+    awesomeFacts.push(`${facts[i]}!!!`)
+    i++
+  }
+  return awesomeFacts
+}
+
+function iLoveTheBeatles(n) {
+  const array = []
+  do {array.push('I love the Beatles!'); n++}
+  while (n < 15)
+  return array
+}
