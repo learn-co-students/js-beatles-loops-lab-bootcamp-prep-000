7eaec976145c3ea67494de926103b0750dea46e2
Charles Day <daycharles@gmail.com>
2018-02-19 19:29:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < musicians.length; i++) {
+      array.push(`${musicians[i]} plays ${instruments[i]}`);
+
+    }
+  return array;
+}
+
+function johnLennonFacts(facts){
+
+    var newFacts = []
+    var i = 0;
+    while (i < facts.length){
+      newFacts.push(facts[i] + "!!!");
+      i++;
+    }
+    return newFacts;
+}
+
+function iLoveTheBeatles(number){
+  var love = [];
+  do {
+    love.push("I love the Beatles!");
+    number++;
+  } while (number < 15);
+  return love;
+}
