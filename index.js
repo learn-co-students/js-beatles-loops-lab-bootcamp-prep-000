0ac0c02603ca978ce1c2a668d13fb7e44ec2c5f1
jennieboyasun@gmail.com <jennieboyasun@gmail.com>
2018-02-19 23:47:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+function theBeatlesPlay (musicians, instruments) {
+  var newArr = [];
+  for (var i = 0; i < musicians.length; i++){
+    newArr.push(musicians[i] + ' plays ' + instruments[i]);
+  }
+  return newArr;
+}
+
+function johnLennonFacts(array) {
+  while (i < array.length ) {
+    console.log (array[i] + '!!!')
+    i++;
+  }
+}
