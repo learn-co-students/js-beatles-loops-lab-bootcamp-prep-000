3d9947b999f62de415b66a282863db130919f433
Ray <ray@protozo.net>
2018-02-20 05:50:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 function theBeatlesPlay(musicians, instruments)
 {
-  var newArray = [];
+  var newarray = [];
   for (var i = 0; i < musicians.length; i++)
   {
     newarray.push(`${musicians[i]} plays ${instruments[i]}`);
   }
-  return newArray;
+  return newarray;
+}
+
+function johnLennonFacts(facts)
+{
+  var counters = 0;
+  var newfacts = [];
+  while(facts.length > counters)
+  {
+    newfacts.push(`${facts[counters]}!!!`);
+    counters++;
+  }
+  return newfacts;
+}
+
+function iLoveTheBeatles(n)
+{
+  var newarray = [];
+  do
+  {
+    newarray.push(`I love the Beatles!`);
+    n++;
+  }
+  while (n<15);
+  return newarray;
 }
