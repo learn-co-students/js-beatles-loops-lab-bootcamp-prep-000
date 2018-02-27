61e4a9c7f70316f61358851497f43c81ffff28fe
Eduardo Magalhaes <ed83isme@gmail.com>
2018-02-27 17:44:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-function theBeatlesPlay(musicians, instruments) {
-  var arr = [];
-  for (var i = 0; i < musicians.length; i++){
-    arr.push(`${musicians[i]} plays ${instruments[i]}`);
-  }
-  return arr;
-}
-
-function johnLennonFacts(arr) {
-  var i = 0;
-  while (i < arr.length){
-    arr[i] += "!!!";
-    i++;
-  }
-  return arr;
-}
-
-function iLoveTheBeatles(num) {
-  var arr = [];
-  do {
-    arr.push("I love the Beatles!");
-    num++;
-  } while (num < 15)
-  return arr;
-}
+var theBeatlesPlay = (musicians, instruments) => Array(musicians.length).fill(0).map((x,i)=>`${musicians[i]} plays ${instruments[i]}`);
+var johnLennonFacts = arr => arr.map(x=>x+"!!!");
+var iLoveTheBeatles = num => num<=15?Array(15-num).fill("I love the Beatles!"):["I love the Beatles!"];
