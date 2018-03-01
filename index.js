1b7da01510745a9dcf54862e157862d59079c940
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-03-01 15:47:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function theBeatlesPlay(musicians, instruments) {
+  var myArray = [];
+  
+  for (let i = 0; i < musicians.length; i++) {
+    myArray.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  
+  return myArray;
+}
+
+function johnLennonFacts(facts) {
+  for (let i = 0; i < facts.length; i++) {
+    facts[i] += '!!!';
+  }
+  
+  return facts;
+}
+
+function iLoveTheBeatles(number) {
+  var myArray = [];
+  do {
+    myArray.push('I love the Beatles!');
+    number += 1;
+  } while(number < 15);
+  return myArray;
+  
+}
