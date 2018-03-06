3de634f330b58b5f44f0333fc0963796d896be4c
david ritchey <ritchey0713@gmail.com>
2018-03-06 18:44:08 +0000

    Done.

--- index.js
+++ index.js
@@ -12,28 +12,32 @@ function theBeatlesPlay(musicians, instruments){
   
 }  
 
-/*
-function johnLennonFacts(){
   const facts=["He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice"];
+
+function johnLennonFacts(array){
   var newFacts = [];
   var i = 0;
-  while(i < facts.length){
-    newFacts.push(facts[i]+ "!!!")
+  while(i < array.length){
+    array[i]+= "!!!"
     i++;
   }
-  return newFacts;
+  return array;
 }
-*/
-function johnLennonFacts(){
-  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-  var newFacts = [];
-  var i = 0
-  while (i < facts.length) {
-    newFacts.push(facts[i] + "!!!")
-    i++;
+
+function iLoveTheBeatles (n){
+  var beatleLove = [];
+  var i = 0;
+  do{beatleLove.push("I love the Beatles!")
+  i++
   }
-  return newFacts;
+  while(i<=n && n<=15);
+
+return beatleLove;
 }
+
+
+
+
