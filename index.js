8921c86ab8b4172fb6f7bd7ffaab99c07edefa82
david ritchey <ritchey0713@gmail.com>
2018-03-06 04:19:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,10 @@
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+    
+function theBeatlesPlay(musicians, instruments){
+  var newMusicians= []
+  for(var i = 0; i<musicians.length; i++);
+    newMusicians.push(musicians[i] + "plays" + instruments[i]);
+  
+  
+}  
