8c201425293d46d1063a6119999de199afec4219
LeezaLuncheon Luncheon <leezaluncheon@gmail.com>
2018-03-06 07:02:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function theBeatlesPlay(musicians,instruments){
+  var beatlesFacts = []
+  for (let i = 0; i < musicans.length && i < instruments.length; i++){
+    beatlesFacts += `$(musicians[1]) plays $(instruments)`
+    }
+
+}
+
+function johnLennonFacts(array){
+  while (i < array.length) {
+        var johnLennonFacts += array[i]+"!"
+        i--
+    }
+    return johnLennonFacts
+}
+
+function iLoveTheBeatles(number){
+  var howMuch = []
+  do{
+    howMuch+="I love the Beatles!"
+  }  while(number < 15)
+}
