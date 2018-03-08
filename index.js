e66f00bdc142237a0fae57d8673531748e3e1c7f
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-03-08 02:37:51 +0000

    Done.

--- index.js
+++ index.js
@@ -3,10 +3,40 @@ function theBeatlesPlay(musicians, instruments){
   var newarray = []
   
   
-  for 
+  for (var i = 0; i<musicians.length; i++ ){
+  
+  newarray.push(musicians[i]+" plays "+instruments[i]  )
   
   
+  }
   
+  return newarray
+  
+  }
+  
+function johnLennonFacts(facts) {
+  var i = 0
+  while (i<facts.length){
+    
+    facts[i]= facts[i]+"!!!"
+    i++
+    
+    
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(number) {
+  
+  var newarray = []
+  
+  do {
+    newarray.push("I love the Beatles!")
+    number++
+  }while (number<15)
+  
+  
+  return newarray
   
   
 }
