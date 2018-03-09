18a3701f60f94180ffc82927a212aa941631c767
Jamie Catania <catania.jamie@gmail.com>
2018-03-09 05:13:38 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,22 @@ function theBeatlesPlay (musicians, instruments) {
   }
   return arr;
 }
+
+function johnLennonFacts (facts) {
+  let i = 0;
+  while(i < facts.length) {
+    facts[i] += "!!!"
+    i++;
+  }
+  return facts;
+}
+
+function iLoveTheBeatles (num) {
+  let arr = [];
+
+  do {
+    arr.push("I love the Beatles!");
+    num++;
+  } while (num < 15);
+  return arr;
+}
