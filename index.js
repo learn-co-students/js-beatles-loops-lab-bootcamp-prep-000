48907c9e2a0a84e9209d5a559d72d132f8654be9
Shirley Collins <shirley.ednave@gmail.com>
2018-03-13 00:25:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function theBeatlesPlay(musicians, instruments) {
+  var beatlesInstruments = [];
+  for (var i = 0; i < musicians.length; i++) {
+    var tempString = `${musicians[i]} plays ${instruments[i]}`;
+    beatlesInstruments.push(tempString);
+  }
+  return beatlesInstruments;
+}
+
+function johnLennonFacts(facts) {
+ var factsWithExclamations = [];
+ var i = 0;
+ while (i < facts.length) {
+   var tempString = `${facts[i]}!!!`;
+   factsWithExclamations.push(tempString);
+   i += 1;
+ }
+ return factsWithExclamations;
+}
+
+function iLoveTheBeatles(num) {
+  var beatlesLove = [];
+  
+    do {
+      beatlesLove.push("I love the Beatles!");
+      num -= 1;
+    } while (num >= 0 && num < 15);
+
+  return beatlesLove;
+}
