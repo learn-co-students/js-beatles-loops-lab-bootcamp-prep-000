0b6ecb1f8d783f9c57d7544a239126e939125d09
Mendy Wolosow1 <mendywoly+1@gmail.com>
2018-03-13 11:08:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+function theBeatlesPlay(musicians, instruments) {
+  var arr1 = []
+  for(var i = 0; i < musicians.length ; i++) {
+    arr1.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return arr1
+}
+
 function johnLennonFacts(array) {
-  
+ var i = 0
+ var arr2 = []
+  while(i < array.length) {
+    arr2.push(`${array[i]}!!!`)
+    i++
+  }
+  return arr2
+}
+
+function iLoveTheBeatles(num){
+  var arr3 = []
+  do {
+    arr3.push("I love the Beatles!")
+    num ++
+  } while (num < 15)
+  return arr3
 }
