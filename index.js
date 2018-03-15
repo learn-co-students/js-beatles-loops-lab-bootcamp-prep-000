57ace655510a2de217bb93781b8f56b920886942
Amanda Meyer <amandajaymeyer@gmail.com>
2018-03-15 05:37:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-function theBeatlesPlay(arr musicians, arr instruments){
-  var array = {};
-  for (i = 0; i < musicians.length; i++) {
-    push.array(`${musicians} plays ${instruments}`);
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var n = 0; n < musicians.length; n++) {
+    array.push(`${musicians[n]} plays ${instruments[n]}`);
   }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  var array = [];
+  var n = 0;
+  while (n < facts.length) {
+    array.push(`${facts[n]}!!!`);
+    n++;
+  }
+  return array;
+}
+
+function iLoveTheBeatles(n) {
+  var array = [];
+  do {
+    array.push(`I love the Beatles!`);
+    n++;
+  } while (n < 15);
   return array;
 }
