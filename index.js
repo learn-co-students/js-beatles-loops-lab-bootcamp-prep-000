a4561ad2f3390c9a8ad4fa4b28031a4f78aa620d
Tarah Nixon <tarah.sage.nixon@gmail.com>
2018-03-17 01:06:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function theBeatlesPlay(musicians, instruments) {
+  let array = [];
+  
+  for (let i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  
+  let i = 0;
+  while(i < facts.length) {
+    facts[i] = `${facts[i]}!!!`
+    i++;
+  }
+  
+  return facts;
+}
+
+function iLoveTheBeatles(num) {
+  let array = [];
+  
+  do {
+    array.push("I love the Beatles!");
+    num++;
+  } while (num < 15)
+  
+  return array;
+}
