722bc108ff3871306c8245bad9e41509f57f6a69
Ryan Patel <rajanpatel1011@gmail.com>
2018-03-19 01:31:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,10 @@
+function theBeatlesPlay(musicians,instruments){
+  var newArry = [];
+  for(var i; i <musicians.length-1;i++){
+     
+     musicians[i] = `${i}`;
+    newaArry = instruments[1] = `John Lennon plays guitar`;
+    return newArry;
+  }
+}
+
