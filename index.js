6220e5313834ce356fc80a583c4092eb8abdab26
Christopher <christopherccm@msn.com>
2018-03-22 22:01:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay(musicians, instruments) {
+  var artistsArray = new Array();
+  var musicianPlays = " plays "
+
+  for (let i = 0; i <= musicians.length; i++) {
+    artistsArray.push(musicians[i] + musicianPlays + instruments[i]);
+  }
+  return artistsArray;
+}
