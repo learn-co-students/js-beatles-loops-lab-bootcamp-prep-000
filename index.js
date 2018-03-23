7228b9bcf9fee237248f3bbb4bd7e212901e8c55
Brian Feibus <brianfeibus@gmail.com>
2018-03-23 22:45:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('test')
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
+var instruments = ["guitar", "bass guitar", "lead guitar", "drums"]
+
+function theBeatlesPlay(musicians, instruments){
+  var empty=  [];
+    for (var i =0; i < musicians.length; i++){
+    empty.push (musicians[i] + " plays " + instruments[i]);
+  }
+      return empty;
+  }
+
+  const facts = [
+    "He was the last Beatle to learn to drive",
+    "He was never a vegetarian",
+    "He was a choir boy and boy scout",
+    "He died in 1980"
+  ];
+
+
+  function johnLennonFacts(facts) {
+    var result = [];
+    var x = 0;
+    while (x < facts.length) {
+      result.push(facts[x] + "!!!");
+      x++}
+    return result
+  }
+
+function iLoveTheBeatles(number){
+  var empty= []
+    do {
+      empty.push ("I love the Beatles!")
+        number++
+          } while (number<15);
+            return empty
+          }
