4ad19e7d6ea17392fccf375391e8a1a132c8e8f2
Eero Saarinen <eiusaarinen@gmail.com>
2018-03-25 09:41:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 function theBeatlesPlay (musicians, instruments) {
   var musiciansWithIstruments = []
   
-  for (i = 0; i < musicians.length(); i++) {
+  for (let i = 0; i < musicians.length; i++) {
     let statement = musicians[i] + ' plays ' + instruments[i]
     musiciansWithIstruments.push(statement)
   }
   
   return musiciansWithIstruments
+}
+
+function johnLennonFacts (array) {
+  var newArray = []
+  let i = 0
+  
+  while (newArray.length < array.length) {
+    newArray[i] = array[i] + '!!!'
+    i++
+  }
+  
+  return newArray
+}
+
+function iLoveTheBeatles (number) {
+  var array = []
+  
+  do {
+    array.push('I love the Beatles!')
+    number++
+  } while (number < 15)
+  
+  return array
 }
