af24db4a16068f3376aca19f571180818f98346d
Monica Seaman <monica.l.seaman@gmail.com>
2018-03-25 01:32:32 +0000

    Done.

--- index.js
+++ index.js
@@ -14,3 +14,12 @@ function johnLennonFacts(array) {
   }
   return newArray;
 }
+
+function iLoveTheBeatles(n) {
+  var newArray = [];
+  do {
+    newArray.push("I love the Beatles!");
+    n++;
+  } while (n < 15);
+  return newArray;
+}
