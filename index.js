b07042b8343be5b1a31bee07ae5a2d1a4fe42714
Czarek <vancarek@gmail.com>
2018-03-27 20:38:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function theBeatlesPlay(musicians, instruments) {
+  let array = [];
+  
+  for(let i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  let i = 0;
+  while(i < facts.length) {
+    facts[i] = facts[i] + "!!!"; 
+    i++;
+  }
+  
+  return facts;
+}
+
+function iLoveTheBeatles(n) {
+  let array = [];
+  
+  do {
+    array.push('I love the Beatles!');
+    n++;
+  } while (n < 15);
+  
+  return array;
+}
