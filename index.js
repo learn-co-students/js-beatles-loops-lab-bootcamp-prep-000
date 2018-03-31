445eb785561bf18616687d147679552c57b470b2
Nasser Issa <nasissa97@gmail.com>
2018-03-31 01:02:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 function theBeatlesPlay(muscians, instruments){
   var array = [];
   for (var i = 0; i < muscians.length; i++){
-    array.push(muscians[i] + ' play ' + instruments[i])
+    array.push(muscians[i] + ' plays ' + instruments[i])
   }
+  return array;
+}
+
+function johnLennonFacts(array){
+  var i   = 0;
+  var val = [];
+      
+  while(i < array.length){
+    val.push(array[i] + "!!!");
+    i++;
+  }
+  return val;
+}
+
+function iLoveTheBeatles(num){
+  var array = [];
+  do {
+    array.push("I love the Beatles!")
+    num++
+  } while(num < 15 )
   return array;
 }
