4ae860775c02fe4cfcbc5aad89e38179ddda8d91
Nasser Issa <nasissa97@gmail.com>
2018-03-31 00:31:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function theBeatlesPlay(muscians, instruments){
-  var emp = [];
-  for (var i = 0; i < muscians; i++){
-    emp.push(muscians, instruments)
+  var array = [];
+  for (var i = 0; i < muscians.length; i++){
+    array.push(muscians[i] + ' play ' + instruments[i])
   }
+  return array;
 }
