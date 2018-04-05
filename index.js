6ed55a7ccf35bfb373f2ee0d795a5a0f05c26504
Rob LeCheminant <lecheminant@gmail.com>
2018-04-05 21:18:18 +0000

    Done.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function theBeatlesPlay(musicians, instruments){
 
 function johnLennonFacts (facts){
   let i = 0;
-  while(i < 4){
+  while(i < facts.length){
     facts[i] = facts[i] + '!!!';
     i++;
   }
