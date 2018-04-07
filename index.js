7ef5c40112d496c49d1953adcf7fe98d15b52cd3
Ernesto ballon <ernestoballono@gmail.com>
2018-04-07 22:30:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-function theBeatlesPlay(m,in){
-  var a = []
-  for(let i = 0; i<m.length;i++){
-    a[i] = '${m[i]} plays $in[i]'
+function theBeatlesPlay(musicians,instruments){
+  var array = [];
+  for(var i = 0; i < musicians.length; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array;
+}
+function johnLennonFacts(a){
+  var x=true;
+  var y = a.length-1;
+  while(x){
+    a[y]=a[y]+'!!!'
+    if(y===0) (x = false);
+    y--;
   }
   return a
 }
+function iLoveTheBeatles(n){
+  var empty = [];
+  do{
+    empty.push("I love the Beatles!")
+    n++;
+    
+  }while(n<15)
+  return empty;
+  
+}
+
+
+
+
+
