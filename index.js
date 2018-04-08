6791b80225f7525854a8cbec15caf4b753a9a4a7
Marc Jean-Gilles <marcjeangilles@gmail.com>
2018-04-08 21:09:19 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay(musicians, instruments){
+  var array = [];
+  for (var i = 0; i <4; i++){
+    array.push(musicians[i] + "plays" + instruments[i]);
+  }
+  return array;
+}
+
+const facts = ["He was the last Beatle to learn to drive",
+        "He was never a vegetarian",
+        "He was a choir boy and boy scout",
+        "He hated the sound of his own voice"];
+
+function johnLennonFacts(facts){
+  while(i <4){
+  johnLennonFacts.push(facts[i]+ "!!!");
+}
+return johnLennonfacts;
+}
+
+function iLoveTheBeatles(n){
+  var emptystart = [];
+  do {
+    emptystart.push("I love the Beatles!");
+  } while (n<15, n++);
+  return emptystart;
+}
