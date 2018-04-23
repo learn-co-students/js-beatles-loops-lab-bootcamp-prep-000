f7c558c519ac0f1d16dcc43fb27d90040fe0af39
Marcus Cheung <myc2131@tc.columbia.edu>
2018-04-23 07:49:43 +0000

    Done.

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ function theBeatlesPlay(musicians, instruments){
   for (var i = 0; i < musicians.length; i++){
     array.push(musicians[i] + ' plays ' + instruments[i]);
   }
-  return array
+  return array;
 }
 
-function johnLennonFacts(newFacts){
-  for (var i = 0; i < newFacts.length; i++){
-    newFacts[i] = newFacts[i] + '!!!'  
+function johnLennonFacts(facts){
+  var newFacts = [];
+  var i = 0;
+  while (i < facts.length){
+    newFacts.push(facts[i] + '!!!');
+    i++;
   }
-  return newFacts
+  return newFacts;
 }
 
 function iLoveTheBeatles(number){
