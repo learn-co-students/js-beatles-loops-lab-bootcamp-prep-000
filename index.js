ba8d7f09c30f5c31f6cffeaa421c073807bd0f2f
Miyuki Abe <miyuki0809@gmail.com>
2018-04-25 02:10:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+var musicians = ['John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Star']
+
+var instruments = ['Guitar', 'Bass Guitar', 'Lead Guitar', 'Drums']
+
+function theBeatlesPlay(musicians, instruments) {
+  var theBeatlesPlay = []
+  for (var i = 0; i < musicians.length ; i++) {
+  theBeatlesPlay.push(musicians[i] + ' plays ' + instruments[i])
+   }
+  return theBeatlesPlay
+}
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  var johnLennonFacts = []
+  while (facts.length > i) {
+  johnLennonFacts.push(facts[i] + "!!!")
+  i++
+}
+   return johnLennonFacts
+}
+ 
+var n =0;
+ function iLoveTheBeatles (n) {
+   var iLoveTheBeatles = []
+   do {
+     iLoveTheBeatles.push("I love the Beatles!");
+     n++;
+   } while (n < 15)
+   return iLoveTheBeatles
+ }
+ 
+
