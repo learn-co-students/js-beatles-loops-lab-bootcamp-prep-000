1dc9f0a0028c9db202830f2f2ef5939510a71afb
Jed Peek <jedpeek@yahoo.com>
2018-04-26 20:37:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 function theBeatlesPlay(musicians, instruments){
   var arr = [];
   for(let i = 0; i < musicians.length; i++){
-     arr.push(musicians[i])
+     arr.push(musicians[i] +" plays "+ instruments[i])
   }
   return arr
+}
+
+function johnLennonFacts(array){
+  var arr = [];
+  let i = array.length -1
+  while(i >= 0){
+    arr.push(array[i] + "!!!");
+    --i;
+  }
+  arr.reverse()
+  return arr;
+}
+
+function iLoveTheBeatles(n){
+  var arr = [];
+  do{
+    arr.push("I love the Beatles!");
+    n++;
+  }
+  while(n < 15);
+  return arr;
 }
