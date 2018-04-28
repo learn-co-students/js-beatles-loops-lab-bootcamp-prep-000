f93754a3803af4559c1d067726b047cee84d2014
Sammy Akharaz <ketrixz@gmail.com>
2018-04-28 23:11:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function theBeatlesPlay(musicians, instruments) {
+  let arr = []
+  for (let i = 0; i < musicians.length; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return arr
+}
+function johnLennonFacts(facts){
+  let i = 0
+  let arr = []
+  while (i<facts.length){
+     arr.push(`${facts[i]}!!!`)
+     i++
+  }
+  return arr
+}
+function iLoveTheBeatles (num) {
+  let arr = []
+  do{
+    arr.push("I love the Beatles!")
+    num++
+  }while(num<15)
+  return arr
+}
