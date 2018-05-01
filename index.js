20895f4ecb4583a5488cbe92ebe5a306db79bea5
Riley Egan <rilegan@mac.com>
2018-05-01 23:53:32 +0000

    Done.

--- index.js
+++ index.js
@@ -11,10 +11,11 @@ function theBeatlesPlay(musicians, instruments) {
 
 function johnLennonFacts(facts) {
   var newArr = [];
-  
   var i = 0;
-  while(i < facts.length - 1) {
-    newArr.push(`${facts[i]}!!!`);
+  
+  while(i < facts.length) {
+    newArr.push(facts[i] + "!!!");
+    i++;
   }
   
   return newArr;
@@ -25,10 +26,8 @@ function iLoveTheBeatles(num) {
   
   do {
     newArr.push("I love the Beatles!");
-  } while (num < 15) {
-    newArr.push("I love the Beatles!");
     num++;
-  }
+  } while (num < 15);
   
   return newArr;
 }
