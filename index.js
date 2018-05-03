7d934761bd580d1a234c22b59358ab32bb62edc4
rafaelalfinito@hotmail.com <rafaelalfinito@hotmail.com>
2018-05-03 22:50:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 function theBeatlesPlay(arrMusicians, arrInstruments){
   var arr = []
   
-  instrumernt = arrInstruments[0]
-  for(var i = 0; i < arrMusicians.length, i++){
-    arr.push(arrMusicians[i] + instrumernt)
-    console.log(arr)
+  for(var i = 0; i < arrMusicians.length; i++){
+    arr.push(arrMusicians[i] + " plays " + arrInstruments[i])
   }
+  return arr
+}
+
+
+function johnLennonFacts(arrFacts){
+  var arr = []
+  var i = 0
+  while(i < arrFacts.length){
+    arr.push(arrFacts[i] + "!!!")
+    i++
+  }
+  
+  return arr
+  
+}
+
+
+function iLoveTheBeatles(number){
+  var arr = []
+  do{
+    arr.push("I love the Beatles!")
+    number++
+  }while(number<15)
+  
   return arr
 }
