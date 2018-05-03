6d42f124d506f81abb69a8f383f08806478940cc
jmoypace <jmoypace@gmail.com>
2018-05-03 18:42:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function theBeatlesPlay(arrMusic, arrInstru){
+  
+  var emptyarr=[];
+ 
+  for(let i=0; i<arrMusic.length; i++){
+    var phrase=`${arrMusic[i]} plays ${arrInstru[i]}`;
+    emptyarr[i]=(phrase);
+  }
+  return emptyarr;
+}
+function johnLennonFacts(jonfact){
+  var jonlen=jonfact.length-1;
+  while(jonlen>=0){
+    jonfact[jonlen]=jonfact[jonlen]+"!!!"
+    console.log(jonfact[jonlen])
+    jonlen--;
+  }
+  return jonfact
+}
+function iLoveTheBeatles(num){
+  var emp=[];
+  do{
+    emp.push("I love the Beatles!")
+    num++;
+  }
+  while(num<15);
+  return emp;
+}
