725ce00fa21269ea449f7dbbe0de43f17f3465a7
Emily Jennings <emily.jennings@flatironschool.com>
2018-05-12 03:06:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+musicians = ["John", "Ringo", "Paul", "George"]
+instruments = ["guitar", "bass", "guitar", "drums"]
+
+function theBeatlesPlay(musicians, instruments) {
+  array = [];
+  for (i = 0, i < 3, i++) {
+    if (instanceof <= 4) {
+      var string = (musicians.[i] + "plays the " + instruments[i])
+      array.push(string)
+    }
+  }
+}
