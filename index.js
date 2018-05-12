21736ca20a00362e44c17398e7edd4c49be4dbdf
Emily Jennings <emily.jennings@flatironschool.com>
2018-05-12 03:40:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-musicians = ["John", "Ringo", "Paul", "George"]
-instruments = ["guitar", "bass", "guitar", "drums"]
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
 
 function theBeatlesPlay(musicians, instruments) {
-  array = [];
-  for (i = 0, i < 3, i++) {
-    if (instanceof <= 4) {
-      var string = (musicians.[i] + "plays the " + instruments[i])
-      array.push(string)
-    }
+  var array = [];
+  for (let i = 0; i < musicians.length; i++) {
+      array.push(musicians[i] + " plays " + instruments[i])
   }
+  return array;
 }
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(facts) {
+  var factsPlus = []
+  var i = 0
+  while (facts.length > factsPlus.length) {
+    factsPlus.push(facts[i] + "!!!")
+    i++
+  }
+  return factsPlus
+}
+
