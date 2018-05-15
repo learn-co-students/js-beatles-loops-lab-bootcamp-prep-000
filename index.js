0a493f3cbc4d89f11df1561fa425536e4f6f56c6
eatingfoil@gmail.com <eatingfoil@gmail.com>
2018-05-15 02:06:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function theBeatlesPlay(musicians, instruments) {
+  var arr = [];
+  for (let i = 0; i < musicians.length; i++) {
+    var str = `${musicians[i]} plays ${instruments[i]}`;
+    arr.push(str);
+  }
+  return arr
+}
+
+function johnLennonFacts(facts) {
+  let i = 0;
+  var arr = [];
+  while (i < facts.length) {
+    var str = `${facts[i]}!!!`;
+    arr.push(str);
+    ++i
+  }
+  return arr
+}
+
+function iLoveTheBeatles (n) {
+  var arr = [];
+  do {
+    arr.push("I love the Beatles!");
+    n++;
+  } while (n < 15);
+  return arr
+}
