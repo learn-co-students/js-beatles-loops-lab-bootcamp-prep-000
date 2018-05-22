90844d6f1b4a920adee5ee600a67c8f60d018cdc
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-05-22 16:49:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-function theBeatlesPlay()
+function theBeatlesPlay(musicians,  instruments){
+  let musiciansAndInstruments = []
+  for (let i= 0; i < musicians.length; i++){
+    musiciansAndInstruments.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return musiciansAndInstruments
+}
+
+function johnLennonFacts(facts){
+  let i = 0
+  let newArray = []
+  while(i < facts.length){
+    newArray.push(facts[i] + "!!!")
+    i++
+  }return newArray
+}
+
+
+function iLoveTheBeatles(num){
+  let secondArray = []
+  do{secondArray.push("I love the Beatles!")
+      num++
+  } while(num < 15)
+  return secondArray;
+}
+
