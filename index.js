b1f5733de05f9c5cde4c3f2d1e46536c59620809
quip550@gmail.com Chi <quip550@gmail.com>
2018-05-22 01:51:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,44 @@
+function theBeatlesPlay(musicians, instruments) {
+  var theBeatles = [];
+  for(var i = 0; i<musicians.length; i+=1) {
+    theBeatles.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return theBeatles;
+}
+
+//test function
+console.log(theBeatlesPlay(['a','b'],['1','2']));
+
+function johnLennonFacts(array){
+  for(var i = 0; i<array.length; i+=1) {
+    array[i]=array[i] + '!!!';
+  }
+  return array;
+}
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+//test function
+johnLennonFacts(facts);
+console.log(facts);
+
+function iLoveTheBeatles(n) {
+  var y = n+1
+  var storage = [];
+  if(n<15) {
+  do {
+    storage.push('I love the Beatles!')
+  } while (storage.length<y)
+  }
+  else {
+    storage.push('I love the Beatles!')
+  }
+  return storage
+}
+
+console.log(iLoveTheBeatles(3))
