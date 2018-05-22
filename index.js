ee79265763bb839a64a56188fcc703cdfa35d569
Cassandra Corrales <cmcorrales3@gmail.com>
2018-05-22 00:50:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 function theBeatlesPlay(musicians, instruments) {
-  var array = []
-  for(var i=0; i < musicians.length; i++) {
-    array.push(musicians[i]+' plays '+instruments[i])
+  let array = []
+  for(let i=0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   return array;
 }
 
 function johnLennonFacts(facts) {
-   var results = [];
-   var count = 0;
+   let results = [];
+   let count = 0;
    while (count < facts.length) {
-     results.push(facts[count] + "!!!");
+     results.push(`${facts[count]}!!!`);
      count++;
    }
    return results;
  }
  
  function iLoveTheBeatles(num) {
-     var love = [];
+     let love = [];
      do {
          love.push("I love the Beatles!");
          num++;
