42040a87b9e9521968dcf099f675480f12a177b4
ralphcuello1016@gmail.com <ralphcuello1016@gmail.com>
2018-05-24 04:06:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function theBeatlesPlay (musicians, instruments) {
+  var array = [];
+  for ( var i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array;
+}
+
+function johnLennonFacts(array) {
+  const facts = [];
+  let i = 0;
+ while (i < array.length) {
+   facts.push(`${array[i]}!!!`)
+   i++;
+   }
+ return facts;
+}
+
+function iLoveTheBeatles (n) {
+  var ten = [];
+  do {
+ten.push("I love the Beatles!");
+n++
+  } while (n < 15 )
+  
+ return ten;
+  }
