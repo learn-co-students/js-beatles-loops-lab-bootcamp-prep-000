d95329984b2881be0c1c293614c70e53b632d72e
colin.jeske@gmail.com <colin.jeske@gmail.com>
2018-05-26 01:01:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function theBeatlesPlay (musicians, instruments) {
+  var newArray = [];
+  
+  for (var i=0; i < musicians.length; i++) {
+    var newString = musicians[i] + " plays " + instruments[i];
+    newArray = [...newArray, newString];
+  }
+  
+  return newArray;
+}
+
+function johnLennonFacts (facts) {
+  var newArray = [];
+  var i = 0;
+  while (i < facts.length) {
+    newArray = [...newArray, facts[i] + "!!!"];
+    i++;
+  }
+  
+  return newArray;
+}
+
+function iLoveTheBeatles (num) {
+  var newArray = [];
+  do {
+    newArray = [...newArray, "I love the Beatles!"];
+    num++;
+  } while (num < 15);
+  return newArray;
+}
