3c4221274d0a2d3e713bd020c991bbb42d171ee8
Mahwish Riaz-Jamil <mahwishcee@gmail.com>
2018-06-05 23:48:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 function theBeatlesPlay(musicians, instruments){
   var emptyArray = [];
 
-  emptyArray[0] = `${musicians[0]} play ${instruments[0]}`;
+  emptyArray.push(`${musicians[0]} plays ${instruments[0]}`);
   
-  for (int i = 1; i<musicians.length(); i++){
+  for (let i = 1; i<musicians.length; i++){
     emptyArray[i] = `${musicians[i]} plays ${instruments[i]}`
   }
   
   return emptyArray;
 }
 
-function johnLennonFacts(){
-  
+function johnLennonFacts(facts){
+  var myArray = [];
+  var counter = 0;
+  while (facts.length > myArray.length){
+    myArray.push(facts[counter] + "!!!"); 
+    counter++;
+  }
+  return myArray;
 }
 
+function iLoveTheBeatles(number){
+  var emptyArray = [];
+  do{
+    emptyArray.push("I love the Beatles!");
+    number++;
+  }
+  while(number < 15);
+  
+  return emptyArray;
+}
 
   // for (int i = 0; i < musicians.length(); i++){
   //   for (int i = 0; i < instruments.length(); i++){
