25a5cb778539d879e496ac6bcc1016809288d187
Corey H <c.l.halicke@gmail.com>
2018-06-05 15:44:41 +0000

    Done.

--- index.js
+++ index.js
@@ -32,20 +32,54 @@ function theBeatlesPlay(musicians, instruments) {
 
 //DONE Create a function johnLennonFacts. 
 
-//This function will accept one argument, an array of facts about John Lennon (note that it might not be exactly the following facts):
+//DONE This function will accept one argument, an array of facts about John Lennon (note that it might not be exactly the following facts):
 
-//Use a while loop to loop over the facts array and add "!!!" to the end of every fact. 
+//DONE Use a while loop to loop over the facts array and add "!!!" to the end of every fact. 
 
-//The function should return an array of strings with exclamation points
+//DONE The function should return an array of strings with exclamation points
 
 
 
 function johnLennonFacts(facts) {
+  let i = 0
   while (i < facts.length) {
     facts[i] = facts[i] + '!!!'
+    i++
   }
   return facts
 }
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+//DONE Create a function iLoveTheBeatles which accepts a number as a parameter. 
+
+//DONE The body of the function should create a variable that stores an empty array. 
+
+//DONE Then, implement a do-while loop inside the function that adds "I love the Beatles!" to the empty array. 
+
+//DONE Then the loop should increment the number passed in as a parameter.
+
+//DONE The condition of the loop should check to see that the parameter number is less than 15. 
+
+//DONE The function should return the array with the strings "I love the Beatles!".
+
+
+
+
+
+function iLoveTheBeatles(number) {
+  var empty = []
+  do {
+    empty.push("I love the Beatles!")
+    number++
+  } while (number < 15)
+  return empty
+}
+
+
+
 
 
 
