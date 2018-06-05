45473954bb6c93cb08fa1306f48858a778ce5112
Mahwish Riaz-Jamil <mahwishcee@gmail.com>
2018-06-05 23:06:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function theBeatlesPlay(musicians, instruments){
+  var emptyArray = [];
+
+  emptyArray[0] = `${musicians[0]} play ${instruments[0]}`;
+  
+  for (int i = 1; i<musicians.length(); i++){
+    emptyArray[i] = `${musicians[i]} plays ${instruments[i]}`
+  }
+  
+  return emptyArray;
+}
+
+
+
+  // for (int i = 0; i < musicians.length(); i++){
+  //   for (int i = 0; i < instruments.length(); i++){
+  //   console.log(`${musicians[i]} plays ${instruments[i]}`);
+  // }
+  // }
