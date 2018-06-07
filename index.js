1c7b47ec737c1fcd809eb368132d00191b7a4f0e
Jeff <dr.tek90@gmail.com>
2018-06-07 18:27:36 +0000

    Done.

--- index.js
+++ index.js
@@ -7,8 +7,20 @@ function theBeatlesPlay(music, instrum){
 }
 
 function johnLennonFacts(array){
-  for (var i=0;i<array.length;i++){
-    array[i]=array[i]+"!";
+  var i=0
+  while (i<array.length){
+    array[i]=array[i]+"!!!";
+    i++;
   }
   return array
+}
+
+function iLoveTheBeatles(n){
+  var array=[];
+  do {
+    array = [...array,'I love the Beatles!'];
+    n++;
+  }
+    while(n<15);
+    return array;
 }
