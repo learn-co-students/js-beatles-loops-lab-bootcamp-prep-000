3a504b27548d2a8d471f920d856d003e3462e09d
cdapaaaddo@gmail.com <cdapaaaddo@gmail.com>
2018-06-08 00:27:33 +0000

    Done.

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles(n) {
   var arr = []
   do {
-    arr.push("I love the Beatles!") * n++
+    arr.push("I love the Beatles!")
+    n++
   } while (n < 15)
   return arr
 }
