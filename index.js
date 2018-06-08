6d489b91bbc726e8fd5e80767755b62c1570b26c
cdapaaaddo@gmail.com <cdapaaaddo@gmail.com>
2018-06-08 00:16:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theBeatlesPlay(musicians, instruments) {
+  var emptyarr = []
+  for (let i = 0; i < musicians.length ; i++) {
+    emptyarr.push(`${musicians[i]} plays ${instruments[i]}`)
+  } return emptyarr
+}
+
+function johnLennonFacts(facts) {
+  var factstr = []
+  let i = 0
+  while (i < facts.length) {
+    factstr.push(`${facts[i]}!!!`)
+    i++
+  } return factstr
+}
+
+function iLoveTheBeatles(n) {
+  var arr = []
+  do {
+    arr.push("I love the Beatles!") * n++
+  } while (n < 15)
+  return arr
+}
