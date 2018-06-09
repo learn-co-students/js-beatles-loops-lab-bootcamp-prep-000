98b71ce5399c655bdc91492c99f49f062dfa2bff
Issouf zabre <sheridanzabre@gmail.com>
2018-06-09 02:14:23 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+ function theBeatlesPlay(musicians,instruments){
+  var emptyArray= []
+  for (var i=0;i<musicians.length;i++){
+  emptyArray.push( `${musicians[i]} plays ${instruments[i]}`)
+}
+return emptyArray
+  }
+  
+  function johnLennonFacts(Array){
+    while (Array.length>0){
+      Array.push('!!!')
+    }
+    return Array
+  }
+  
