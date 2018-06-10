0d67077676faeca55ed98877ca24d4d27aa7a243
Anthony Udezi <tonywaka@uniben.edu>
2018-06-10 00:16:54 +0000

    Done.

--- index.js
+++ index.js
@@ -3,40 +3,34 @@ var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Star
 var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
 function theBeatlesPlay(array1,array2) {
   var string = []
-  for (i = 0; i < (array1.length); i++) {
-     string = string.concat(`${array1[i]}  plays  ${array2[i]}`)
+  for (let i = 0; i < (array1.length); i++) {
+     string = string.concat(`${array1[i]} plays ${array2[i]}`)
   }
-  console.log(string)
+  return (string)
 }
 theBeatlesPlay(musicians,instruments)
 
 
-facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
+facts = ["He was the last Beatle to learn to drive",
+  "He was never a vegetarian","He was a choir boy and boy scout",
+  "He hated the sound of his own voice"];
 function johnLennonFacts(johnLennonFacts) {
-  fact = []
-  i = johnLennonFacts.length - 1
-  while (i > -1) {
-      fact = fact.concat(`${johnLennonFacts[i]} !!!`)
-      --i
+  let fact = []
+  let i = 0
+  while (i < johnLennonFacts.length) {
+   fact = fact.concat(`${johnLennonFacts[i]}!!!`)
+    i++
   }
-  console.log(fact)
+  return (fact)
 }
 johnLennonFacts(facts)
 
 function iLoveTheBeatles(numb) {
-   array1 = []
-   function maybeTrue() {
-   return array1.length < numb
-}
-
+   const array1 = []
    do {
    array1 = array1.concat("I love the Beatles!")
- } while (maybeTrue());
-return array1
+   numb++
+ } while (array1.length < numb);
+ return array1
 }
-iLoveTheBeatles(17)
+iLoveTheBeatles(15)
