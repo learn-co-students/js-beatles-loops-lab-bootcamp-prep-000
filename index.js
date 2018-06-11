24abbabab5b578f4784ba84c9ae71a00c5c118bb
robert.zacchia@gmail.com <robert.zacchia@gmail.com>
2018-06-11 19:42:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-t
+function theBeatlesPlay (musicians, instruments)
+{
+  const array = []
+  
+  for (let i = 0, l = musicians.length; i < l; i++) 
+  {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  
+return array;
+}
+
+function johnLennonFacts (facts)
+{
+  const bigfacts = []
+  
+ let i = 0;
+    
+    while (i < facts.length)
+    {
+      bigfacts.push(`${facts[i]}!!!`)
+      i++
+    }
+  
+  return bigfacts;
+}
+
+function iLoveTheBeatles (n)
+
+{
+  var love = [];
+  
+  do{
+    love.push("I love the Beatles!")
+    n++
+  }
+  while(n < 15)
+
+  
+  return love;
+}
