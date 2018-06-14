4d6297d30efc2a3f008201b94b5aa1ac518ea4c0
bilojan91@gmial.com <bilojan91@gmial.com>
2018-06-14 20:32:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+
+function araa(){
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+for (var b=0;b<4;b++){
+  
+  console.log(facts[b]);
+  
+}}
