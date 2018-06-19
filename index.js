aeccb529ad13c29d445aa33fc47447dc09b74041
Lyssa Treibitz <cerylean@gmail.com>
2018-06-19 16:18:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+
+function theBeatlesPlay (musician, instruments) 
+{
+  var newMusic = []
+ for ( let i = 0; i < musician.length; i++) 
+ {
+ newMusic.push(`${musician[i]} plays ${instruments[i]}`) 
+ } 
+ return newMusic
+  }
+  
+  function johnLennonFacts (facts) {
+    var i = 0
+    while (i < facts.length) {
+      (facts[i] = `${facts[i]}!!!`)
+      i++
+    } 
+    return facts
+  }
+  
+  
+  function iLoveTheBeatles (number) {
+    var array = []
+    do {
+      array.push("I love the Beatles!")
+      number++ } 
+      while (number < 15)
+    
+  return array}
+  
+  
+  
+  
