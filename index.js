1555b1e3e9bfc80b8cb14328e722cc1644808fba
Rosa Tu <rosalina.tu@gmail.com>
2018-06-19 17:46:16 +0000

    Done.

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ function theBeatlesPlay(musicians, instruments){
   
 var  playlist= []
 
-for(let i = 0; i < 3; i++){
- 
- playlist.push('musicians[${i}] plays instruments[${i}]') 
-}   
+for(let i = 0; i < 3; i++)
+{
+ if(i ===0){
+ playlist.push("John Lennon plays guitar")}
+ else if (i === 1){
+    playlist.push("Paul McCartney plays Bass guitar")}
+  else if (i === 2) {
+     playlist.push("George Harrison plays Lead guitar")
+  else if (i === 3 ) {  
+    playlist.push("Ringo Starr plays drums")
+  }
+   
+
 return playlist
 }
