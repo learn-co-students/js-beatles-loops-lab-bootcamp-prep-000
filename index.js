36558eefb9a6ce08081eb7be3dd9dca35a75644a
Rosa Tu <rosalina.tu@gmail.com>
2018-06-19 17:08:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+var musicians = []
+var instruments = []
+function theBeatlesPlay(musicians, instruments){
+  
+var  playlist= []
+
+for(let i=(musicians[0], instruments[0]);();musicians++, instruments++;){
+  console.log{"${musicians[x]} plays ${instruments[x]}"}
+}
+
+
+  
+}
