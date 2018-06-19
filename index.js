a13f4fe5d629c25311842136f0bb955936b57c4d
Rosa Tu <rosalina.tu@gmail.com>
2018-06-19 17:36:19 +0000

    Done.

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ function theBeatlesPlay(musicians, instruments){
 var  playlist= []
 
 for(let i = 0; i < 3; i++){
-  
-console.log("musicians$[i] plays instruments$[i]") 
+ 
+ playlist.push(musicians[i] 'plays' instruments[i]) 
 }   
 
 return playlist
