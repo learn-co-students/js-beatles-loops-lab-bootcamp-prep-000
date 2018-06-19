bf34f372634fb5a241dbd90911c380388f07458c
Rosa Tu <rosalina.tu@gmail.com>
2018-06-19 17:48:46 +0000

    Done.

--- index.js
+++ index.js
@@ -5,16 +5,8 @@ var  playlist= []
 
 for(let i = 0; i < 3; i++)
 {
- if(i ===0){
- playlist.push("John Lennon plays guitar")}
- else if (i === 1){
-    playlist.push("Paul McCartney plays Bass guitar")}
-  else if (i === 2) {
-     playlist.push("George Harrison plays Lead guitar")
-  else if (i === 3 ) {  
-    playlist.push("Ringo Starr plays drums")
+ playlist.push(musicians[i]'plays'instruments[i])}
   }
    
-
 return playlist
 }
