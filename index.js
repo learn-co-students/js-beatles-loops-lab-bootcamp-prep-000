e4b18f6dbc60c10aa8b0bd831724bf3ceea3bd2a
Rosa Tu <rosalina.tu@gmail.com>
2018-06-19 17:52:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-
+function() {
+    const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+    const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
+}
+    
 function theBeatlesPlay(musicians, instruments){
   
 var  playlist= []
