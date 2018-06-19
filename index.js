0fb43840e8a24aaa47f71b5dcce09e11e0256c8c
Rosa Tu <rosalina.tu@gmail.com>
2018-06-19 17:55:05 +0000

    Done.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var  playlist= []
 
 for(let i = 0; i < 3; i++)
 {
-  console.log("${musicians[i]} plays ${instruments[i]}")
+  console.log("musicians[i] plays instruments[i]")
  playlist.push()
   }
    
