16f3aa46c2a475d353f03c5b07522c5d888875ef
Rosa Tu <rosalina.tu@gmail.com>
2018-06-19 17:29:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-var musicians = []
-var instruments = []
+
 function theBeatlesPlay(musicians, instruments){
   
 var  playlist= []
 
-for(let i=(musicians[0], instruments[0]);();musicians++, instruments++;){
-  console.log{"${musicians[x]} plays ${instruments[x]}"}
-}
-
-
+for(let i = 0; i < 3; i++){
   
+}
+playlist.push("${musicians[i]} plays ${instruments[i]}")
+
+return playlist
 }
