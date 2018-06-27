b9edb20d1175a10feebbc505f9f94aa975dc973b
Hugo Escandon <hescandon6@gmail.com>
2018-06-27 15:55:55 +0000

    Done.

--- index.js
+++ index.js
@@ -8,14 +8,9 @@ function theBeatlesPlay(musicians, instruments) {
 
 
 }
-function johnLennonFacts() {
-  var facts = [
-      "He was the last Beatle to learn to drive",
-      "He was never a vegetarian",
-      "He was a choir boy and boy scout",
-      "He hated the sound of his own voice"
-    ];
-    var Facts1 = []
+function johnLennonFacts(facts) {
+
+  const Facts1 = []
    var i = 0
     while (i < facts.length) {
         Facts1.push(facts[i] +"!!!")
