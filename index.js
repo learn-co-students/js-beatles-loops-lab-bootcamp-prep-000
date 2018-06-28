faa5c3d736d894be4df2a95c32b6e7f788b048c8
Charity Jackson <charitylennjackson@yahoo.com>
2018-06-28 21:12:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+
+
+
+function theBeatlesPlay(musicians,instruments){
+  const myArray = [];
+  
+  for(let i=0, l=musicians.length; i<1; i++){
+    
+    myArray.push('${musicians[i]} plays ${instruments[i]}');
+  } 
+  return myArray;
+}
+
+function johnLennonFacts(facts){
+  const shoutedFacts = [];
+  
+  let i = 0;
+    while(i<facts.length){
+      shoutedFcts.push('${facts[i]}!!!');
+      i++;
+    }
+    return shoutedFacts;
+}
+
+function iLoveTheBeatles(n){
+  const arr = [];
+  
+  do{
+    arr.push('I love the Beatles!');
+    n++;
+  }while(n<15);
+  return arr;
+  }
