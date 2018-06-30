c55aa5618313ead7a83c3bc2f3a642083b2765dc
Carie Pigeon <cariepigeon81@gmail.com>
2018-06-30 17:50:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function theBeatlesPlay(musiciansArr, instrumentsArr) {
   var finalArr = [];
   for (var i = 0; i < musiciansArr.length; i++) {
-    debugger;
+    //debugger;
     //using the same [i] indexing for both arrays since both //arrays have the same amount of items.
     finalArr.push(musiciansArr[i] + " plays " + instrumentsArr[i]);
   }
-  console.log(finalArr);
+  //console.log(finalArr);
   return finalArr;
 }
