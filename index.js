115682b50ae803f46a3ed0b9c1d672486b466299
Carie Pigeon <cariepigeon81@gmail.com>
2018-06-30 20:56:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 function theBeatlesPlay(musiciansArr, instrumentsArr) {
   var finalArr = [];
   for (var i = 0; i < musiciansArr.length; i++) {
-    //debugger;
     //using the same [i] indexing for both arrays since both //arrays have the same amount of items.
     finalArr.push(musiciansArr[i] + " plays " + instrumentsArr[i]);
   }
-  //console.log(finalArr);
   return finalArr;
-}
+  }
+  
+  function johnLennonFacts(factsArr) {
+    var newArray = [];
+    var i = 0;
+    while (i < factsArr.length) {
+      //factsArr = factsArr[i] + "!!!";
+      newArray.push(factsArr[i] + "!!!");
+      i++;
+    }
+    //return factsArr;
+    return newArray;
+  }
+  
