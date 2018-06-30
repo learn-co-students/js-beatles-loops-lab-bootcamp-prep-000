604675aa76db566af4a2f2d1bc33c9b6536cec1d
Brennan B <bdbasinger777@gmail.com>
2018-06-30 15:57:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function theBeatlesPlay(musicians, instruments) {
+  var a = []
+  for (var i = 0; i < musicians.length; i++) {
+    var str = `${musicians[i]} plays ${instruments[i]}`
+    a.push(str)
+  }
+  return a
+}
+
+
+function johnLennonFacts(facts) {
+  var a = []
+  var i = 0
+  while (i < facts.length) {
+    a.push(`${facts[i]}!!!`)
+    i++
+  }
+  return a
+}
+
+
+function iLoveTheBeatles(number) {
+  var a = []
+  do {
+    a.push("I love the Beatles!")
+    number++
+  } while (number < 15)
+  return a
+}
+
+
+
+
+
+
