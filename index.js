3b9949646ff376697ed3f22bd4bc4da9241507d3
Viktor Ivanov <sessgreem@gmail.com>
2018-07-04 16:46:20 +0000

    Done.

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ function iLoveTheBeatles(number){
   do{
     array.push('I love the Beatles!');
     number--;
-  }while(number > 0 && number < 15);
+  }while(number >= 0 && number < 15);
   return array;
 }
