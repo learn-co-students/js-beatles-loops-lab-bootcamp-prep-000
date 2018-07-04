fa64cf6730f400a97a0b964818ea979ca4b094bb
al chi <alchi1206@gmail.com>
2018-07-04 21:06:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,42 @@
+function theBeatlesPlay(musicians, instruments) {
+  var facts = []
+  for (let i=0; i<4;i++) {
+    facts.push (`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return facts;
+}
+
+function johnLennonFacts (facts){
+  var excitedArray =[]
+ var i = 0
+ while (i<facts.length){
+  excitedArray.push (`${facts[i]}!!!`)
+   i++;
+ }
+  return excitedArray;
+}
+
+function iLoveTheBeatles(n){
+ var madStrings = []
+ let i=0
+  do {
+    madStrings.push ("I love the Beatles!");
+    i++;
+    } while (i<15);
+    return madStrings;
+}
+function iLoveTheBeatles(n){
+ var madStrings = []
+ let i=0
+  if (n<15){
+    do {
+    madStrings.push ("I love the Beatles!");
+    i++;
+    i>n;
+    } while (i<=n && i<15);
+    return madStrings;
+    }
+    else {
+      return "I love the Beatles!"
+    }
+}
