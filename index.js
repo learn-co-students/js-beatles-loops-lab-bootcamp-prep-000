5d52570400f452056cbd73fc25274d55f419585c
Chukwuemelie Oguchi <chukwuemelie4@gmail.com>
2018-07-10 21:20:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+var mus= []
+  for (var i = 0; i < musicians.length; i++) {
+    var array= musicians[i]+" "+ "plays"+" "+instruments[i]
+    mus.push(array)
+  }
+return mus
+}
+
+function johnLennonFacts(facts) {
+var mus=[]
+var i=0
+while (i<facts.length) {
+  var array= facts[i] +"!!!"
+  mus.push(array)
+ var i= ++i
+}
+return mus
+}
+
+function iLoveTheBeatles(num) {
+  var empty= []
+  do {
+    num=++num
+    empty.push("I love the Beatles!")
+
+  } while (num<15);
+return empty
+}
