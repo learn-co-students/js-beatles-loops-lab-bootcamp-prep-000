46d5bd398cf998f2eda6353430ebfd8ea00c235c
Juan Elias Castillo <eliasiscoding@gmail.com>
2018-07-11 13:20:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments) {
+  var Beatles = []
+  for (var i = 0; i < musicians.length; i++) {
+    Beatles[i] = musicians[i] + ' ' + 'plays' + ' ' + instruments[i] 
+  }return Beatles
+}
+
+
+function johnLennonFacts(facts) {
+  
+}
