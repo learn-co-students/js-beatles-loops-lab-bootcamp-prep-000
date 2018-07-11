d1f7a03dd1867e0ee2d259f4b90307fae3fbe377
al chi <alchi1206@gmail.com>
2018-07-11 10:27:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
-function theBeatlesPlay(musicians, instruments) {
-  var facts = []
-  for (let i=0; i<4;i++) {
-    facts.push (`${musicians[i]} plays ${instruments[i]}`)
-  }
-  return facts;
+function theBeatlesPlay (musicians, instruments) {
+  var beatles = [];
+  for (let i=0; i<musicians.length; i++)
+   beatles.push(`${musicians[i]} plays ${instruments[i]}`);
+   return beatles;
 }
 
-function johnLennonFacts (facts){
-  var excitedArray =[]
- var i = 0
- while (i<facts.length){
-  excitedArray.push (`${facts[i]}!!!`)
-   i++;
- }
-  return excitedArray;
-}
-
-
-function iLoveTheBeatles(n){
- var madStrings = []
- let i=0
-  if (n<15){
-    do {
-    madStrings.push ("I love the Beatles!");
+function johnLennonFacts (facts) {
+  var array=[];
+  let i=0;
+  while (i<facts.length) {
+    array.push(`${facts[i]}!!!`);
     i++;
-    i>n;
-    } while (i<=n && i<15);
-    return madStrings;
-    }
-    else {
-      return "I love the Beatles!"
-    }
+  } return array;
+}
+
+function iLoveTheBeatles (number) {
+  var array = [];
+  let i=0;
+  if (number<15){
+    do {
+    array.push('I love the Beatles!');
+    i++;
+  } while (i<=number);
+  } else {
+    return "I love the Beatles!";
+  }
+  return array;
 }
