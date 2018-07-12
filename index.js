1ecc2c4d0f4eb0f850d597438f575e5f1e53884d
Aleksandar Gakovic <algakovic@gmail.com>
2018-07-12 09:13:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// add solution here
+function theBeatlesPlay(arraymus,arrayinst){
+    const music = [];
+    
+    for (let index = 0, len = arraymus.length; index < len; index++) {
+      music.push(`${arraymus[index]} plays ${arrayinst[index]}`);
+    }
+    
+  return music;
+}
+    
