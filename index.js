045da1af8a76d53715e804cef2d763388424b7b5
Nalongsak Luangkhot <loongluangkhot@gmail.com>
2018-07-15 13:39:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // add solution here
+function theBeatlesPlay(arrayMusician,arrayInstrument) {
+  
+  var array = []
+
+  for (var i = 0; i < arrayMusician.length && i < arrayInstrument.length; i++) {
+    array.push(`${arrayMusician[i]} plays ${arrayInstrument[i]}`)
+  }
+  
+  return array
+}
+
+function johnLennonFacts(arrayFact) {
+  for (var i = 0; i < arrayFact.length; i++) {
+    arrayFact[i] = arrayFact[i] + '!!!'
+  }
+  return arrayFact
+}
+
+function iLoveTheBeatles(n) {
+  
+  var array = []
+  
+  do {
+    array.push('I love the Beatles!')
+    n++
+  } while(n < 15)
+  
+  return array
+}
+
