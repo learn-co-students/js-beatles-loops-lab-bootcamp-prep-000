8aad8305874d55ac5036f877e649f16af92c3161
Amanda S <shottsan@dukes.jmu.edu>
2018-07-15 18:34:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+var array = [];
+  for (var i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+var excitedFacts = [];
+var i = 0
+  while (i < facts.length) {
+    excitedFacts.push(`${facts[i]}!!!`)
+    i++
+  }
+  return excitedFacts;
+}
