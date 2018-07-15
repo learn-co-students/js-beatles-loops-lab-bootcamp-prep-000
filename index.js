808d48f30ebdb822004e5079437a7e63ffb21912
Jess Elledge <jessbooks28@gmail.com>
2018-07-15 18:57:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// add solution here
+function theBeatlesPlay(array1, array2) {
+	var beatles = [];
+	for (var i = 0; i < array1.length; i++) {
+		beatles.push(`${array1[i]} plays ${array2[i]}`)
+	} return beatles;
+}
+
+function johnLennonFacts(array) {
+	var exclaim = [];
+	var i = 0;
+	while (i < array.length) {
+		exclaim.push(`${array[i]}!!!`)
+		i++
+	} return exclaim;
+}
+
+function iLoveTheBeatles(num) {
+	var beatlesLove = [];
+	if (num > 15) {
+	  beatlesLove.push("I love the Beatles!");
+	  } else {
+	    while (num < 15) {
+		beatlesLove.push("I love the Beatles!");
+		num++;
+	}
+} return beatlesLove;
+}
