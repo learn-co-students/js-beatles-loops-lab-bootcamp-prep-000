be74d215f5047dab276de2d1b84ee80a5356d472
Edward Castillo <edwardcastillo@me.com>
2018-07-17 01:46:18 +0000

    Done.

--- index.js
+++ index.js
@@ -4,3 +4,20 @@ function theBeatlesPlay(musicians, instruments) {
     array.push(`${musicians[n]} plays ${instruments[n]}`); // adds values to empty array as it cycles each musician
   } return array; // returns the array of new strings
 }
+
+function johnLennonFacts(facts) {
+  var n = 0;
+  while (n < facts.length) {
+    facts[n] += "!!!";
+    n++;
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(n) {
+  var array = []; // creates empty array stored in a variable
+  do {
+    array.push("I love the Beatles!"); // adds value to empty array as it cycles up to 15
+  } while (n++ && n < 15);
+  return array;
+}
