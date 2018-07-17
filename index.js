027d1d9f8ceebbfddde3f7914fe2da5095dda765
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-07-17 04:41:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var beatles=[];
+  for(var i = 0; i < musicians.length; i++) {
+    beatles.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return beatles
+}
+function johnLennonFacts(array) {
+  var newFacts=[];
+  var i=0;
+  while(i<array.length) {
+    newFacts.push(array[i] + "!!!");
+    i++;
+  }
+  return newFacts;
+}
+function iLoveTheBeatles(n) {
+  var beatles=[];
+  do {
+    beatles.push(`I love the Beatles!`);
+    n++;
+  } while(n<15);
+  return beatles;
+}
