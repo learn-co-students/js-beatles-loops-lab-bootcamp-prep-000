009570ef5a231dc790f106b637ea4e868ad8328c
karen S <buzz.kyt@gmail.com>
2018-07-18 17:48:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// add solution here
+function theBeatlesPlay(musicians, instructment) {
+  var emptyArray = [];
+  for (let i = 0; i <4; i++) {
+   emptyArrary = [...emptyArray, `${musicians[i]} plays ${instructment[i]}.`];
+  }
+  return emptyArray;
+}
+
+const facts = ["He was the last Beatle to learn to drive",
+  "He was never a vegetarian","He was a choir boy and boy scout","He hated the sound of his own voice"]
+
+function johnLennonFacts() {
+    while (let i = 0; i < 4; i++) {
+    newFacts = [...facts,`${facts(i)}!!!`];
+  }
+  return newFacts;
+}
