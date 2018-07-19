53bcd689e6f8cb49f442da583ee43f988b352541
Mai Campbell <jirallc@gmail.com>
2018-07-19 15:55:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 function theBeatlesPlay(musicians, instruments) {
   var arr = [];
-
   for(let i = 0; i < musicians.length; i++) {
-    arr.push(musicians[i] + " plays " + instruments[i]);
-    }
-    return arr;
+    arr.push(musicians[i] + ' plays ' + instruments[i]);
+  }
+  return arr;
 }
 
 function johnLennonFacts(facts) {
   var newFacts = [];
   var i = 0;
-  while (i < facts.length) {
-    newFacts.push(facts[i] + "!!!");
+  while(i < facts.length) {
+    newFacts.push(facts[i] + '!!!');
     i++;
   }
   return newFacts;
@@ -23,7 +22,6 @@ function iLoveTheBeatles(num) {
     love.push('I love the Beatles!');
     num++;
   } while (num < 15) {
-    // num++;
   }
-  return love;
+  return love; 
 }
