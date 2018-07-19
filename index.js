5e1e3eefc5b57de5dd12fd9e3b8ebb1c263afc24
timothy.knoll@gmail.com <timothy.knoll@gmail.com>
2018-07-19 23:58:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// add solution here
+function theBeatlesPlay (musicians, instruments) {
+  let result = []
+  for (let i = 0; i < musicians.length; i++){
+    result.push(`${musicians[i]} plays ${instruments[i]}`) }
+  return result
+}
+
+function johnLennonFacts (array) {
+  let comment = []
+  let i = 0
+  while (i < array.length){
+    comment.push(array[i] + "!!!")
+    i++
+  }
+  return comment
+}
+
+function iLoveTheBeatles (n) {
+  let final = []
+  do {
+    final.push("I love the Beatles!")
+    n++
+  }
+  while (n<15) {
+  }
+    return final
+}
+
