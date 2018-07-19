02169875963ec3e55cd2abd915aac18dd1a89704
Fogang Rodriq <fogangrodriq@gmail.com>
2018-07-19 23:36:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var got = []
+  // var arLength = musicians.length
+  for(var i=0; i<musicians.length; i++ ){
+    var our = `${musicians[i]} plays ${instruments[i]}`;
+    //var our = musicians[i]+ 'plays'+ instruments[i]
+    got.push(our);
+  }
+  return got;
+}
+function johnLennonFacts(array){
+  while(1 < array.length){
+    array[i--] = `${array[i--]}!!!`
+    return array;
+  }
+}
