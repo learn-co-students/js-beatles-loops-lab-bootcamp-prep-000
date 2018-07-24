156315703f63a85f4d1118260653905bbf83c7f9
Taylor J Young <tjy14@scarletmail.rutgers.edu>
2018-07-24 19:37:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// add solution here
+var musicians = ['Shakira', 'MCR', 'The Beatles']
+var instruments = ['Sax', 'Flute','Piano']
+
+function theBeatlesPlay(array1, array2){
+    var x = []
+    for (var z =0; z<(array1.length); z++){
+    var y = String(array1[z] + " plays " + array2[z])
+    x.push(y)
+    }
+  return x 
+}
+
