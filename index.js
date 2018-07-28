b859c72bffc9984bfb80198597401a1325e86ee3
Joe Muro <jmuro0321@gmail.com>
2018-07-28 17:10:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var talents = [ ];
+  
+  for (let i = 0; i < musicians.length; i++) {
+    if (i == 0) {
+      talents.push("John Lennon plays Guitar");
+    } else {
+      talents.push(musicians[i] + " plays " + instruments[i]);
+    }
+  }
+  
+  return talents;
+}
+
+function iLoveTheBeatles(n) {
+  var love = [ ];
+  
+  do {
+    love.push("I love the Beatles!");
+    n++;
+  } while (n < 15);
+  
+  return love;
+}
+
+function johnLennonFacts(facts) {
+  var loudFacts = [];
+  
+  while (facts.length > 0) {
+    loudFacts.push(facts.shift() + "!!!");
+  }
+  
+  return loudFacts;
+}
