090e509f15712f7af3032bb267b5315009a8d0e1
Amir Safoev <amirsafoev0@gmail.com>
2018-07-28 19:07:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var array[]
+  for ( i = 0; i < 4; i++){
+    if (i === 0) {
+    console.log(musicians.[i], ...array, instruments[i]);
+    
+  
+  }
+}
+}
