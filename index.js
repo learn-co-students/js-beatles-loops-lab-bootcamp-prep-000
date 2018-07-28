ec2bdf22ca02385d757a5eb52e19eb0c194148ee
Amir Safoev <amirsafoev0@gmail.com>
2018-07-28 19:39:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
-  var array[]
-  for ( i = 0; i < 4; i++){
-    if (i === 0) {
-    console.log(musicians.[i], ...array, instruments[i]);
+  var emptyArray = []
+  for ( i = 0; i < musicians.length ; i++){
     
-  
+ emptyArray.push(`${musicians[i]} plays ${instruments[i]}`);
+    
   }
-}
+  return emptyArray; 
 }
