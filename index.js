af8628dc91696b67c28bee905192a50280281e85
Amir Safoev <amirsafoev0@gmail.com>
2018-07-28 20:24:13 +0000

    Done.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function theBeatlesPlay(musicians, instruments){
 function johnLennonFacts(facts){
   let i = 0;
   while (i<facts.length ) {
-   return (facts[i],...'!!!');
+      (facts[i],...'!!!'; i++);
    
   }
 
