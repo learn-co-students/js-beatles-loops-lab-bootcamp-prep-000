99aa8defc17d7ae753667f4eec8bd32821d9e8db
sim.catley@hotmail.com <sim.catley@hotmail.com>
2018-07-29 17:30:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var array = []
+  for (var i = 0; i < musicians.length; i++){
+  var string = musicians[i] + " plays " + instruments[i]
+    array.push(string)
+  }
+  return array
+}
+
+function johnLennonFacts(facts){
+  var array = []
+  var i = 0
+  while (i < facts.length){
+  var string = facts[i] + "!!!"
+    array.push(string)
+    i++
+  }
+  return array
+}
+
+function iLoveTheBeatles(n){
+  var array = []
+  var i = n
+  do {
+    var string = "I love the Beatles!"
+    array.push(string)
+    i++
+  } while (i < 15);
+  return array
+}
+
+
+
+
+
