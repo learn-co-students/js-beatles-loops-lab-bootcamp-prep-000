3737ec1bcf3d51ca2045d791509db9e5d413e5b9
thomasl.pittman@gmail.com <thomasl.pittman@gmail.com>
2018-07-29 00:03:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// add solution here
+
+function theBeatlesPlay(musicians, instruments){
+  var emptyArray = [];
+  for (let i = 0; i < musicians.length; i++ ){
+    emptyArray.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return emptyArray;
+}
+
+function johnLennonFacts(array){
+  var newArray = [];
+  var i = parseInt(0);
+  while (i < array.length){
+   newArray.push(array[i] + "!!!");
+   i++;
+  }
+  return newArray;
+}
+
+
+function iLoveTheBeatles(number){
+  var emptyString = [];
+  do {
+    var string = "I love the Beatles!";
+    emptyString.push(string);
+    number++;
+  }while(number < 15)
+  return emptyString;
+}
