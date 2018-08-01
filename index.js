e6f1efc8cc9fc3c4d1a1bf88db467617a2f096d6
Joe Smith <reasonablecoffee@gmail.com>
2018-08-01 16:40:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,74 @@
 // add solution here
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i == 1) {
+      array.push("I am 1 strange loop.")
+    }
+    else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+ return array; }
+ 
+ function whileLoop(n) {
+   while (n > 0) {
+     console.log(--n)
+     if (n <= 0) 
+     return ('done')
+ }}
+
+ function doWhileLoop(array) {{
+   do {array.pop()}
+   while (array.length > 0 || maybeTrue()) 
+ }
+   function maybeTrue() {
+  return Math.random() >= 0.5
+}
+return array}
+
+function theBeatlesPlay(musicians, instruments) {
+  var x = []
+  for (let z = 0; z < 4; z++){
+  x.push(`${musicians[z]} plays ${instruments[z]}`)
+  }
+  return x
+}
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(facts) {
+  var i = 0
+   while (1 > facts.length > 0) {
+     console.log(facts[i]++)
+     if (i > 0)
+   }}
+   return facts[i] + "!!!"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
