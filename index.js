93086ec2cfb3f9434b146b7f9bd3d8e7fd9d3de8
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-08-02 12:07:27 +0000

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function theBeatlesPlay(musicians, instruments){
         newArr.push(musicians[i] + " plays " + instruments[i]);
     }
     return newArr;
+
 }
 
 // TODO + Create a function `johnLennonFacts`. This function will accept one argument, an array of facts about John Lennon (note that it might not be exactly the following facts):
@@ -20,5 +21,22 @@ function johnLennonFacts(array){
         newArr.push(array[i] + '!!!');
         i++;
     }
+    return newArr;
+}
+
+// TODO + Create a function `iLoveTheBeatles` which accepts a number as a parameter. The body of the function should create a variable that stores an empty array. Then, implement a do-while loop inside the function that adds `"I love the Beatles!"` to the empty array. Then the loop should increment the number passed in as a parameter. The condition of the loop should check to see that the parameter number is less than `15`. The function should return the array with the strings `"I love the Beatles!"`.
+
+function iLoveTheBeatles(n){
+    let newArr = [];
+    let i = 0;
+    do {
+        if(n < 15){
+        newArr.push('I love the Beatles!');
+        i++
+        } else {
+        newArr.push('I love the Beatles!');
+        return newArr;
+        }   
+    } while (i <= n);
     return newArr;
 }
