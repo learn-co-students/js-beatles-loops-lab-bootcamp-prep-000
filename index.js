62da8afa9a92e5a2e57e479267ea131672346eab
Albert Chun <albert.s.chun@gmail.com>
2018-08-03 20:20:19 +0000

    Done.

--- index.js
+++ index.js
@@ -8,32 +8,25 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
-
-
 function johnLennonFacts(facts) {
-  var array = [];
+  var newArray = [];
   var i = 0;
-  while (i < facts.length); {
-  array.push(facts[i] + "!!!");
+  while (i < facts.length) {
+  newArray.push(facts[i] + "!!!");
   i++;
   }
-  return array;
+  return newArray;
 }
 
 
 function iLoveTheBeatles(number) {
   var array = [];
-  (number++);
   do {
+    (number++);
     array.push("I love the Beatles!");
   } while (number < 15);
   return array;
 }
+
 
 
