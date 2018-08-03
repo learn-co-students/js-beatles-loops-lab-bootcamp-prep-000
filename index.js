e94221fa65dcd9de484492bf190d791482cb3583
Alex S <alex.sunga@fliptech.co>
2018-08-03 01:15:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments){
+  const array = []
+  for (let i = 0; i < musicians.length; i++){
+  array.push (`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+}
+
+function johnLennonFacts (facts){
+  const exclamation = []
+  let i = 0;
+  while(i < facts.length){
+    exclamation.push(`${facts[i]}!!!`)
+  }
+  
+}
