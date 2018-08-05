990c8503c2394a2c9ddb67723661e3e6aee9f79c
Yuan Pan <y39pan@edu.uwaterloo.ca>
2018-08-05 20:29:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var result = [];
+  for(var counter = 0; counter < musicians.length; counter++){
+    result.push(`${musicians[counter]} plays ${instruments[counter]}`);
+  }
+  return result;
+}
+
+function johnLennonFacts(facts){
+  var counter = 0;
+  while(counter < facts.length){
+    facts[counter] += "!!!";
+    counter++;
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(number){
+  var array = [];
+  do{
+    array.push("I love the Beatles!");
+    number++;
+  } while(number < 15)
+  return array;
+}
