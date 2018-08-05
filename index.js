94b46502562a30fb7dc1096518acee77d28c56b2
Ashley Wong <ashwong04@gmail.com>
2018-08-05 21:38:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 // add solution here
 
 function theBeatlesPlay (arrayMusicians, arrayInstruments){
-  let newArray = '';
+  var newArray = [];
  
  for (var i= 0; i < arrayMusicians.length; i++){
-   newArray.push( arrayMusicians[i] + " plays  " + arrayInstruments[i]);
+   newArray.push (arrayMusicians[i] + " plays  " + arrayInstruments[i]);
  }
  return newArray;
+}
+
+
+
+function johnLennonFacts(arrFacts){
+  let newFacts = [];
+  var i = 0
+  
+  while (i < arrFacts){
+  newFacts.push (arrFacts[i] + "!!!");
+  i++;
+  }
+  return newFacts;
 }
