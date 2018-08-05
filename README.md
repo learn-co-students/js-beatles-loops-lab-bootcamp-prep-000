057d468d3b1d636c120ebafdc2910c17732a1c53
John Werenski <jweredc@gmail.com>
2018-08-05 17:15:43 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Beatles Loops
+pposed# Beatles Loops
 
 ## Objectives
 
--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  let arrayNew = [];
+  for (let i = 0; i < musicians.length; i++ ) {
+    arrayNew.push(`${musicians[i]} plays ${instruments[i]}`);
+    
+  }return arrayNew;
+}
+
+
+function johnLennonFacts(facts) {
+  let x = 0;
+  while (x < facts.length) {
+    facts.push('!!!');
+    x++;
+  } 
+  return facts;
+}
