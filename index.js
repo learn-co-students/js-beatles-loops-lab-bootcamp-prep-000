1bc76130b40fb573d8a4db6f00e46cb6da1887da
Noah Lee <no@hlee.me>
2018-08-05 21:10:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// add solution here
+function theBeatlesPlay(m, i){
+  let e = []
+  for(let x = 0; x < m.length; x++){
+    e.push(`${m[x]} plays ${i[x]}`)
+  } return e
+}
+
+function johnLennonFacts(f){
+  let x = 0
+  while(x < f.length){
+    f[x] += '!!!'
+    x++
+  } return f
+}
+
+function iLoveTheBeatles(n){
+  let e = []
+  do {e.push('I love the Beatles!')
+  n++
+  } while(n < 15)
+  return e
+}
