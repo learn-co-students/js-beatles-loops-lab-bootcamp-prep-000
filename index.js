0dcab696f0502f23e52f946b95e7521e1a6f65e8
Grace Kang <gracekang.93@gmail.com>
2018-08-07 04:04:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  let array = [];
+  for (let i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  let array = [];
+  while (facts.length > 0) {
+    array.push(`${facts[facts.length - 1]}!!!`);
+    --facts.length;
+  }
+  array.reverse();
+  return array;
+}
+
+function iLoveTheBeatles(num) {
+  let array = [];
+  do {
+    array.push('I love the Beatles!');
+    num++;
+  } while(num < 15);
+  return array;
+}
