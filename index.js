e8c4108b1ca3d4dfd4cf9e44a845afd2db9e1b89
Tirem Bareno-Sosna <tirempbs@gmail.com>
2018-08-07 22:01:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var strings = [];
+  for (let i = 0; i < musicians.length; i++) {
+      strings.push(`${musicians[i]}` + " plays " + `${instruments[i]}`);
+  }
+  return strings;
+}
+
+function johnLennonFacts(facts) {
+  var loudFacts = [];
+  let i = 0;
+  while (i < facts.length) {
+    loudFacts.push(`${facts[i]}` + "!!!");
+    i++;
+  }
+  return loudFacts;
+}
+
+function iLoveTheBeatles(i) {
+  var love = [];
+  do {
+    love.push("I love the Beatles!");
+    i++;
+  } while (i < 15);
+  return love;
+}
