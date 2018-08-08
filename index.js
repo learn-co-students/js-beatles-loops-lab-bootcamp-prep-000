f754e556e5620ae004daf9047acf6bf44feb8acb
Hank Liu <hanke.liu@gmail.com>
2018-08-08 01:28:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = new Array();
+  for (i=0, i<=musicians.length, ++i) {
+    array[i] = `${musicians(i)} + 'plays'+ ${instruments[i]}`;
+  }
+  return array;
+}
