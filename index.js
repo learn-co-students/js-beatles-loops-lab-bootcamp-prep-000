1978b606452aeb511e8d9de4a75902bb7d9fa2f7
Kaveh Saleminejad <lcaveh@gmail.com>
2018-08-08 20:43:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i=0; i<musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array;
+}
+
+function iLoveTheBeatles(facts) {
+  var array =[];
+  var i=0;
+  while (i < (facts.length)) {
+    array.push(`${facts[i]} !!!`);
+    i++;
+  }
+  return array;
+}
