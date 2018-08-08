f1c9237f45e5a97f8b5f918fba3ea6aa1c6e9849
mmetros@wesleyan.edu <mmetros@wesleyan.edu>
2018-08-08 16:03:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 // add solution here
 
+function theBeatlesPlay(musicians, instruments){
+  var emptyArray = [];
+
+  for(var i = 0; i < musicians.length ; i++){
+    // create a string using the first index of
+    // musicians array and first index of instruments array
+    emptyArray.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return emptyArray
+}
+
+
+function johnLennonFacts(facts){
+// use a while loop
+var i = 0
+  while(i < facts.length){
+    facts[i] = `${facts[i]}!!!`
+    i++
+  }
+  return facts
+}
+
+
+
 function iLoveTheBeatles(number){
   var emptyArray = [];
   // create a do-while loop
