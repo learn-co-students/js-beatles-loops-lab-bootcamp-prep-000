741190d57a51c9dc69199457c76635ff10ce4f4b
Preston A Arnette <arnettepreston@gmail.com>
2018-08-09 07:18:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // add solution here
+
+// indexes array "musicians" and array "instruments" and pushes it as a string into "storredArray"
+function theBeatlesPlay(musicians, instruments) {
+  var storedArray = []
+  for (var i = 0; i < 4; i++) {
+    storedArray.push(`${musicians[i]} plays ${instruments[i]}`)
+  }  
+  
+  return storedArray
+}
+
+
+
+function johnLennonFacts(facts) {
+  var newFacts = []
+  var i = 0
+  while (i < facts.length) {
+  newFacts.push(facts[i] +'!!!')
+  i++
+}
+ 
+ return newFacts
+}
+
+function iLoveTheBeatles(n) {
+  var love = []
+  do {
+    love.push("I love the Beatles!")
+    n++
+  } while (n < 15)
+ 
+  return love
+}
