2da60edd060d604569b093f5dbdca1011d4840c6
Hank Liu <hanke.liu@gmail.com>
2018-08-09 16:25:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
+
 function theBeatlesPlay(musicians, instruments) {
   var array = new Array();
-  for (i=0, i<=musicians.length, ++i) {
-    array[i] = `${musicians(i)} + 'plays'+ ${instruments[i]}`;
+  for (let i=0; i < musicians.length; i++) {
+    array[i] = `${musicians[i]} plays ${instruments[i]}`;
   }
   return array;
 }
+
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  var j = '!!!';
+  var myArray= new Array();
+
+  while (i < facts.length) {
+    myArray[i] = facts[i].concat('!!!'); // tried plus for concat; concat not working, tested in console.
+    ++i; // tried ++1
+  }
+  return myArray;
+}
+//
+//
+// function  iLoveTheBeatles(n) {
+//   var myArray = new Array();
+//   do {
+//     myArray.push("I love the Beatels!");
+//     n++;
+//   } while(n <15);
+//   return myArray;
+// }
