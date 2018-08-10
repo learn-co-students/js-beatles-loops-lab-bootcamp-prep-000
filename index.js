762584716e5122ee5b7a06af08d3ab3b316fe89a
Nathaniel Stiers <nvstiers@gmail.com>
2018-08-10 06:19:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var newArray = [];
+  for (var i = 0, l = musicians.length; i < l; i++){
+    newArray.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return newArray
+}
+
+function johnLennonFacts(facts){
+ var shoutedFacts = [];
+ var i = 0
+  while (i < facts.length) {
+    shoutedFacts.push(facts[i]+"!!!")
+    i++
+  }
+
+  return shoutedFacts
+}
