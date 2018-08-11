dac65d02f459458beeb73d9536a26e99be1812e9
Michael Tompkins <mtompkins82@gmail.com>
2018-08-11 13:57:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// add solution here
+function theBeatlesPlay(arrMus, arrInst){
+  var emp = [];
+  arrMus = ['John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Starr'];
+  arrInst = ['Guitar', 'Bass Guitar', 'Lead Guitar', 'Drums'];
+  var i;
+  for(i = 0; i < arrMus.length; i++){
+    emp.push(arrMus[i] + ' plays ' + arrInst[i]);
+  }
+  return(emp);
+}
+
+function johnLennonFacts(array){
+  var i = 0;
+  while(i < array.length){
+    array[i] = array[i] + '!!!';
+    i++;
+  }
+  return array;
+}
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function iLoveTheBeatles(num){
+  var arr = [];
+  do{
+    arr.push('I love the Beatles!')
+    num++
+}while(num < 15);
+return arr;
+}
+
