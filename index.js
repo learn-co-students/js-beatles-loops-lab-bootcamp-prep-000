a1bbd00e0d2fe8ec289eac5c00c2b62d3621e6b7
Fadi hajj <fadi.elhajj@lau.edu>
2018-08-12 17:25:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+function theBeatlesPlay(musArr, instArr){
+  var emptyArr = [];
+  
+  for (var i=0; i<musArr.length; i++)
+  {
+    emptyArr.push(musArr[i] + " plays " + instArr[i]);
+  }
+  return emptyArr;
+}
+
+function johnLennonFacts(factsArr){
+  
+  var i=0;
+  while (i < factsArr.length){
+    factsArr[i] += '!!!';
+    i++;
+  }
+  return factsArr;
+}
+
+function iLoveTheBeatles(num){
+  var empty = [];
+  do{
+    empty.push('I love the Beatles!');
+    num++;
+  }
+  while (num < 15);
+  return empty;
+}
