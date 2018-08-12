565596224414cab4e6333d8b345b1a8750695700
Austin Luft <ausluft@gmail.com>
2018-08-12 18:27:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // add solution here
+function theBeatlesPlay (arrayMusicians, arrayInstruments) {
+  var array = [];
+  
+  for (var i = 0; i < arrayMusicians.length; i++) {
+  array.push(`${arrayMusicians[i]} plays ${arrayInstruments[i]}`);
+  }
+  
+  return array;
+}
+
+function johnLennonFacts (facts) {
+  var factsExciting =[];
+  var i = 0;
+  
+  while (i < facts.length) {
+  factsExciting.push(`${facts[i]}!!!`);
+  i = i + 1
+  }
+  return factsExciting;
+}
+
+function iLoveTheBeatles (number) {
+  var array = []
+  
+  do {
+    array.push("I love the Beatles!");
+    number = number + 1;
+  } while (number < 15)
+  
+  return array;
+}
