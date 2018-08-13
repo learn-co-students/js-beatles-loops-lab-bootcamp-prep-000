ed76204d0231282d57035dc57dd2112764e7f7b0
Joshua Vieira <physicsnerd38@gmail.com>
2018-08-13 19:19:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < musicians.length; i++) {
+   array.push(musicians[i] + " " + "plays" + " " + instruments[i]);
+  }
+    return array;
+}
+
+function johnLennonFacts(array) {
+  var facts = [];
+  var i = 0;
+  while (i < array.length) {
+      facts.push(`${array[i]}` + "!!!");
+      i++;
+  }
+    return facts;
+}
+
+function iLoveTheBeatles(number) {
+  var arr = [];
+  do {
+    arr.push("I love the Beatles!");
+    number++;
+  } while (number < 15);
+  return arr;
+}
