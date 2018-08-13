1b65a75da42013da2f69c616785170bae8c2516a
Fickry Bil Iman <fickry.bil.iman@gmail.com>
2018-08-13 08:07:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  let result = [];
+  for (let i = 0; i < musicians.length; i++) {
+    result.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return result;
+}
+
+function johnLennonFacts(facts) {
+  for (let i = 0; i < facts.length; i++) {
+    facts[i] = facts[i] + '!!!';
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(number) {
+ let result = [];
+ do {
+   result.push('I love the Beatles!');
+   number++;
+ } while (number < 15);
+ return result;
+}
