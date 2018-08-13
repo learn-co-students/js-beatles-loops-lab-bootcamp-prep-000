e5aee6f3b3164b3c0b3dadd1998a416d9ad8ba20
Patrick Cheng <pat.rick.cheng@hotmail.com>
2018-08-13 06:13:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var answer = []
+  for(var x=0;x< musicians.length;x++){
+    answer.push(`${musicians[x]} plays ${instruments[x]}`)
+  }
+  return answer
+}
+
+function johnLennonFacts(facts){
+  var count = 0
+  var answer = []
+  while(count<facts.length){
+    answer.push(`${facts[count]}!!!`)
+    count++
+  }
+  return answer
+}
+
+function iLoveTheBeatles(num){
+  var answer = []
+  do{
+    answer.push("I love the Beatles!")
+    num++
+  }while(num <15)
+  return answer
+}
