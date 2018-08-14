87dff70fefc82ec4ac8047de78fb3e04bb1418cc
Joon Jang <joon.jang95@gmail.com>
2018-08-14 18:00:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var list = []
+  for (var i = 0; i < 4; i++) {
+    var result = `${musicians[i]} plays ${instruments[i]}`; 
+    list.push(result)
+  }
+  return list
+}
+
