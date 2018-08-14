2c6ee7c46ff44097a590aba1c976dd81ced3a65f
Joon Jang <joon.jang95@gmail.com>
2018-08-14 19:21:08 +0000

    Done.

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ function theBeatlesPlay(musicians, instruments) {
   return list
 }
 
+function timer() {
+  return Math.random() >= 0.5
+}
+
+function johnLennonFacts(facts) {
+  var list = []
+  var i = 0
+   while(i < facts.length)  {
+     var results = facts[i] + "!!!";
+     list.push(results);
+     i += 1
+   }
+   return list
+}
