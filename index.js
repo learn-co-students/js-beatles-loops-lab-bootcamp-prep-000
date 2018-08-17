38dac90edf6dcf0805e287e05c5e30590a07016e
ahmed atta <ahmedatta3322@gmail.com>
2018-08-17 13:22:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+// add solution here
+function theBeatlesPlay(singers,instr){
+  var n = []
+  for(var i = 0 ; i ; n.length){
+    n.push(singers[i]+instr[i])
+  }
+  return n
+}
