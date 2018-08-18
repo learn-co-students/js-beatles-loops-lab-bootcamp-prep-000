a635be3cbabe111881e01fc67d8cecf704d80aa9
Violet Moon <violet.wmoon@gmail.com>
2018-08-18 09:15:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  let beatlesStrings = []
+
+  for (var i = 0; i < musicians.length; i++) {
+    beatlesStrings.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return beatlesStrings
+}
+
+function johnLennonFacts(lennonArray) {
+  let newArray = []
+  var i = 0
+
+  while (i < lennonArray.length) {
+    newArray.push(`${lennonArray[i]}!!!`)
+    i++
+  }
+    return newArray
+  }
+
+function iLoveTheBeatles(number) {
+  let beatlesLove = []
+  var i = number
+
+  do {
+      beatlesLove.push('I love the Beatles!')
+      i++
+  } while (i < 15)
+
+  return beatlesLove
+}
