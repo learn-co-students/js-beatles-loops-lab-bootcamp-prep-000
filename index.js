369daa11a23904d94c052189c48381c44fc207b9
Paul Ishiyama <paul.h.ishiyama@gmail.com>
2018-08-20 04:41:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+
+function theBeatlesPlay(artist, instrument) {
+  var Beatles = [];
+  for (var i = 0; i < artist.length; i++) {
+    Beatles.push(artist[i] + " plays " + instrument[i]);
+  }
+  return Beatles;
+}
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  var exc = [];
+  while (i < facts.length) {
+    exc.push(facts[i] + "!!!");
+    i++;
+  }
+  
+  return exc;
+}
+
+function iLoveTheBeatles(n) {
+  var luv = [];
+  do {
+    luv.push("I love the Beatles!");
+    n++;
+  } while (n < 15);
+  return luv;
+}
