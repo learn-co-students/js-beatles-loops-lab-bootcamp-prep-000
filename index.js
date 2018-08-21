2809601d484625d8d61568362d71545befd63853
jvpink200 <jvillarr20@gmail.com>
2018-08-21 03:06:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments){
+  var musicians = [];
+  var instruments = [];
+}
 var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-
-
-theBeatlesPlay(musicians, instruments);
-
-function theBeatlesPlay(musicians, instruments) {
-  var newArr = [];
-  for(var i =0; i < musicians.length; i++) {
-    newArr.push(musicians[i] + " plays " + instruments[i]);
-  }
-  return newArr;
-}
-
-function johnLennonFacts(facts){
-    var results = [];
-    var i = 0;
-  while(i < facts.length){
-    results.push(facts[i]+"!!!");
-  }
-  return results;
-}
