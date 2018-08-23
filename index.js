c651caef3a6050dbeee1ca3758861adb10b3c403
cwoolnoughchamorro@googlemail.com <cwoolnoughchamorro@googlemail.com>
2018-08-23 15:12:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+        const artists = [];
+            musicians.forEach((musician, index) => { 
+                artists.push(`${musician} plays ${instruments[index]}`)
+       
+        })
+        return artists;
+    }
+    theBeatlesPlay(musicians, instruments);
+    
+
+function johnLennonFacts(facts){
+      let results =[];
+      let i = 0;
+      while(i < facts.length){
+          results.push(facts[i] + "!!!")
+          i++
+      } 
+      return results;   
+  }
+
+  johnLennonFacts(facts)
+  
+  function iLoveTheBeatles(number){
+    let myArray = [];
+    do {
+      number++
+      myArray.push("I love the Beatles!");
+    } while (number < 15);
+    return myArray;
+  }
+      
+  
