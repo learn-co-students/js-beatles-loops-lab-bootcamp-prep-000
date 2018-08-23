3a03ac9778754a950ce7c7eb37a1fa29fbe70301
Stefanieg84 <stefanieg84@gmail.com>
2018-08-23 18:52:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var emptyArray = [];
+  
+  for(let i = 0; i < musicians.length; i++){
+    if(i === 0){
+      emptyArray.push(`${musicians[i]} plays ${instruments[i]}`);
+    } else {
+      emptyArray.push(`${musicians[i]} plays ${instruments[i]}`);
+    }
+  }
+  
+  return emptyArray;
+}
+
+function johnLennonFacts(array){
+  let i = 0;
+  
+  while(i < array.length){
+    array[i] = array[i] + "!!!";
+    ++i;
+  }
+  
+  return array;
+}
+
+function iLoveTheBeatles(number){
+  var emptyArray = [];
+  
+  do{
+    emptyArray.push("I love the Beatles!");
+    ++number;
+  }while(number<15)
+  
+  return emptyArray;
+}
+
