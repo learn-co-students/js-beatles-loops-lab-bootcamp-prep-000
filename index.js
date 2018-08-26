aeb315d7d9fbb48488af7d5d5baa1979d7389307
gc6123@bard.edu <gc6123@bard.edu>
2018-08-26 01:03:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// add solution here
+function theBeatlesPlay(musiciansArray, instrumentsArray) {
+  var music = []
+  for (let i = 0; i < musiciansArray.length; i++) {
+    music.push(`${musiciansArray[i]} plays ${instrumentsArray[i]}`)
+  } 
+  return music
+}
+function johnLennonFacts(facts) {
+ let i = 0; 
+ while (i < facts.length) {
+ facts[i] = facts[i] + '!!!'
+ i++;
+ }
+return facts
+}
+function iLoveTheBeatles(number) {
+  var empty = [];
+  do {empty.push("I love the Beatles!"); 
+    number++;
+  }
+  while (number < 15)
+  return empty
+}
