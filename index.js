87e1da3c226734d173fd09885a384cccc1ec0d54
Matt Orcutt <morcutt@gmail.com>
2018-08-26 00:07:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // add solution here
+
+function theBeatlesPlay(musicians,instruments){
+  var play = [];
+  for(let i=0; i<musicians.length; i++){
+    play.push(musicians[i] +' plays '+ instruments[i]);   
+  }
+  return play;
+}
+
+function johnLennonFacts(facts){
+  var i=0;
+  while(i<facts.length){
+    facts[i] += "!!!";
+    i++;
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(num){
+  var cause = [];
+  do {
+    cause = ['I love the Beatles!'];
+  } while (cause.length<num);
+  return cause;
+}
