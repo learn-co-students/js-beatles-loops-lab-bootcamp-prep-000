bd76be5f47c63b57b4154cdbc76ff4c019638091
truongvd@gmail.com <truongvd@gmail.com>
2018-08-26 00:08:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var beatles = [];
+  for (let i = 0; i < musicians.length; i++) {
+    beatles.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return beatles;
+}
+
+function johnLennonFacts(facts) {
+  var lennonFacts = [];
+  var numOfFacts = facts.length;
+  while (numOfFacts !== 0) {
+    lennonFacts.unshift(facts[numOfFacts-1] + "!!!");
+    numOfFacts--;
+  }
+  return lennonFacts;
+}
+
+function iLoveTheBeatles(number) {
+  var beatlesFan = [];
+  do {
+    beatlesFan.push("I love the Beatles!");
+    number++;
+  } while(number < 15);
+  return beatlesFan;
+}
