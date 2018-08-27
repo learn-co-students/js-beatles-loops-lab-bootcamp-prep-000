de125faca918a52cf1a32175a4950692fd4e2222
Steven Rico <intuitive.io.programmer@gmail.com>
2018-08-27 20:37:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // add solution here
+function theBeatlesPlay(musician, instrument) {
+  var beatles = [];
+  for (var i = 0; i < musician.length; i++) {
+    beatles.push(musician[i] + " plays " + instrument[i])
+  }
+  return beatles;
+}
+
+function johnLennonFacts(facts) {
+    var newFacts = [];
+    var i = 0;
+    while (i < facts.length) {
+        newFacts.push(facts[i] +"!!!");
+        i++;
+    }
+    return newFacts;
+}
+
+function iLoveTheBeatles(number) {
+  var beatlesMessage = [];
+  do {
+    beatlesMessage.push("I love the Beatles!");
+    number++;
+  }
+  while (number < 15);
+  return beatlesMessage;
+}
