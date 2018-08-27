b5a6490a31ffa373070ee45d0c60ed5ae1bbd785
Gabrielle Bellini <gbellini90@gmail.com>
2018-08-27 22:19:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// add solution here
+function theBeatlesPlay(musicians,instruments){
+var arrayBeatles=[];
+ for (let musiciansIndex=0;musiciansIndex<musicians.length;musiciansIndex++)
+ {arrayBeatles.push(musicians[musiciansIndex] + " plays " + instruments[musiciansIndex])
+ }
+  return arrayBeatles;
+}
+
+function johnLennonFacts(facts){
+  var newJohnFacts=[];
+  var i= 0;
+ while (i<facts.length){
+   newJohnFacts.push(facts[i] + "!!!")
+   i++;
+ }
+ return newJohnFacts
+}
+
+function iLoveTheBeatles(number){
+  var array=[];
+  do{array.push("I love the Beatles!")
+    number++;
+  }
+  while(number<15);
+  return array;
+} 
+ 
