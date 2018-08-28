bc8b5636b6d0a57e16bd89900418a711dd95b577
Christopher Anderson <andersonchris227@gmail.com>
2018-08-28 00:11:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) { 
+  const arr=[];
+  for (let i=0; i < musicians.length; i++){
+   arr.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return arr;
+}
+function johnLennonFacts(facts) {
+  const excitedFacts = [];
+  let i = 0;
+  while (i<facts.length) {
+  excitedFacts.push(`${facts[i]}!!!`);
+  i++;
+}
+return excitedFacts;
+}
