dc03b851dcab85929900096b7cb3c19e5d8e6796
Rich Keyzor <web_dev_rich@keyzor.net>
2018-08-29 20:12:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // add solution here
+function theBeatlesPlay(musArr, instArr) {
+  var arr = [];
+  for (var i = 0; i < musArr.length; i++) {
+    arr.push(musArr[i] + " plays " + instArr[i]);
+  }
+  return arr;
+}
+
+function johnLennonFacts(factsArr) {
+  var i = 0;
+  while (i < factsArr.length) {
+    factsArr[i] += "!!!";
+    i++;
+  }
+  return factsArr;
+}
+
+function iLoveTheBeatles(num) {
+  var arr = [];
+  do {
+    arr.push("I love the Beatles!");
+    num++
+  } while (num < 15);
+  return arr;
+}
