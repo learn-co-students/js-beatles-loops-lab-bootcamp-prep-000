987317d101763a9291879a5c737d477a0bdd03c9
Joshua Gelberger <joshua_gelberger@alumni.brown.edu>
2018-08-29 12:58:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+function theBeatlesPlay(musicianArray, instrumentArray) {
+  var array = [];
+  
+  for (var i=0; i < musicianArray.length; i++) {
+    var string = musicianArray[i] + ' plays ' + instrumentArray[i];
+    array.push(string);
+  }
+  
+  return array;
+}
+
+function johnLennonFacts(factsArray) {
+  var i = 0;
+  while (i < factsArray.length) {
+    factsArray[i] = factsArray[i] + '!!!';
+    i++;
+  }
+  return factsArray;
+}
+
+function iLoveTheBeatles(n) {
+  var array = [];
+  do {
+    array.push('I love the Beatles!');
+    n++;
+  } while (n < 15);
+  return array;
+}
