7d10ffb3ecc217c712539a0a7dba3b3c72dfa278
Pablo Eusebio <nunieboy88@gmail.com>
2018-08-30 17:55:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // add solution here
+
+
+function theBeatlesPlay(musician, instruments){
+  var array = []
+   for (var i = 0; i < 4; i++){
+     array.push(musician[i] + " plays " + instruments[i]);
+   } 
+   return array
+}
+
+function johnLennonFacts(facts){
+  var array = [];
+  var n = 0;
+  while (n < facts.length) {array.push(facts[n++] + `!!!`)}
+  return array
+}
+
