2d589f07b23ed2af90ffd2859127ddcdf2ccc894
Tanner Strickland <tannerstrickland2000@yahoo.com>
2018-09-01 00:40:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// add solution here
+function theBeatlesPlay(array1,array2){
+  
+  var theBeatlesPlayThis =[];
+  
+  for (var i = 0; i < array1.length; i++) 
+  {
+   theBeatlesPlayThis.push(`${array1[i]}` + " plays " +`${array2[i]}`);
+  
+  }
+  return theBeatlesPlayThis;
+}
+
+
+function iLoveTheBeatles(int) 
+{ var newArray = [];
+  do {
+    newArray.push("I love the Beatles!");
+       int++;
+  } while ([int<15]);
+      return newArray;
+}
