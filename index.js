c0451c675bfea9067cc0f6456e0708c5eb3aff25
kelleynurena@gmail.com Urena <kelleynurena@gmail.com>
2018-09-03 19:09:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// add solution here
+
+function theBeatlesPlay(musician, instrument) {
+  const strings = [];
+  for(var i = 0; i < musician.length; i++) {
+    const sentence = musician[i] + " plays " + instrument[i]
+    strings.push(sentence)
+  }
+  
+    return strings
+}
+
+
+function johnLennonFacts(facts) {
+  const newFacts = [];
+  for(var i = 0; i < facts.length; i++) {
+    const newS = facts[i] + "!!!"
+    newFacts.push(newS)
+  }
+  return newFacts
+  
+}
+
+function iLoveTheBeatles(number) {
+  const earray = []
+  do {
+    number++;
+    earray.push("I love the Beatles!");
+  } while(number < 15);
+  return earray;
+  
+}
+
+
+
