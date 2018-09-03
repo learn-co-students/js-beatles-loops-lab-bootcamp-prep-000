a844780750aaa870b8d61e1dfaf175939411ef7a
Matthew Dizon <matthewdizon94@gmail.com>
2018-09-03 18:49:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var beatles = [];
+  for (var i = 0; i < 4; i++){
+    beatles.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return beatles;
+}
+
+function johnLennonFacts(array) {
+  var i = 0;
+  var exclamation = [];
+  while (i < array.length) {
+    exclamation.push(`${array[i]}!!!`)
+    i++;
+  }
+  return exclamation;
+}
+
+function iLoveTheBeatles(n) {
+  var arr = [];
+  do {
+    arr.push("I love the Beatles!");
+    n++;
+  } while (n < 15);
+  return arr;
+}
