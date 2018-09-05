1667243a67af94562ea40e80b3c59d842c51c931
Vishal D <vishaldiwadkar@protonmail.com>
2018-09-05 04:13:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay(mus, inst) {
+  var arr = [];
+  for (var i=0; i < mus.length; i++) {
+  arr[i] = mus[i] + " plays " + inst[i]   
+  }
+return arr
+}
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  while (i < facts.length) {
+    facts[i] = facts[i] + "!!!"
+    i++;
+  }
+return facts
+}
+
+function iLoveTheBeatles(num){
+  var empArr = [];
+  do {
+      empArr.unshift("I love the Beatles!");
+      num++;
+      }
+  while (num < 15);
+return empArr
+}
