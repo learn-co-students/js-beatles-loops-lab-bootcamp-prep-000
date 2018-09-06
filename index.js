86dfd9dad8b582be79b8d62fcda5233d93596dab
ca.fernando415@gmail.com <ca.fernando415@gmail.com>
2018-09-06 20:35:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var tmp = [];
+  for (let i = 0; i < musicians.length; i++) {
+    tmp[i] = musicians[i] + " plays " + instruments[i];
+  }
+  return tmp;
+}
+function johnLennonFacts(facts) {
+  var tmp = [];
+  let i = 0;
+  while(i < facts.length) {
+    tmp[i] = facts[i++] +"!!!";
+  }
+  return tmp;
+}
+function iLoveTheBeatles(num) {
+  var tmp = [];
+
+  let i = num > 15 ? 15 : (15 - num);
+  do {
+    if (num > 15) {
+      tmp.push("I love the Beatles!");
+      return tmp;
+    } else {
+      tmp.push("I love the Beatles!");
+      i++;
+    }
+  } while (i < 15 || 15%i === 0)
+  return tmp;
+}
