902201da10fe84ef31e576400e311cf9592d1be7
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-06 20:55:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var  newArray = [];
+  for (let i = 0; i < musicians.length; i++){
+    if (i === 0) {
+      newArray.push(`${musicians[0]} plays ${instruments[0]}`);
+    } else {
+      newArray.push(`${musicians[i]} plays ${instruments[i]}`);
+    }
+  }
+  return newArray;
+}
+
+function johnLennonFacts(facts) {
+  let newFacts = [];
+  var i = 0;
+  while (i < facts.length) {
+    newFacts.push(facts[i] + "!!!");
+    i++;
+  }
+  return newFacts;
+}
+
+function iLoveTheBeatles(number) {
+  var newArray = [];
+  do {
+    newArray.push("I love the Beatles!");
+    number++;
+
+  } while (number < 15);
+  return newArray;
+}
