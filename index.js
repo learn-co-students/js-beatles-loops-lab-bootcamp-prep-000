7aeeda99fcffc623c699fc6211c3a721af52715f
Nathan M <nathanminissale@gmail.com>
2018-09-06 10:32:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var newArray = new Array();
+  for (var i = 0; i < musicians.length; i++) {
+    newArray.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return newArray
+}
+
+function johnLennonFacts(facts) {
+  var i = 0
+  while (i < facts.length) {
+    var element = facts[i] + "!!!";
+    facts.splice(i, 1, element);
+    i++
+  }
+  return facts
+}
+
+function iLoveTheBeatles(n) {
+  var newArray = new Array();
+  do {
+    newArray.push("I love the Beatles!")
+  } while (14 > n && n++); // The condition of the loop should check to see that
+                           // the parameter number is less than 15.
+                           // Remember Arrays are 0 indexed so 15 = 14
+  return newArray
+}
