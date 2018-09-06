3000bfb2170911faf2e55aec941e4b1f6b033d8c
Nkosi Phillip <internap627@gmail.com>
2018-09-06 02:39:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // add solution here
+function theBeatlesPlay(mus, ins){
+  
+  var arr = [];
+  for(var i = 0 ; i < mus.length ; i++){
+    arr.push(`${mus[i]} plays ${ins[i]}`);
+  }
+  return arr;
+}
+
+function johnLennonFacts(arr){
+  var count = 0;
+  var facts = "!!!";
+  while(count < arr.length){
+    arr[count] = arr[count]+facts;
+    count++
+  }
+  return arr;
+}
+
+function iLoveTheBeatles(num){
+  var arr = [];
+  do {
+    arr.push("I love the Beatles!");
+    num++
+  }while(num < 15)
+  return arr;
+}
