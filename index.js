f017cc0bac73c822c4c0906d3928d628e6629a49
Matt Readout <matt.readout@gmail.com>
2018-09-07 21:24:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var theBand = [];
+  var i;
+  for (i = 0; i < 4; i++) {
+    theBand.push(musicians[i] + ' plays ' + instruments[i])
+  }
+  return theBand;
+}
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  var numberOfFacts = facts.length;
+  while (i < numberOfFacts) {
+    facts[i] = facts[i] + "!!!";
+    i++;
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(number) {
+  var beatlesArray = [];
+  do {
+    beatlesArray.push('I love the Beatles!');
+    number++;
+  }
+  while (number < 15);
+  return beatlesArray;
+}
