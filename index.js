2dc760558bdd48c6db0424c8f87f3a5e1ce3b418
Jae Park <wjp241@nyu.edu>
2018-09-08 16:33:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  
+  for (var i = 0; i < musicians.length; i++) {
+    var str = musicians[i] + " plays " + instruments[i];
+    array.push(str);
+  }
+  
+  return array;
+}
+
+function johnLennonFacts(array) {
+  var newArr = [];
+  var i = 0;
+  
+  while (i < array.length) {
+    newArr.push(array[i] + "!!!");
+    i++;
+  }
+  
+  return newArr;
+}
+
+
+function iLoveTheBeatles(num) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!");
+    num ++;
+  } while (num < 15);
+  
+  return array;
+}
