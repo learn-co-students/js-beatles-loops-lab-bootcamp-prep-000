21b6d92f489be88c47600d20e9369cf3bb1d147e
Michael Muniz <michael.muniz94@gmail.com>
2018-09-12 04:46:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < 4; i++) {
+    if (i === 0) {
+      array.push(`${musicians[0]} plays ${instruments[0]}`);
+    }
+    else if (i === 1) {
+      array.push(`${musicians[1]} plays ${instruments[1]}`);
+    }
+    else if (i === 2) {
+      array.push(`${musicians[2]} plays ${instruments[2]}`);
+    }
+    else if (i === 3) {
+      array.push(`${musicians[3]} plays ${instruments[3]}`);
+    }
+  }
+  return array;
+}
