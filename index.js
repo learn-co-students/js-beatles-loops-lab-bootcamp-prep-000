dd781fb3a28b7c899838a7259e1f2189319dc9d1
Paul Clark <clark.paul123@gmail.com>
2018-09-15 20:45:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// add solution here
+function theBeatlesPlay(a, b) {
+  var c = [];
+    for (var i = 0; i < a.length; i++) {
+      c.push(a[i] + " plays " + b[i]);
+    }
+  return c;
+}
+
+function johnLennonFacts(array) {
+  var omg = [];
+  for (var i = 0; i < array.length; i++) {
+    omg.push(array[i] + "!!!");
+  }
+  return omg;
+}
+
+function iLoveTheBeatles(num) {
+  var array = [];
+  
+  do {
+        array.push('I love the Beatles!');
+        num++;
+  }
+    while (num < 15);
+    return array;
+}
