76ce08752f4d336cfea8b4aeeea55167c7e581c2
Nasser Sanou <nassersanou23@gmail.com>
2018-09-16 20:33:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// add solution here
+// Musicians & Instruments 
+function theBeatlesPlay(musicians, instruments) {
+  var store1 = []
+  
+  for (let i = 0; i < musicians.length && instruments.length; i++ ) {
+    store1.push(`${musicians[i]} plays ${instruments[i]}`)
+    
+     }
+     return store1
+  }
+  
+
+// FACTS
+function johnLennonFacts(facts) {
+const withBang = []
+
+  var i = 0
+
+  while ( i < facts.length) {
+    withBang.push(facts[i] += '!!!')
+    i +=1 
+  }
+  
+  return withBang
+};
+
+// NUMBER times "I love the Bea...."
+function iLoveTheBeatles(n) {
+  var store2 = [] 
+  
+    do {  store2.push("I love the Beatles!") 
+        n += 1
+       }
+       
+    while ( n < 15) {
+          
+     }
+            
+    return store2
+}
+ 
