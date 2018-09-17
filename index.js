0892d8724f79ae2860ffd36d80b676aeec5af3e7
John Pena <jpena91@gmail.com>
2018-09-17 00:14:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// add solution here
+  function theBeatlesPlay(musicians, instruments) {
+  
+  var emptyarray = [];
+  
+  var arraylength = musicians.length;
+    
+    for (var i = 0; i < arraylength; i++)
+  
+    {emptyarray.push(musicians[i] + " plays " + instruments[i]);
+    
+}
+
+  return emptyarray;
+  
+}
+
+function johnLennonFacts (facts) {
+  var empty = [];
+var i = 0;
+  while (i < facts.length) { 
+    empty.push(`${facts[i]}!!!`);
+   
+    i++;
+  }
+   
+  return empty; 
+
+}
+
+function iLoveTheBeatles(n) {
+  
+  var vacantarray = [];
+    
+   do {
+      vacantarray.push("I love the Beatles!");
+    n++;
+  }
+    while (n < 15);
+    
+    return vacantarray;
+}
