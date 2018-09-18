7e0cbc2c383e8eb6b44bf10247bc7c990d9e724c
Stephen Falck <stephen_falck@hotmail.com>
2018-09-18 17:46:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+
+  for (var i=0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  var array = [];
+  var i = 0;
+
+  while(facts[i]) {
+    array.push(facts[i] + '!!!')
+    i++;
+  }
+  return array;
+}
+
+function iLoveTheBeatles(n) {
+  var array = [];
+
+  do {
+      array.push(`I love the Beatles!`)
+      n++;
+  } while(n < 15)
+  return array;
+}
