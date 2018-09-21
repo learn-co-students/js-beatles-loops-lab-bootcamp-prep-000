2818a3a0996fa6aeecc2ac7b807efc93dc4b87d7
devinkapelanski <devinkapelanski@yahoo.com>
2018-09-21 05:52:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-function theBeatlesPlay(musicians, instruments) {
+function theBeatlesPlay(musician, instrument) {
   var array = [];
-  for (var i = 0; i < musicians.length; i++) {
-    array.push(musicians[i] + " plays " + instruments[i]);
+  for (var i = 0; i < musician.length; i++) {
+    array.push(musician[i] + " plays " + instrument[i]);
   }
   return array;
 }
