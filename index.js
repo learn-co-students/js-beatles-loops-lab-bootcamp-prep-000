89d623b6ae6fb0606311ecc75196fcd70cca2aa9
devinkapelanski <devinkapelanski@yahoo.com>
2018-09-21 05:44:34 +0000

    Done.

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles(number) {
   var array = [];
   do {
-    array.push("I love the Beatles!")
+    array.push("I love the Beatles!");
     number++;
   } while (number < 15);
   return array;
