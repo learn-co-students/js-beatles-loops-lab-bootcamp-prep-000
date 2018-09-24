3ae5ea7e679a6edd13402ba2b31c4b63b50c3fe9
Levi C Uzodike <leviuzodike@gmail.com>
2018-09-24 15:56:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // add solution here
+function theBeatlesPlay(mus, ins){
+  var arr=[]
+  for(var i=0;i<mus.length;i++){
+    arr.push(`${mus[i]} plays ${ins[i]}`)
+  }
+  return arr
+}
+
+function johnLennonFacts(fac){
+  var arr=[]
+  for(var i=0;i<fac.length;i++){
+    arr.push(`${fac[i]}!!!`)
+  }
+  return arr
+}
+
+function iLoveTheBeatles(num){
+  var arr=[]
+  do{
+    arr.push("I love the Beatles!")
+    num++
+  } while(num<15);
+  return arr
+}
