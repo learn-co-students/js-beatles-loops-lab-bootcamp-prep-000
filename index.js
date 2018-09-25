270222c5a117f8f29ec8eaac9004340512bc7a3e
Skylar Salerno <skylar.salerno@gmail.com>
2018-09-25 20:59:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // add solution here
+function theBeatlesPlay(artists,instruments){var bandList=[];for(var i=0;i<artists.length;i++){ bandList.push(`${artists[i]} plays ${instruments[i]}`);}return bandList;}
+function johnLennonFacts(facts){var i=0;while(i<facts.length){facts[i]=String(facts[i])+'!!!';i++}return facts}
+function iLoveTheBeatles(n){var talk=[];do{talk.push("I love the Beatles!");n+=1;} while(n<15);return talk;}
