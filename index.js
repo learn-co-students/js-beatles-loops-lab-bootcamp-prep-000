253d2251a3a9538b2275f22783d8a7f24408478d
Jordan Laird <jordanmack.laird@gmail.com>
2018-09-25 06:25:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// add solution here
+
+function theBeatlesPlay(musicians, instruments){
+  var arr = []
+  for (var i = 0; i <= 3; i++) {
+      arr.push(musicians[i] + " plays " + instruments[i])
+    }
+  return arr;
+}  
+
+function johnLennonFacts(facts) {
+  var string = []
+  var i = 0
+  while (i < facts.length){
+    string.push(facts[i] + "!!!");
+    i++;
+  }
+  return string;
+}
+
+function iLoveTheBeatles(i) {
+  var arr = [];
+  do {
+    arr.push('I love the Beatles!')
+    i++;
+  } while (i < 15);
+  return arr;
+}
