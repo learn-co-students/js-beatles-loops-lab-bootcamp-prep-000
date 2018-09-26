fcbbb3a9290502cde584f7d4acec78f65a1c0801
kdnguyen9088@gmail.com <kdnguyen9088@gmail.com>
2018-09-26 01:46:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // add solution here
+function theBeatlesPlay(arraym,arrayi){
+var em = []
+for (var i = 0; i < arraym.length; i++) {
+  em[i] = arraym[i] + ' plays ' + arrayi[i]
+}
+return em
+}
+
+function johnLennonFacts(facts){
+var i = 0;
+var newfacts=[];
+while (facts.length > i) {
+newfacts.push(facts[i]+"!!!")
+  i++;
+}
+return newfacts
+}
+
+function iLoveTheBeatles(n){
+var em = [];
+do {
+em.push("I love the Beatles!")
+} while (n < 15);
+n++
+}
