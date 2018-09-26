a4d2575af7b8587f3732305297754e97b3818724
Rachel Lum <rlum894@gmail.com>
2018-09-26 15:57:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// add solution here
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
+
+
+function BeatlesLoops (musicians, instruments) {
+  return `${musicians} plays the ${instruments}`
+}
+
+//function theBeatlesPlay (musicians, instruments) {
+  
+}
