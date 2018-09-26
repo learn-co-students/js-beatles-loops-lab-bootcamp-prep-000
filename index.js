a0566ede638232fef1a45124ae053cb6da85267a
Robert Riley <rob_a_riley@yahoo.com>
2018-09-26 18:35:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments){
+  var BeatlesArray = [];
+  var i;
+  for (i = 0; i < musicians.length; i++){
+    BeatlesArray[i] = musicians[i] + " plays " + instruments[i];
+  }
+  return (BeatlesArray);
+}
+
+function johnLennonFacts (facts){
+  var i = 0;
+  while (i < facts.length){
+    facts[i] = facts[i] + "!!!";
+    i++;
+  }
+  return (facts);
+}
+
+function iLoveTheBeatles(number){
+  var BeatleLove = [];
+  do {
+      BeatleLove.push("I love the Beatles!");
+      number++;
+      }
+    while (number < 15);
+    return (BeatleLove);
+}
