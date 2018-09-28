bc0d4d6811dfe1a5ac0805ab40fb86e2c6ccff42
Brian <briandsalemi@gmail.com>
2018-09-28 23:04:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// add solution here
+function theBeatlesPlay(musician, instrument) {
+  var b = [];
+  for (var musicianNum = 0; musicianNum < musician.length; musicianNum++) {
+    b.push(`${musician[musicianNum]} plays ${instrument[musicianNum]}`);
+} 
+  return b;
+} 
+
+function johnLennonFacts()
