d32bf9be14d5493602e0416d4731035de1fd4070
abadawi2 <abadawi@terpmail.umd.edu>
2018-10-01 20:50:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var newArr = [];
+for (var i = 0; i < musicians.length; i++) {
+  newArr.push (musicians[i] + " plays " + instruments[i]);
+}
+return newArr;
+}
+
+function johnLennonFacts (facts){
+  var end = [];
+  var i = 0;
+  while(i < facts.length) {
+    end.push(facts[i] + '!!!')
+    i++;
+  }
+  return end;
+}
+
+function iLoveTheBeatles(number){
+  var empty = [];
+  do {
+    empty.push("I love the Beatles!");
+    number++;
+  }
+  while (number<15);
+  return empty;
+}
