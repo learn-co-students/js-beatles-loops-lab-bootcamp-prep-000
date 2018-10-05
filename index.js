2e5443b91aabd5a533af8c49dc609629e2f98817
Henry Brooke-Dean <henrybrookedean@hotmail.com>
2018-10-05 14:18:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var newArray = []; 
+  for(var i =0; i < musicians.length; i++) {
+    newArray.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return newArray;
+}
