081da7833dae4fab1b681a4fa326465825e165b0
Tona Morales-Calkins <tona.mc@gmail.com>
2018-10-05 00:53:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  function accessBeatle(musicians, index) {
+   console.log(musicians[index]);
+   }
+  function accessInstrument(instruments, index) {
+   console.log(instruments[index]);
+   }
+
+  var bi = [ ];
+    for (let i = 0; i < 4; i++) {
+	    var b = accessBeatle(i);
+	    var n = accessInstrument(i);
+		 bi.push("${b} plays the ${n}.")
+	}
+	
+}
+
+ function johnLennonFacts(array) {
+   let f = 0;
+   while (f < array.length + 1) {
+   var jlf = array[f];
+   ++f;
+   ${jlf} + " !!!";
+   return; 
+		}
+  
+}
+
+
+
+
+function iLoveTheBeatles(number) {
+  var beatleLove = [ ];
+  
+  do {
+    "I love the Beatles!";
+    beatleLove.push("I love the Beatles!");
+    number++;
+  }  
+    
+  while (number < 15);
+  return beatleLove;
+}
+
+
