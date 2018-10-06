e04f16dd465871ccdab87fc83da0fbb4f54f30bd
Phil Hartley <philh725@gmail.com>
2018-10-06 03:36:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // add solution here
+
+var array = []
+
+function theBeatlesPlay(musicians, instruments) {
+  for (var i = 0; i < musicians.length; i ++) {
+    array.push(musicians[i] + ' plays ' + instruments[i])
+    /* `` characters weren't working. i guess thats a problem
+    with needing to update my browser? this works too i guess. */
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  let i = 0
+  while (i < facts.length) {
+    facts[i] = facts[i] + '!!!'
+    i ++
+  }
+  return facts
+}
+
+function iLoveTheBeatles(number) {
+  var newArray = []
+  do {
+    newArray.push("I love the Beatles!");
+    number ++
+  } while (number < 15);
+  return newArray
+}
+
