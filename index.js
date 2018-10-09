c3fbd8374985d0d66139aae2ea8bbe0fe037fc92
dselca <dselca@gmail.com>
2018-10-09 18:42:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 // add solution here
-var musicians = []
-var instruments = []
+var array = []
 
-
-function theBeatlesPlay(array1, array2) {
-  for(var i = 0; i <= array.length; i++){
-    console.log(`${musicians[0]} plays ${instruments[0]}`)
+function theBeatlesPlay(musicians, instruments) {
+  for(var i = 0; i <= musicians.length-1 && instruments.length-1; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
   }
+  return array
+}
+
+function johnLennonFacts(array) {
+  while(array.length >= 0) {
+    array.push('!!!')
+  }
+  return array
 }
