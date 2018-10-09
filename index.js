074653a15b629b6de92b9bce99a1ba438e4a7b8f
dselca <dselca@gmail.com>
2018-10-09 18:14:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // add solution here
+var musicians = []
+var instruments = []
+
+
+function theBeatlesPlay(array1, array2) {
+  for(var i = 0; i <= array.length; i++){
+    console.log(`${musicians[0]} plays ${instruments[0]}`)
+  }
+}
