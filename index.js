e0474e585ecdc4ee76fd3c680d594d45c71e1382
Lauren Piontek <piontekle@gmail.com>
2018-10-12 19:19:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (let i=0; i < musicians.length; i++) {
+  array.push(musicians[i] + " plays " + instruments[i])
+  }
+  return array
+  }
+  
+  function johnLennonFacts (facts) {
+    var results = [];
+    var counter = 0;
+    while (counter < facts.length) {
+      results.push(facts[counter] + "!!!");
+      counter++;
+    }
+    return results;
+  }
+  
+  function iLoveTheBeatles (number) {
+    var array = [];
+    do {
+      array.push("I love the Beatles!")
+      number++
+    } while (number < 15);
+    return array;
+    }
