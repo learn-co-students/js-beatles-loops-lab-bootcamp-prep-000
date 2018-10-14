d3dee10b1dd3cbadfd56550f2f11d64f0e22d830
Samuel Ellis <pleasecastsamellis@gmail.com>
2018-10-14 01:20:07 +0000

    Done.

--- index.js
+++ index.js
@@ -19,11 +19,12 @@ while (i < facts.length){
 }
 return newFacts;
 }
+
 function iLoveTheBeatles(n) {
   var array = [];
   do {
-    array.push = "I love the Beatles!";
+    array.push("I love the Beatles!");
     n++;
-  } while (n > 15);
+  } while (n < 15);
   return array;
 }
