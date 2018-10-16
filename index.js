c26067cc42069eafc790fdc5c65cd60e772183a7
Sarah Anderson <saranderson13@gmail.com>
2018-10-16 17:31:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array;
+}
+
+function johnLennonFacts(array) {
+  var i = 0;
+  var johnLennon = [];
+  while(i < array.length) {
+    johnLennon.push(`${array[i]}!!!`);
+    i++;
+  }
+  return johnLennon;
+}
+
+function iLoveTheBeatles(i) {
+  var theBeatlesAreCool = [];
+  do {
+    theBeatlesAreCool.push("I love the Beatles!");
+    i++;
+  } while (i < 15);
+  return theBeatlesAreCool;
+}
