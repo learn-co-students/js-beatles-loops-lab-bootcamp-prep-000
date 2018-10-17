bb424651a446ef7a4572881d20b5d4f91eb4db5a
Rachel Lum <rlum894@gmail.com>
2018-10-17 19:42:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
-
-
-function BeatlesLoops (musicians, instruments) {
-  return `${musicians} plays the ${instruments}`
+function theBeatlesPlay (musicians, instruments) {
+  var array = []
+  var musiciansLength = musicians.length
+  var instrumentsLength = instruments.length
+  for (var i = 0; i < musiciansLength && instrumentsLength; i++)
+  
+   {array.push(`${musicians[i]} plays ${instruments[i]}`)
+   }
+   return array
 }
 
-//function theBeatlesPlay (musicians, instruments) {
+
+function johnLennonFacts (facts) {
+  var factsLength = facts.length
+  var array = []
+  var i = 0
+  while (i < factsLength) {
+    array.push(`${facts[i++]}!!!`)
+  }
+  return array
+}
+
+
+function iLoveTheBeatles (n){
+  var array = []
+  function incrementVariable(){
+  n = n + 1;
+  }
+  do {
+    incrementVariable(n);
+    array.push(`I love the Beatles!`);
+  }
+  while (n < 15)
   
+  return array
 }
