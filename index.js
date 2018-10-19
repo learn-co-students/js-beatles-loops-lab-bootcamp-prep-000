5f86ffe36e2b1c374f2a0dbdb65c047ba36a37c4
Victoria L Sumner <vicsumner@gmail.com>
2018-10-19 23:50:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var newArray = [];
+  
+  for(var i=0; i< musicians.length; i++){
+    newArray.push(musicians[i]+ " plays "+instruments[i]);
+  }
+   return newArray;
+}
+
+function johnLennonFacts(facts){
+  var jlFacts = [];
+  var i=0;
+  
+ while (i<facts.length){
+    jlFacts.push(facts[i] +"!!!");
+    i++;
+  }
+  return jlFacts;
+}
+
+
+function iLoveTheBeatles (n){
+   var foo = [];
+
+ do{
+    foo.push("I love the Beatles!")
+    n = n + 1;
+ }
+ while (n < 15);
+ return foo;
+}
+
+
