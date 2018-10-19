252d4d26a7a74b89bd17a73257652e43d1b5a33f
dselca <dselca@gmail.com>
2018-10-19 16:47:35 +0000

    Done.

--- index.js
+++ index.js
@@ -2,15 +2,33 @@
 var array = []
 
 function theBeatlesPlay(musicians, instruments) {
-  for(var i = 0; i <= musicians.length-1 && instruments.length-1; i++){
+  for(var i = 0; i <= musicians.length - 1 && instruments.length - 1; i++){
     array.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   return array
 }
 
 function johnLennonFacts(array) {
-  while(array.length >= 0) {
-    array.push('!!!')
+  var newArray = [];
+  var i = 0;
+  while(i < array.length) {
+    newArray.push(`${array[i]}!!!`);
+    i++ ;
   }
+  return newArray
+}
+
+var i = 0;
+
+function incrementVariable() {
+  i = i + 1;
+}
+
+function iLoveTheBeatles(n) {
+  var array = [];
+  do {
+    array.push('I love the Beatles!');
+    n++
+  } while (n < 15 );
   return array
 }
