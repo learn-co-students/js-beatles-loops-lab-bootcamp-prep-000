f1ad748f49f1e5696eec08d3f8f126080d7e737f
Collin Bradley <herescollin@gmail.com>
2018-10-19 20:35:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments){
+  var myVariable = []
+  for (var i = 0; i< musicians.length; i++ ) {  
+   
+    myVariable.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return myVariable
+}
+
+function johnLennonFacts(facts){
+  var factList = []
+  for( var i = 0; i < facts.length; i++){
+    factList.push(`${facts[i]}!!!`)
+  }
+  return factList
+}
+
+function iLoveTheBeatles(num){
+  var beatlesLove = [];
+  do { 
+    beatlesLove.push("I love the Beatles!");
+    num++
+  } while (num < 15);
+  return beatlesLove
+}
