898062d1363f896011001940d70a05111a7f2d97
Cliff Jurgensen <jivefox.002@gmail.com>
2018-10-23 17:36:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var empty = [ ];
+  for (let i = 0; i < 4; i++) {
+      empty.push (`${musicians[i]} plays ${instruments[i]}`);
+  }
+      return empty;
+}
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  while (i < facts.length) {
+    facts[i] = facts[i] + '!!!'
+     i++;
+}
+return facts;
+}
+
+function iLoveTheBeatles(n) {
+  var array = [ ]
+  do {
+    array.push ('I love the Beatles!')
+    n++;
+  } while (n < 15);
+  return array
+}
