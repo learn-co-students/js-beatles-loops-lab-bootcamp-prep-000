d5bbf5b78ba3b5dab2e44e1c3e2c7f927f77418f
Jonathan Koss <jonathan_koss@yahoo.com>
2018-10-23 23:28:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
 function theBeatlesPlay(musicians, instruments) {
-  var array = [] 
+  var array = [];
   for(var i = 0; i < musicians.length; i++) {
-  array.push(`${musicians[i]} plays ${instruments[i]}`)
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
   }
-  return array
+  return array;
 }
 
 function johnLennonFacts(facts) {
-  var i = 0
-  while (i < facts.length) {
-    facts[i] = facts[i] + "!!!"
-    i++
+ var array = [];
+ var i = 0;
+  while(i < facts.length) {
+    array.push(facts[i] + '!!!' );
+    i++;
   }
-  return facts
+  return array;
 }
 
 function iLoveTheBeatles(number) {
-  var array = []
+  var array = [];
   do {
-    array.push("I love the Beatles!")
-    number++
+    array.push('I love the Beatles!');
+    number++;
   }
-  while(number < 15)
-  return array 
+  while(number < 15);
+  return array;
 }
+
+/*
+  describe('iLoveTheBeatles', function() {
+    it("returns an array of 'I love the Beatles!' 8 times when passed the parameter 7 ", function() {
+      expect(iLoveTheBeatles(7)).toEqual(["I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!"]);
+    });
+
+    it("returns an array of 'I love the Beatles!' once when passed the parameter 17", function() {
+      expect(iLoveTheBeatles(17)).toEqual(["I love the Beatles!"]);
+    });
+  });
+
+});
+
+*/
