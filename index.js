2140ff1da4371a66b6727bcd32aa9dc1d748080e
Taimur Shah <tshah@scu.edu>
2018-10-23 21:40:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+var beatlesLoops = []
+
+function theBeatlesPlay(musicians, instruments) {
+    for (var i = 0; i < musicians.length; i++) {
+        beatlesLoops.push(`${musicians[i]} plays ${instruments[i]}`)
+    }
+    return beatlesLoops
+}
+
+function johnLennonFacts(facts) {
+  var newArray = [];
+  var i = 0;
+  while (i < facts.length) {
+    newArray.push(`${facts[i]}!!!`);
+    i++;
+  }
+  return newArray
+}
+
+
+function iLoveTheBeatles(number) {
+  var e = [];
+  do {
+    e.push("I love the Beatles!");
+    number++
+  }
+  while (number < 15);
+  return e
+}
