ff319fe52fc17a9af5ad81e78babecdf811ad4db
Carol Gardiner <carolan.gardiner@gmail.com>
2018-10-29 11:10:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// add solution here
+function theBeatlesPlay(muscians, instruments) {
+  var allMembers = [];
+  for(let i = 0; i < muscians.length; i++) {
+    allMembers.push(`${muscians[i]} plays ${instruments[i]}`);
+  }
+  return allMembers;
+}
+
+function johnLennonFacts(facts) {
+  var newFacts = [];
+  var i = 0; 
+  while(i < facts.length) {
+    newFacts.push(facts[i] + "!!!");
+    i++;
+  }
+  return newFacts; 
+}
+
+function iLoveTheBeatles(number){
+  let beatleLove = []; 
+  do {
+    beatleLove.push("I love the Beatles!");
+    number++;
+  }while(number < 15 );
+  return beatleLove;
+}
