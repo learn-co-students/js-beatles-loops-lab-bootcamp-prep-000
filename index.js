93818da94270bbbef54e3a36e425a2fe34cb5d9a
Wil Cook <wil.cook.is@gmail.com>
2018-10-30 13:23:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // add solution here
+
+function theBeatlesPlay (musicians, instruments) {
+  var whoPlaysWhat = [];
+  for (i = 0; i < musicians.length; i++) {
+    whoPlaysWhat.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return whoPlaysWhat;
+}
+
+
+function johnLennonFacts (array) {
+  while (i < array.length) {
+    array[i] = `${array[i]} !!!`;
+    i++
+  }
+  return array;
+}
