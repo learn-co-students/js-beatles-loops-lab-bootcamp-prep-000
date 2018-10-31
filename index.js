f76c19c6c85d5365611207c9da077c0ad7f8520b
Tommy Curley <thomas.curley93@gmail.com>
2018-10-31 11:09:32 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var play = [];
+
+  for (let i = 0; i < musicians.length; i++) {
+    play.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return play;
+}
+
+function johnLennonFacts(facts) {
+  var funFacts = [];
+  var i = 0;
+
+  while (i < facts.length) {
+    funFacts[i] = facts[i] + "!!!";
+    i++;
+  }
+  return funFacts;
+}
+
+function iLoveTheBeatles(n) {
+  var love = [];
+
+  do {
+    love.push("I love the Beatles!");
+    n++;
+  } while (n < 15)
+
+  return love;
+}
