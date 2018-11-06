db38a91654fa810c404652d98ed97090e5db02d9
Cheng Vue <cvu3@ucsc.edu>
2018-11-06 18:30:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// add solution here
+function theBeatlesPlay(musicianArr, instArr) {
+  var resultArr = [];
+
+  for (var i = 0; i < musicianArr.length; i++) {
+    var musician = musicianArr[i];
+    var instrument = instArr[i];
+    var str = `${musician} plays ${instrument}`;
+
+    resultArr.push(str);
+  }
+
+  return resultArr;
+}
+
+
+function johnLennonFacts(arr) {
+  var lennonFactsArr = [];
+  var i = 0;
+
+  while (i < arr.length) {
+    var lennonFact = arr[i];
+
+    lennonFactsArr.push(lennonFact + '!!!');
+    i++;
+  }
+
+  return lennonFactsArr;
+}
+
+
+function iLoveTheBeatles(n) {
+  var iLoveBeatlesArr = [];
+
+  do {
+    iLoveBeatlesArr.push("I love the Beatles!");
+    n += 1;
+  } while (n < 15);
+
+  return iLoveBeatlesArr;
+}
