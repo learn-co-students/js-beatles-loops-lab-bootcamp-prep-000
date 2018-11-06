232a32d81868def8eacb7f8b6718469e00892301
Szabolcs Veer <futuremove@gmail.com>
2018-11-06 17:14:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// add solution here
+
+
+function theBeatlesPlay(musicians,instruments){
+  var newVar  = [];
+
+  for( var i = 0; i < musicians.length; i++){
+  newVar.push( musicians[i] + " plays " + instruments[i]);
+
+   }
+return newVar;
+}
+
+function johnLennonFacts(facts){
+ var x = 0;
+ var z = [];
+  while(x < facts.length ){
+  z.push(facts[x] + "!!!");
+  x++;
+  }
+ return z;
+ }
+
+function iLoveTheBeatles(n){
+ 
+  var ar1 = [];
+  do { n = n + 1;
+  ar1.push("I love the Beatles!");
+  
+  } while (n < 15);
+  return ar1;
+}
