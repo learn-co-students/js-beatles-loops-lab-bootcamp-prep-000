c654b168ea059a7f0dbc8cf4463e137d97d8acf9
inherited-architect-4472 <elaine.onb@gmail.com>
2018-11-07 11:33:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+
+function theBeatlesPlay(musc, instr){
+  var arr = [];
+  for(var i = 0; i < musc.length; i++){
+    var str = musc[i] + " plays " + instr[i];
+    arr.push(str);
+  }
+  return arr;
+}
+ function iLoveTheBeatles(n){
+   var arr = [];
+   do{
+     arr.push("I love the Beatles!");
+     n++;
+   } while(n < 15)
+   return arr;
+ } 
+
+
+ function johnLennonFacts(facts){
+   var i = 0;
+   var arr =[];
+   while( i < facts.length){
+     arr.push(facts[i] + "!!!")
+     i++;
+   }
+   return arr;
+ }
