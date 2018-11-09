e4c8841a03202c524501041f959cebd441d425f7
Carlo Bruno <carlofbruno@gmail.com>
2018-11-09 08:35:15 +0000

    Done.

--- index.js
+++ index.js
@@ -8,12 +8,6 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 function johnLennonFacts(array) {
-  const facts = [
-    "He was the last Beatle to learn to drive",
-    "He was never a vegetarian",
-    "He was a choir boy and boy scout",
-    "He hated the sound of his own voice"
-  ];
   let i =0;
   while ( i < array.length) {
     array[i] += '!!!';
