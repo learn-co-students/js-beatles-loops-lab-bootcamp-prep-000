a6d6f5b0747db3f6bf5e2be94ead8fa438ceed72
Travis Martin <travisemerymartin@gmail.com>
2018-11-09 01:54:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array1 = []
+  for (var i = 0; i<4; i++) {
+    array1.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array1;
+}
+
+function johnLennonFacts(facts) {
+  var i = facts.length;
+  while (i > 0) {
+    i=i-1;
+    facts[i] = facts[i] + "!!!";
+  }
+  return facts
+}
