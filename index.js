94101d6af38639264f2e146e31ae148449a2f932
Dmitriy Sgibnev <dmitriy.sgibnev@gmail.com>
2018-11-12 07:32:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  const facts = []
+
+  for (let counter = 0; counter < musicians.length; counter++) {
+    facts.push(`${musicians[counter]} plays ${instruments[counter]}`)
+  }
+
+  return facts
+}
+
+function johnLennonFacts(facts) {
+  const exaltedFacts = []
+  let counter = 0
+
+  while (facts.length !== counter) {
+    exaltedFacts.push(`${facts[counter]}!!!`)
+    counter++
+  }
+
+  return exaltedFacts
+}
+
+function iLoveTheBeatles(n) {
+  const array = []
+
+  do {
+    array.push("I love the Beatles!")
+    n++
+  } while (n < 15)
+
+  return array
+}
