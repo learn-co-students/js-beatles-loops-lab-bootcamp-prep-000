92910bfac253ad7063dab5d4e26b96e140dcc600
Raymond Chow <raychow1944@yahoo.com>
2018-11-16 05:27:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var arr = [];
+  for(let i = 0; i < musicians.length; i++) {
+    let temp = `${musicians[i]} plays ${[instruments[i]]}`;
+    arr.push(temp);
+  }
+  return arr;
+}
+
+function johnLennonFacts(facts) {
+  let size = facts.length;
+  while(size >= 0) {
+    facts[size-1] = facts[size-1] + "!!!";
+    size--; 
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(n) {
+  let arr = [];
+  do{
+    arr.push("I love the Beatles!");
+    n++;
+  } while(n < 15);
+  return arr;
+}
