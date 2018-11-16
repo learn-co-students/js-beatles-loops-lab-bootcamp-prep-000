5d27a376978444b8352ea251bc9fc5a1b709d4b7
Tali Scheer <tali.scheer@gmail.com>
2018-11-16 17:03:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+function theBeatlesPlay(arr1, arr2) {
+ var newArr = [];
+  for (var i = 0; i < arr1.length; i++) {
+  var str = arr1[i] + ' plays ' + arr2[i];
+  newArr.push(str);
+  }
+  return newArr;
+}
+
+function johnLennonFacts(factsArr){
+  var newArr = [];
+  var i = 0;
+  while (i < factsArr.length)
+  {
+    var str = factsArr[i++] + '!!!';
+    newArr.push(str);
+  }
+  return newArr;
+}
+
+function iLoveTheBeatles(x){
+  var newArr = [];
+  do {
+    newArr.push('I love the Beatles!');
+    x++;
+    }
+  while (x < 15);
+    return newArr;
+}
