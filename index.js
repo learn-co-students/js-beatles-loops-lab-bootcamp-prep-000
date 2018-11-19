dc1478f7eba6e4dc7f58c7be53f71dbbf8a0ed75
Jonathan Koss <jonathan_koss@yahoo.com>
2018-11-19 04:07:50 +0000

    Done.

--- index.js
+++ index.js
@@ -6,16 +6,15 @@ function theBeatlesPlay(musicians, instruments) {
   return array;
 }
 
+function johnLennonFacts(facts) {
+  var array = [];
+  for(var i = 0; i < facts.length; i++) {
+    array.push(`${facts[i]}!!!`); 
+  }
+  return array;
+}
 
 /*
-    describe('Beatles Loops', function() {
-  it("returns an array of strings containing what instruments each musician plays", function() {
-    const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-    const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-
-    expect(theBeatlesPlay(musicians, instruments)).toEqual(["John Lennon plays Guitar", "Paul McCartney plays Bass Guitar", "George Harrison plays Lead Guitar", "Ringo Starr plays Drums"]);
-  });
-
   describe('johnLennonFacts', function(){
     it("returns an array of strings with exclamation points", function() {
       expect(johnLennonFacts([
