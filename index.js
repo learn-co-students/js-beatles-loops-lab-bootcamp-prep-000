ae065550da0a47936bbc5784cb6c1791c3479b2e
Jonathan Koss <jonathan_koss@yahoo.com>
2018-11-19 04:25:10 +0000

    Done.

--- index.js
+++ index.js
@@ -14,23 +14,18 @@ function johnLennonFacts(facts) {
   return array;
 }
 
+function iLoveTheBeatles(number) {
+  var array = [];
+  
+  do {
+    array.push(`I love the Beatles!`);
+    number++;
+  }
+  while(number < 15);
+  return array;
+}
+
 /*
-  describe('johnLennonFacts', function(){
-    it("returns an array of strings with exclamation points", function() {
-      expect(johnLennonFacts([
-        "He was the last Beatle to learn to drive",
-        "He was never a vegetarian",
-        "He was a choir boy and boy scout",
-        "He hated the sound of his own voice"
-      ])).toEqual(["He was the last Beatle to learn to drive!!!", "He was never a vegetarian!!!", "He was a choir boy and boy scout!!!", "He hated the sound of his own voice!!!"]);
-
-      expect(johnLennonFacts([
-        "foo",
-        "bar",
-      ])).toEqual(["foo!!!", "bar!!!"])
-    });
-  });
-
   describe('iLoveTheBeatles', function() {
     it("returns an array of 'I love the Beatles!' 8 times when passed the parameter 7 ", function() {
       expect(iLoveTheBeatles(7)).toEqual(["I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!"]);
