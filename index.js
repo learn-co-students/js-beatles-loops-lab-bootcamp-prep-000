78bb27d3f2428e1a7282c0844c4543784df55294
Naenae L <oneprincesslea@gmail.com>
2018-11-20 21:12:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // add solution here
+var musicians = ["jagger","mercury", "madonna"]; 
+
+var instruments = ["drums", "bass", "piano" ]; 
+
+function theBeatlesPlay(musicians, instruments){
+ for (i=0; i<musicians.length; i++){
+   console.log (`${musicians[i]} plays ${instruments[i]}`); 
+ } 
+} 
+
+theBeatlesPlay(musicians, instruments); 
