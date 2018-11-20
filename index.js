b049f6040772aff9d419755fca91041226bff0b3
Jatin Paliwal <jatinpaliwal181@gmail.com>
2018-11-20 09:38:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments)
+{
+  var arr=[]
+  var str
+  for (var i=0;i<musicians.length;i++)
+  {
+    str=musicians[i] +' plays '+ instruments[i];
+    arr[i]=str
+  }
+  return arr
+}
+
+function johnLennonFacts(facts)
+{
+  var i=0
+  var arr=[]
+  while (facts.length>i)
+  {
+    arr.push(`${facts[i]}`+'!!!');
+    i++;
+  }
+  return arr
+}
+
+function iLoveTheBeatles(n)
+{
+  var arr=[]
+  do
+  {
+    n++
+    arr.push('I love the Beatles!')
+  }while (n<15)
+  return arr
+}
