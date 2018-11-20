1458dd0239ad4ec27a3ffbda8369fe3633229e54
khensura.loveel Love EL <khensura.loveel@gmail.com>
2018-11-20 02:06:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // add solution here
+function theBeatlesPlay(array, intstruments) {
+  let newArray = [];
+  
+  for (let i = 0; i < array.length; i++) {
+      
+      newArray.push(`${array[i]} plays ${intstruments[i]}`)
+  }
+   return newArray;
+}
+
+
+function johnLennonFacts(array) {
+  let i = 0;
+ while( i < array.length) {
+   array[i] = array[i] + "!!!";
+   i++;
+ }
+ return array;
+}
+
+
+
+function iLoveTheBeatles(num){
+  let hi = [];
+  do{
+    hi.push("I love the Beatles!");
+    num++
+    
+  } while(num < 15);
+  return hi;
+}
