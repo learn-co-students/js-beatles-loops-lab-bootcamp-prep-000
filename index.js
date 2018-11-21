e4b8b3c6b7651a48bfdb3ea6852cdce3fa3a67b5
Carell Hamil <ayannahamil@gmail.com>
2018-11-21 17:13:23 +0000

    Done.

--- index.js
+++ index.js
@@ -11,23 +11,15 @@ function theBeatlesPlay(musicians, instruments){
 
 //a function which accepts an array of facts about John Lennon
 //and adds !!! to end the of each statement
-const newFacts = [];
-
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
 
 var i = 0;
 
 function johnLennonFacts(facts){
   while (i < facts.length){
-    newFacts[i] = facts[i] + "!!!";
+    facts[i] = facts[i] + "!!!";
     i++;
   }
-  return newFacts;
+  return facts;
 }
 
 //a function iLoveTheBeatles which accepts a number as a parameter
