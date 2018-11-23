e797c7987bb2914119d62b51b0ba028ea95d3b14
Naenae L <oneprincesslea@gmail.com>
2018-11-23 16:34:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
-// add solution here
-/* Create a function johnLennonFacts.
+var beatleStuff=""; 
+var i=0; 
 
-This function will accept one argument, an array of facts about John Lennon:
+function iLoveTheBeatles (i){
+ 
+   do {
+     i=i+1;
+    var beatleStuff = "I love the Beatles!"; 
+    console.log(`${beatleStuff}, like, X ${i}!`);  
+   }  while (i<15); 
+}; 
 
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian", 
-  "He was a choir boy and boy scout", 
-  "He hated the sound of his own voice" 
-];
-Use a while loop to loop over the facts array and add "!!!" to the end of every fact.*/ 
+iLoveTheBeatles(1); 
 
-var lennonFacts = [ "He was the last Beatle to learn to drive",
-  "He was never a vegetarian", 
-  "He was a choir boy and boy scout", 
-  "He hated the sound of his own voice" 
-]; 
-
-function johnLennonFacts () {
-  while (i=0, i<=lennonFacts.length, i++){
-    console.log(`Did you know, ${lennonFacts[i]}!!!`); 
-  } 
-} ; 
-
-johnLennonFacts(lennonFacts); 
