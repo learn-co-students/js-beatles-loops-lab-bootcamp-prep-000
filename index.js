5743aca86fcba3fb8eeb1c98bf0edc70552b85dc
Leonardo Peralta <leo.pm15@gmail.com>
2018-12-03 20:20:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var instrumentsPlayedBy = [];
+
+for (var i = 0; i < musicians.length; i++) {
+      instrumentsPlayedBy.push(musicians[i] + " plays " + instruments[i])
+  }
+return instrumentsPlayedBy;
+}
+
+function johnLennonFacts() {
+  const facts = [
+    "He was the last Beatle to learn to drive",
+    "He was never a vegetarian",
+    "He was a choir boy and boy scout",
+    "He hated the sound of his own voice"
+  ];
+
+  var i = 0;
+  var shoutFacts = [];
+
+  while (i < facts.length) {
+    shoutFacts.push(facts[i] + "!!!");
+    i++;
+  }
+  return shoutFacts;
+}
