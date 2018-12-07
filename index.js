97ea74ad227f3523ffb4f5f00f7d6b7a751ddde9
Jonathan Koss <jonathan_koss@yahoo.com>
2018-12-07 05:51:11 +0000

    Done.

--- index.js
+++ index.js
@@ -17,6 +17,15 @@ function johnLennonFacts(facts) {
   return array;
 }
 
+function iLoveTheBeatles(number) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!");
+    number++;
+  } while(number < 15);
+  return array;
+}
+
 
 /*
   expect(theBeatlesPlay(musicians, instruments)).toEqual(["John Lennon plays Guitar", "Paul McCartney plays Bass Guitar", "George Harrison plays Lead Guitar", "Ringo Starr plays Drums"]);
