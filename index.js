20520e10bc6010a282c66b03012b6c1435c13404
Isabella de Medici <bellamedici242@gmail.com>
2018-12-10 17:41:02 +0000

    Done.

--- index.js
+++ index.js
@@ -2,27 +2,17 @@ function theBeatlesPlay(musicians, instruments) {
   var arr = [];
   for(var i = 0; i < musicians.length; i++) {
     var s = musicians[i] + " plays " + instruments[i];
-     arr.push(s);
+    arr.push(s);
   }
   return arr;
 }
 
 function johnLennonFacts(facts) {
-  var newArray = [];
-  var i = 0;
+  var newArr = [];
+  i = 0;
   while(i < facts.length) {
-   var newFacts = facts[i] + "!!!";
-   newArray.push(newFacts);
-   i++;
+    var newFacts = facts[i] + "!!!";
+    i++;
   }
-  return newArray;
-}
-
-function iLoveTheBeatles(number) {
-  var arr = [];
-    do  { 
-      arr.push("I love the Beatles!"); 
-      number++; 
-  } while(number < 15);
-  return arr;
+  return newArr;
 }
