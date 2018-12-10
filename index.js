695c5683218d94f951dec8410ba53bd9fb998e8e
Lerena Holloway <holloway.lerena@gmail.com>
2018-12-10 17:11:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+function theBeatlesPlay(arrM, arrI){
+  empArr = [];
+  for(i=0; i < arrM.length; i++){
+    str = `${arrM[i]} plays ${arrI[i]}`;
+    empArr.push(str)
+  }
+  return empArr 
+}
+
+function johnLennonFacts(factsArr){
+  let count = 1;
+
+  while(count < factsArr.length){
+    factsArr.push('!!!');
+    ++count;
+  }
+
+}
+
+
+function iLoveTheBeatles(num){
+  empArr = [];
+  do{
+    empArr.push('I love the Beatles!');
+    ++num;
+  }while (num < 15);
+  return empArr
+}
