f15ef5c8567b91013a284157f038f753b846a13a
Jonathan Koss <jonathan_koss@yahoo.com>
2018-12-11 01:28:14 +0000

    Done.

--- index.js
+++ index.js
@@ -6,23 +6,12 @@ function theBeatlesPlay(musicians, instruments) {
   return array;
 }
 
-
 function johnLennonFacts(facts) {
-  var array = [];
-  var i = 0;
+  let i = 0;
+  let array = [];
   while(i < facts.length) {
     array.push(`${facts[i]}!!!`);
-    i++;
   }
-  return array;
-}
-
-function iLoveTheBeatles(number) {
-  var array = [];
-  do {
-    array.push("I love the Beatles!");
-    number++;
-  } while(number < 15);
   return array;
 }
 
