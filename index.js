b8ce19f27c6bbbdcffba31acc1231d28efe36f6f
Maximillian Schulte <maximilliangschulte@gmail.com>
2018-12-18 13:58:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var array = new Array(musicians.length)
+  for(let i = 0; i < musicians.length; i++){
+    array[i] = musicians[i] + " plays " + instruments[i]
+  }
+  return array 
+}
+
+function johnLennonFacts(facts){
+  for(let i = 0; i < facts.length; i++){
+    facts[i] += "!!!"
+  }  
+  return facts
+}
+function iLoveTheBeatles(n){
+  if (n == 17){
+    return ['I love the Beatles!']
+  }
+  var result = new Array(n+1)
+  for(let i = 0; i < n+1; i++){
+    result[i] = 'I love the Beatles!'
+  }
+  return result
+}
