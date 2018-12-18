8846e149cf88fb57ebf9e8d879f9e5e80ba20163
Juan Alorro <jralorro93@gmail.com>
2018-12-18 22:30:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+
+
+function theBeatlesPlay (musicians, instruments) {
+  var empty = [];
+  for (var counter = 0; counter < musicians.length; counter++) {
+    empty.push(`${musicians[counter]} plays ${instruments[counter]}`);
+  }
+  return empty;
+}
+
+function johnLennonFacts (facts) {
+  var empty = [];
+  var i = 0;
+  while (i < facts.length) {
+    empty.push(`${facts[i]}!!!`);
+    console.log(i++);
+  }
+  return empty;
+}
+
+function iLoveTheBeatles(n) {
+  var empty = [];
+  do {
+    empty.push("I love the Beatles!");
+    n++;
+  } while ( n < 15);
+  return empty;
+}
