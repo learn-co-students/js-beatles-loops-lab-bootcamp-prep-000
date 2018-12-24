35aecef4dd97191b04bf833def2e0b0e077059a6
Hemalatha M <hemalatha.psna@gmail.com>
2018-12-24 00:33:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
+
 // add solution here
+
+function theBeatlesPlay(musicians,instruments) {
+  const array= [] ;
+  const x = musicians;
+  const y = instruments;
+  var length=x.length;
+  for( let i=0;i<length;i++)
+  {
+    array.push(`${x[i]} plays ${y[i]}`);
+  }
+  return array;
+}
+
+function johnLennonFacts(array){
+  const temp =[];
+  let i=0;
+  const x=array.length;
+    while(i<x) {
+    temp.push(`${array[i]}!!!`);
+    i++;
+  }  
+  return temp;
+}
+
+/*function iLoveTheBeatles(n){
+  const arr=[];
+    do{
+    arr.push('I love the Beatles!);
+    n++;
+  }while(n<15);
+  return arr;
+}*/
+// I have to check what is wrong with the above code. cant find.That was the code written by me for this lab.Not working
+function iLoveTheBeatles(n) {
+  const arr = []
+
+  do {
+    arr.push('I love the Beatles!')
+    n++
+  } while (n < 15)
+
+  return arr
+}
