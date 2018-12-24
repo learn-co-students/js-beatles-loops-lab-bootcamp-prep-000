96713d8f4f1a7c8688517e02f6147395df6af94b
Rakan Shaker <shaker.rakan@gmail.com>
2018-12-24 15:44:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var empty = [];
+  for (let i=0; i<musicians.length; i++){
+    empty.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return empty;
+}
+
+function johnLennonFacts (facts){
+  
+}
