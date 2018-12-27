5a07ba27347f5b1ef7b7de7817c81fd505baceb9
Sofia Jonsson <jonssonsofia93@gmail.com>
2018-12-27 07:05:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments){
+ var empty = []; 
+  for (let i= 0; i < 4; i++) {
+  musicians= ['John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Starr'];
+  instruments= ['Guitar', 'Bass Guitar','Lead Guitar','Drums'];
+    empty.push(`${musicians[i]} plays ${instruments[i]}`);
+  }  
+  return empty;
+}
+  
+
+
+function johnLennonFacts(facts){
+var empty= [];
+let i =0;
+while (i <facts.length){
+empty.push(`${facts[i]}!!!`);
+i++;
+}
+return empty;
+}
+
+function iLoveTheBeatles(x) {
+   var emptyArr2 = [];
+   do {
+     emptyArr2.push("I love the Beatles!");
+     x++
+   } while (x < 15);
+   return emptyArr2;
+ } 
+
+
