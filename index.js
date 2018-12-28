4570743d79cac4f09e351bb0e3206f1e75dcd8ad
Aneesh Venuturumilli <veeaneesh@gmail.com>
2018-12-28 03:23:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments){
+  var empty = [];
+  for(var i = 0;i<musicians.length;i++){
+      empty.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return empty;
+}
+
+function johnLennonFacts(facts){
+  var i = 0;
+  while(i<facts.length){
+    facts[i] = facts[i] + "!!!";
+    i +=1;
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(n){
+  var empty = [];
+  do{
+    empty.push("I love the Beatles!");
+    n+=1;
+  }while (n<15);
+  return empty;
+}
