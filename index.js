5703f6074787e2d979f22d254756fbbc85757f0c
shawn a meighn <smeighn@gmail.com>
2019-01-01 19:57:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var beatles = [];
+  for (var i = 0; i < 4; i++){
+    beatles.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return beatles;
+}
+
+function johnLennonFacts(array) {
+  var i = 0;
+  var woo = [];
+  while (i < array.length) {
+    woo.push(`${array[i]}!!!`)
+    i++;
+  }
+  return woo;
+}
+
+function iLoveTheBeatles(n) {
+  var love = [];
+  do {
+    love.push("I love the Beatles!");
+    n++;
+  } while (n < 15);
+  return love;
+}
