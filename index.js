746b435fd8e0afe34e09938875a1be6b790edf89
Jawad Ashour <jawadashour@gmail.com>
2019-01-02 21:58:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var newArray = [];
+  for (let i = 0; i < musicians.length; i++) {
+    newArray.push(musicians[i] + " plays " + instruments[i])
+  }
+  return newArray;
+}
+
+function johnLennonFacts(facts) {
+  var newFacts = [];
+  var i = 0;
+  while (i < facts.length) {
+    newFacts.push(facts[i] + "!!!");
+    i++;
+  }
+  return newFacts;
+}
+
+function iLoveTheBeatles(number) {
+  var loveArray = [];
+  do {
+    loveArray.push("I love the Beatles!");
+    ++number;
+  }
+  while (number < 15);
+  return loveArray;
+}
