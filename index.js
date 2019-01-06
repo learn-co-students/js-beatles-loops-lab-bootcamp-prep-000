6548e6ed1938d6b5a65190fc3b290a93f96a06af
Daniel Gorter <danieljgorter@gmail.com>
2019-01-06 20:48:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var sentences = [];
+  for (let i = 0; i < musicians.length; i++){
+    sentences.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return sentences;
+}
+
+function johnLennonFacts(facts){
+  var superFacts = [];
+  var i = 0;
+  while (i < facts.length){
+    superFacts.push(`${facts[i]}!!!`);
+    i++;
+  }
+  return superFacts;
+}
+
+function iLoveTheBeatles(num){
+  var beatlesLove = [];
+  do {beatlesLove.push('I love the Beatles!'); num++;} while (num < 15);
+  return beatlesLove;
+}
