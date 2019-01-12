bbd1f2df3256f194ec5c429be5a05d1e5e94862d
Joan in DC <joanrigdon@gmail.com>
2019-01-12 23:08:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,35 +1,50 @@
-// add solution here
-
 function theBeatlesPlay(musicians, instruments) {
   var answers= [];
   for (var i = 0; i < musicians.length; i++){
-  answers.push(musicians[i] + ' plays ' + instruments[i]);
-      }
-    return answers;
-    }
-    
-    //second test
-    
-    function johnLennonFacts(facts) {
-// i stands for factsIndex
+    answers.push(musicians[i] + " plays " + instruments[i])
+  }
+  return answers
+}
 
-      var factsWithExclamationPoints = [];
 
-      var i = 0;
-      while (i < facts.length){
-        factsWithExclamationPoints.push(facts[i]+'!!!');
-        i++;
-      }
-      return factsWithExclamationPoints
+//     //second test
+
+function johnLennonFacts(facts) {
+  var answers= [];
+  var i = 0;
+  while (i < facts.length){
+    answers.push(facts[i] + '!!!');
+    i++;
     }
+  return answers
+}
     
-     //third test
-     function iLoveTheBeatles(number) {
-       var answers = [];
-       do {(answers.push('I love the Beatles!') &&
-       number++)}
-       while (number < 15)
-       return answers;
-     }
+//     //third test
+
+function iLoveTheBeatles(number) {
+  var answers= [];
+  do {
+    answers.push('I love the Beatles!');
+    number++;
+  } while 
+      (number < 15 && ((15-number) > 0));
+    return answers
+}
+
+//     function iLoveTheBeatles(number) {
+//       var answers = [];
+// <<<<<<< HEAD
+//       do {(answers.push('I love the Beatles!') &&
+//       number++)}
+//       while (number < 15)
+//       return answers;
+// =======
+//       number = 0;
+//       do {(answers.push('I love the Beatles!') &&
+//       number++)}
+//       while ((15 - number < 15 )
+//       )return answers;
+// >>>>>>> c5ed00e7264656be3832311ba882bd927b956b03
+//     }
      
      
