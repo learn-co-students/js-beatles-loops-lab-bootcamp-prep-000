235cf11e0eb89b6c0d631bc5441e1a94faef5fba
Ben Manekin <ben.manekin@gmail.com>
2019-01-13 23:18:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// add solution here
+function theBeatlesPlay(a, b) {
+  var sentences = []
+  for (var i = 0; i < b.length; i++) {
+  sentences.push(`${a[i]} plays ${b[i]}`);
+  }
+  return sentences
+}
+
+function johnLennonFacts(a) {
+  var i = 0
+  while (i < a.length) {
+    var newThing = a.push("!!!");
+    i++;
+  }
+  return newThing;
+}
+
+function iLoveTheBeatles(n) {
+  var array = []
+  do {
+    array.push("I love the Beatles!");
+    n++;
+  } while (n < 15)
+  return array
+}
