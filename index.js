048759b425e4972dbbec7c21a5b7cf7340fc404a
Max Powell <max_f_powell@hotmail.co.uk>
2019-01-19 17:45:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  for (var i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+}
+
+function johnLennonFacts(array) {
+  var newArray = []
+  let count = 0
+  while (count < array.length) {
+    newArray.push(array[count] +  '!!!')
+    count++
+  }
+  return newArray
+}
+
+function iLoveTheBeatles(number) {
+  var array = []
+  do {
+    array.push('I love the Beatles!')
+    number += 1
+  } while (number < 15)
+  return array
+}
