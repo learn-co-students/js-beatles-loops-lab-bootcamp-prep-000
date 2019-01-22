6df4c86c41a908ac78d401dcc9074e8efcadab78
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-01-22 20:56:20 +0000

    Done.

--- index.js
+++ index.js
@@ -22,8 +22,11 @@ while (i < facts.length) {
 
 function iLoveTheBeatles (number) {
   var love = [];
-  do {love.push("I love the Beatles!")}
+  
+  do {love.push("I love the Beatles!"); number++;}
+  
   while(number < 15)
+  
    return love
 }
 
