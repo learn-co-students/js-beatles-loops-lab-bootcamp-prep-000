db249adc3769a5bd832a2acf3ca540c88553481b
Sean Carter <scarter0313@gmail.com>
2019-01-22 05:20:10 +0000

    Done.

--- index.js
+++ index.js
@@ -12,4 +12,33 @@ function theBeatlesPlay(musicians, instruments) {
   return arr
 }
 
-function iLoveTheBeatles()
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(facts) {
+  const shoutedFacts = []
+  
+  let i = 0
+  while (i < facts.length) {
+    shoutedFacts.push(`${facts[i]}!!!`)
+    i++
+  }
+  return shoutedFacts;
+
+  
+}
+
+function iLoveTheBeatles(n) {
+  const arr = []
+
+  do {
+    arr.push('I love the Beatles!')
+    n++
+  } while (n < 15)
+
+  return arr
+}
