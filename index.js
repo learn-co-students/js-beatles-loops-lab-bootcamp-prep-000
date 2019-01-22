10c58d8363743d336631523fc8b4bd73fa4bd1a7
Sean Carter <scarter0313@gmail.com>
2019-01-22 02:11:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// add solution here
+var musicians = ["John Lennon", "Ringo Starr", "George Harrison", "Paul McCartney"]
+
+var instruments = ["Guitar", "Drums", "Lead Guitar", "Bass Guitar"]
+
+function theBeatlesPlay(musicians, instruments) {
+  const arr = []
+
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return arr
+}
+
+function iLoveTheBeatles()
