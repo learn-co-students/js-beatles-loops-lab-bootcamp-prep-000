1d92eb8e2653235c7b77629f6234297bfb66aa1a
Mahalia Henry-Richards <mahalia_hr@outlook.com>
2019-01-23 00:55:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // add solution here
+function theBeatlesPlay(arrayMusicians, arrayInstruments) {
+  var x = []
+  for(var counter = 0; counter < arrayMusicians.length; counter++ ) {
+    x.push(arrayMusicians[counter] + ' plays ' + arrayInstruments[counter])
+  }
+  return x
+}
+
+function johnLennonFacts(arrayFacts) {
+  var counter = 0
+  var x = []
+  while(arrayFacts.length > counter) {
+    console.log(--counter)
+    x.push(arrayFacts[counter] + '!!!')
+  }
+    return x
+}
