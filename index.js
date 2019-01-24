49ba1424c2378750ca0010e6609366a979b95a8a
cesar mota <cesarmota@ymail.com>
2019-01-24 01:31:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments){
+  var empty=[]
+  
+ for (var index = 0; index< musicians.length; index++) { 
+ var  string= `${musicians[index]} plays ${instruments[index]}`  
+    empty.push(string)
+} 
+  return empty
+}
+
+
+function johnLennonFacts(facts){
+var newfactsarray=facts
+ var i=0
+ while ( newfactsarray.length>i ){
+  newfactsarray.push(newfactsarray[i]+"!!!")
+  i++
+}
+  return newfactsarray
+}
