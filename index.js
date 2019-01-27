04aa5c8baa0cae517155d3296bad648555e599df
kendall willard <kendallwillard@hotmail.com>
2019-01-27 23:31:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  var alpha = []
+  for( var i = 0; i <= musicians.length - 1; i++) {
+    alpha.push(musicians[i] + " plays " + instruments[i])
+  }
+  return alpha;
+}
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  while( i <= facts.length - 1 ) {
+    var newWord = facts[i];
+    newWord += "!!!"
+    facts[i] = newWord
+    i++
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(num) {
+  var alpha = []
+  do {
+    alpha.push("I love the Beatles!")
+    num++
+  } while(num < 15)
+  
+  return alpha
+}
