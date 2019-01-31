5a45329c8053d09321ce3ec146856b25c12ef78e
Christopher Kalfas <chriskalfas@yahoo.com>
2019-01-31 18:23:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // add solution here
+/// #1
+function theBeatlesPlay(musicians, instruments){
+  var theBeatles=[];
+  
+  
+  for (let i=0; i<1; i++){
+ 
+
+
+ 
+ theBeatles.push(`${musicians[0]} plays ${instruments[0]}`);
+theBeatles.push(`${musicians[1]} plays ${instruments[1]}`);
+theBeatles.push(`${musicians[2]} plays ${instruments[2]}`);
+theBeatles.push(`${musicians[3]} plays ${instruments[3]}`);
+}
+return theBeatles;
+
+}   
+
+///#2
+
+function johnLennonFacts(facts){
+   let newArray =[]; 
+   let i= 0
+   
+  while ( i < facts.length); {
+    newArray.push(facts[i] + "!!!" );  
+    i++;
+  }
+
+return newArray;
+}
+
+
+
+
+///#3 
+
+
+//// #4
