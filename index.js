530b83c19c04d945d4697ddcf4b67923c280a642
DANIELLE ALBA <danib.sgh@gmail.com>
2019-02-06 16:22:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// add solution here
+
+function theBeatlesPlay(arrM, arrI){
+let finArr= []
+//console.log(arrI.length)
+for(let i = 0; i < arrM.length; i++){
+  for(let j = 0; j < arrI.length; j++){
+    if(i === j){
+      finArr.push(`${arrM[i]} plays ${arrI[j]}`)
+    }
+  }
+}return finArr
+}
+
+
+
+  function johnLennonFacts(arr)  {
+
+let finArr = []
+var i = 0;
+while(arr.length > i) {
+
+  //console.log(arr[i])
+  finArr.push(`${arr[i]}!!!`)
+  i++
+
+//console.log(finArr);
+
+}return finArr
+  }
+  
+  function iLoveTheBeatles(num){
+  let finArr = [];
+  //let i = 0
+  let text = "I love the Beatles!"
+  
+  do{
+    num++
+    finArr.push(text)
+  }while(num < 15 );
+  return finArr
+}
