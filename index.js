de59dcd48d7f08b45d40b913eee7411e655404af
Monika Tadross <monikk.tadross@gmail.com>
2019-02-11 23:20:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments) {
+ var array = [];
+
+  
+  for (var i = 0; i < musicians.length; i++) {
+    array.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return array;
+}
+
+
+function johnLennonFacts(facts) {
+  var array = [];
+  var i = 0;
+  while (array.length < facts.length) {
+    console.log(array.push(facts[i] + "!!!"))
+    i++;
+  
+  }
+ return array;
+}
+
+
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  var i = 0;
+ 
+  
+  do {array.push("I love the Beatles!");
+    i = i + 1;
+    
+  } while (number < 15);
+  return array;
+}
