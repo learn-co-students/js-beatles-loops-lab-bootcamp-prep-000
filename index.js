e55ccf7bcc96fdf36a2308d5439e01a5dbd9f2a3
Sam Miller <sammillerks@gmail.com>
2019-02-14 04:12:48 +0000

    Done.

--- index.js
+++ index.js
@@ -4,11 +4,33 @@ var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
 
 function theBeatlesPlay(musicians, instruments) {
   let array = [];
-  for (let musicianNumber = 0; musicianNumber <musicians.length; musicianNumber++) {
-    array.pop(musicians[musicianNumber] + "plays")
+  for (let i = 0; i < musicians.length; i++) {
+    array.push(musicians[i] + " plays " + instruments[i])
+    }
+  return array;
   }
-  
-      
-    
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(facts) {
+  var newFacts = [];
+  var i = 0;
+  while (i < facts.length) {
+    newFacts.push(facts[i] + "!!!")
+    i++;
   }
+  return newFacts;
+}
+
+function iLoveTheBeatles(number) {
+  var newArray = [];
+  do {
+    newArray.push("I love the Beatles!");
+  }
+  while (number < 15);
 }
