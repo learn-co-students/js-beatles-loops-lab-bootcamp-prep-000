37808c3dc764452fc08d2af2cc95d0eb4e07cde5
Michael Maggs <michaelmaggs@gmail.com>
2019-02-17 01:04:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
-
-
 function theBeatlesPlay (musicians, instruments) {
   var array = []
-  for (var i = 0; i < musicians.length; i++) {
-   array.push(`${musicians[i]} plays ${instruments[i]}`)
+  for (i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instrument[i]}`
   }
-  return array
 }
 
-function johnLennonFacts (array) {
-  let i = 0
-  while (i < array.length) {
-    array[i] += "!!!"
-    i++
+function theBeatlesPlay (musician, instrument) {
+  var array = []
+  for (i = 0; i < musician.length; i++) {
+    array.push(`${musician[i]} plays )
   }
-return array
-}
-
-function iLoveTheBeatles(num) {
-  var array = [];
-  do {
-    array.push("I love the Beatles!");
-    num++;
-  } while (num < 15);
-  return array
 }
