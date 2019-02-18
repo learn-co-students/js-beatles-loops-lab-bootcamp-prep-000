058e64c27f582e922d5438bfb3c9f980268f928a
Rebecca Cairns <rebecca_cairns@yahoo.com>
2019-02-18 19:57:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// add solution here
+
+function theBeatlesPlay(musicianArr, instrumentArr){
+  var newArr = [];
+  for(let i = 0; i < 4; i++){
+    newArr[i] = musicianArr[i] +  " plays " + instrumentArr[i];
+  }
+  return newArr;
+}
+
+function johnLennonFacts(factArr){
+  var factArrLength = factArr.length;
+  var newArr = [];
+  var incrementer = 0;
+  while(incrementer < factArrLength){
+    newArr.push(factArr[incrementer] + "!!!");
+    incrementer++;
+  }
+  return newArr;
+}
+
+function iLoveTheBeatles(num){
+  var times = 15 - num;
+  var newArr = [];
+  var incrementor = 0;
+  while(incrementor < times){
+    newArr.push("I love the Beatles!");
+    incrementor++;
+  }
+  if(times<1){
+    newArr.push("I love the Beatles!")
+  }
+  return newArr;
+}
