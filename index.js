5c64e30b906909506c52c18cf4cd705e229599d0
Christopher McIntosh <christopher.l.mcintosh@outlook.com>
2019-02-19 22:12:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// add solution here
+function theBeatlesPlay(players, instruments) {
+  var allPlayers = [];
+  for (var i = 0; i < players.length; i++)
+  {allPlayers.push (players[i] + " plays " + instruments[i])
+}
+return allPlayers
+}
+function johnLennonFacts(facts) {
+  var results = [];
+  var i = 0;
+  while (i < facts.length) {
+    results.push(facts[i] + "!!!");
+    i++;
+  }
+  return results;
+}
+function iLoveTheBeatles(num) {
+    var love = [];
+    do {
+        love.push("I love the Beatles!");
+        num++;
+    } while (num < 15);
+
+    return love;
+}
