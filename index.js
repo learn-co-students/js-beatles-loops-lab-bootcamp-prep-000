dee5f9b8f4db66c9f2fbf2f6500af6e79a359bc2
Jake Singer <jacob.b.singer@gmail.com>
2019-02-19 01:36:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var target = []
+  for (var i = 0; i < musicians.length; i++){
+    target.push(musicians[i] + " plays " + instruments[i])
+  }
+  
+  return target
+}
+
+function johnLennonFacts(facts){
+  var target = []
+  var i = 0
+  while(i < facts.length){
+    target.push(facts[i] + "!!!")
+    i ++ 
+  }
+  
+  return target
+}
+
+function iLoveTheBeatles(num){
+  var target = []
+  
+  do{target.push("I love the Beatles!")
+  num ++ 
+  }
+  
+  while (num < 15)
+  
+  
+  return target
+}
+/*
+
+  describe('johnLennonFacts', function(){
+    it("returns an array of strings with exclamation points", function() {
+      expect(johnLennonFacts([
+        "He was the last Beatle to learn to drive",
+        "He was never a vegetarian",
+        "He was a choir boy and boy scout",
+        "He hated the sound of his own voice"
+      ])).toEqual(["He was the last Beatle to learn to drive!!!", "He was never a vegetarian!!!", "He was a choir boy and boy scout!!!", "He hated the sound of his own voice!!!"]);
+
+      expect(johnLennonFacts([
+        "foo",
+        "bar",
+      ])).toEqual(["foo!!!", "bar!!!"])
+    });
+  });
+
+  describe('iLoveTheBeatles', function() {
+    it("returns an array of 'I love the Beatles!' 8 times when passed the parameter 7 ", function() {
+      expect(iLoveTheBeatles(7)).toEqual(["I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!"]);
+    });
+
+    it("returns an array of 'I love the Beatles!' once when passed the parameter 17", function() {
+      expect(iLoveTheBeatles(17)).toEqual(["I love the Beatles!"]);
+    });
+  });
+
+});
+
+*/
