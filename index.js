72772913ec201847cb0ba89bfd08452a43b835fc
Njenga Kariuki <njengak@Njengas-MacBook-Pro-2.local>
2019-02-18 18:13:05 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // add solution here
+// test 1
+const theBeatlesPlay = (artists, instrument) => {
+  let sentArr = [];
+  artists.forEach(artist => {
+    let inst = artists.indexOf(artist)
+    sentArr.push(`${artist} plays ${instrument[inst]}`);
+  });
+return sentArr
+};
+
+
+//test 2
+
+const johnLennonFacts = (array) => {
+  let revArray = [];
+
+  array.forEach(item => {
+    item = item + '!!!';
+    revArray.push(item);
+  });
+  return revArray;
+};
+
+//test 3
+const iLoveTheBeatles = (num) => {
+  let phrase = 'I love the Beatles!'
+  let phraseArr = [];
+  let counter = 0
+
+
+  do {
+    phraseArr.push(phrase);
+    counter +=1
+  }
+  while (counter <= num && num <15);
+  return phraseArr;
+};
