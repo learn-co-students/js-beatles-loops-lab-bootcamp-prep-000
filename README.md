69beb112a20f243dabb62a0391ba42b409b1118a
Adam Minkus <adam.minkus@gmail.com>
2019-02-20 18:24:03 +0000

    Done.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+,
+
 # Beatles Loops
 
 ## Objectives
--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var beatles = []
+for (var i = 0; i < musicians.length; i ++){
+  beatles.push(musicians[i] + ' plays ' + instruments[i])}
+  return beatles
+  }
+  
+function johnLennonFacts(facts){
+  var newFacts = []
+  var i = 0
+  while (newFacts.length < facts.length){newFacts.push(facts[i] + '!!!');
+  i++
+  }
+  return newFacts
+}
+
+function iLoveTheBeatles(number){
+  var hoo = []
+  
+  do {
+    hoo.push('I love the Beatles!')
+    number++
+  }
+   while(number < 15)
+   
+  return hoo
+
+
+
+  
+}
