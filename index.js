da1f6b961713f59059ee9cd4e35ce596cdfe679b
Kenton Archer <ken10archer@gmail.com>
2019-02-25 20:54:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// add solution here
+
+function theBeatlesPlay(musicians, instruments){ // add solution here
+  var beat = []
+  for (var i=0; i < instruments.length; i++ ){
+    beat.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return beat
+}
+  
+  
+  
+function johnLennonFacts(facts){
+  var beat = []
+  for (var i=0; i < facts.length; i++ ){
+    beat.push(`${facts[i]}!!!`)
+  }
+  return beat 
+   //while(facts>0){
+   // console.log(facts++)
+  //}
+  //return facts 
+}
+
+
+function iLoveTheBeatles(n){
+ var love= []
+    
+   do{
+     love.push("I love the Beatles!")
+     n++
+   }while(n<15)
+
+   
+    return love
+}
+  
