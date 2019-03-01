a85dadf52c25755d98b60c3c7925fbff902bbe42
Christopher Cabell <cccabell@icloud.com>
2019-03-01 23:29:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 function theBeatlesPlay(musicians, instruments) {
-  var band = []
+  var band = [];
   for (var counter = 0; counter < musicians.length; counter++) {
-    band[counter] = musicians[counter] + " plays " + instruments[counter]
+    band[counter] = musicians[counter] + " plays " + instruments[counter];
   }
-  return band
+  return band;
 }
 
 function johnLennonFacts(facts) {
-  var counter = 0
+  var counter = 0;
   while (counter < facts.length) {
-    facts[counter] = facts[counter] + "!!!"
-    counter++
+    facts[counter] = facts[counter] + "!!!";
+    counter++;
   }
-  return facts
+  return facts;
 }
 
 function iLoveTheBeatles(num) {
-  var output = []
-  var counter = 15 - num
+  var output = [];
+  var counter = 15 - num;
   do {
-    output.push("I love the Beatles!")
-    counter--
-  } while (counter > 0)
-  return output
+    output.push("I love the Beatles!");
+    counter--;
+  } while (counter > 0);
+  return output;
 }
