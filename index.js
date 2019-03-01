a166b656415711292501b09fec58267ff54543d5
Laura Fretter <fretterlaura@gmail.com>
2019-03-01 18:21:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var newArray = [];
+  for(let i = 0; i < musicians.length; i ++){
+    newArray.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return newArray;
+}
+
+function johnLennonFacts(facts){
+  var i = 0;
+  while( i < facts.length){
+    facts[i] = `${facts[i]}!!!`;
+    i++;
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(num){
+  var love = [];
+  if (num < 15){
+    let i = num;
+    do {
+      i++;
+      love.push("I love the Beatles!");
+    } while (i < 15);
+  }
+  else {
+          love.push("I love the Beatles!");
+  }
+  return love;
+}
