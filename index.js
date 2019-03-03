586ccdbd9a922b0633733c08cc2a67b29d7fca54
grateful-library-0184 <alexcouts18@gmail.com>
2019-03-03 21:17:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments){
+  var beatles = []
+  for (let i = 0; i < 4; i++){
+    beatles.push(musicians[i] + " plays " + instruments[i])
+  }
+  return beatles;
+}
+
+function johnLennonFacts(array){
+  var newFacts = []
+  let i = 0
+  while (i < array.length){
+  newFacts.push(array[i] + "!!!")
+  i++
+  }
+  return newFacts
+}
+
+function iLoveTheBeatles(number){
+  var ilove = []
+  let i = number
+  do {
+    ilove.push("I love the Beatles!")
+    i++
+  } while (i < 15)
+return ilove
+}
+
+
+
