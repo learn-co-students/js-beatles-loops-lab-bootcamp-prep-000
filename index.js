6c682c94f7d5978366e2977af92c2a87d1cca886
Will Smith <smithw1994@gmail.com>
2019-03-03 06:26:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var newA = []
+  for (var i = 0; i < musicians.length; i++) {
+    newA.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return newA
+}
+
+function johnLennonFacts(facts) {
+  let i = 0
+  while (i < facts.length) {
+    facts[i] += '!!!';
+    i++
+  }
+  return facts
+}
+
+function iLoveTheBeatles (number) {
+  var newA = []
+  
+  do {
+    newA.push("I love the Beatles!")
+    number++
+  } while (number < 15); {
+  }
+  return newA
+}
