11dbb95aac9e6856758b62af0ccfdb9d998a5449
Will Jones <willjones2010@gmail.com>
2019-03-04 22:09:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,32 +1,21 @@
-function theBeatlesPlay (musicians,instruments) {
-  var newArray = [];
+function theBeatlesPlay(musicians,instruments) {
+  let newArray = [];
   for (var i = 0; i < musicians.length; i++) {
-    newArray.push(`${musicians[i]} plays ${instruments[i]}`);
+    newArray.push(musicians[i] + ' plays ' + instruments[i]);
+  }
+  return newArray;
 }
-return newArray;
-}
- const facts = [
+function johnLennonFacts(facts) {
+  const facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice"
-  ];
-function johnLennonFacts(array){
-  var newArray = [];
-  var i = 0;
-while (i < array.length) {
-        newArray.push(array[i] +"!!!");
-        i++;
-// for (var i = 0; i < array.length; i ++)
-//     newArray.push(array[i] + '!!!')
-// alternate solution with for loop.
+];
+var i = 0
+newArray = [];
+while (i < facts.length) {
+  newArray.push(facts[i] + '!!!');
+  i++;
 }
-return newArray;
-}
-function iLoveTheBeatles (num) {
-  var newArray = [];
-  var i =  0;
-  do {newArray.push('I love the Beatles!'); num++} 
-  while  (num < 15);
-  return newArray;
 }
