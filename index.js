7945299835161c3450f4104831fce657f5c4b2ec
Nicholas Stano <nicholasstano@gmail.com>
2019-03-10 19:50:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,38 +1,27 @@
 function theBeatlesPlay(musicians, instruments) {
-  
-  const array = [];
-  
-  for (let i = 0, s = musicians.length; i < s; i++)
-  {
-   array.push(`${musicians[i]} plays ${instruments[i]}`)
-       }
+  var array = []
+  for (var i = 0; i < musicians.length; i++) {
+  array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
   return array
 }
 
 function johnLennonFacts(array) {
-  
-const facts = []
-
-let i = 0
-  
-while (i < array.length) {
+  var facts = []
+  let i = 0
+  while (i < array.length) {
   facts.push(`${array[i]}!!!`)
   i++
 }
-
 return facts
-
 }
 
-function iLoveTheBeatles(array) {
-  const love = []
-  
-   do {
-    love.push('I love the Beatles!')
-    array++
-   }
-   while (array < 15) 
- 
-  return love
-  
+function iLoveTheBeatles(n) {
+  var array = []
+  do {
+    array.push("I love the Beatles!")
+    n++
+  }
+    while (n < 15) 
+    return array 
   }
