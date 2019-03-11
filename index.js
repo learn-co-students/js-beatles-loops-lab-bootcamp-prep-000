f796e307c4051cad0bb7400c9f92228b3e549a72
Andrew Waller <andrewhwaller@gmail.com>
2019-03-11 03:06:09 +0000

    Done.

--- index.js
+++ index.js
@@ -9,16 +9,29 @@ function theBeatlesPlay(musicians, instruments) {
 return array
 }
 
-function johnLennonFacts(array) {
-  const facts = [
-    "He was the last Beatle to learn to drive",
-    "He was never a vegetarian",
-    "He was a choir boy and boy scout",
-    "He hated the sound of his own voice"
-];
+function johnLennonFacts(facts) {
+//   let lennonFacts = [
+//     "He was the last Beatle to learn to drive",
+//     "He was never a vegetarian",
+//     "He was a choir boy and boy scout",
+//     "He hated the sound of his own voice"
+// ]
+  const shoutedFacts = []
 
-  while (let i = 0; i < 4; i++) {
-    console.log(`${facts[i]}!!!`);
-
+  let i = 0;
+  while (i < facts.length) {
+    shoutedFacts.push(facts[i] + '!!!');
+    i++;
   }
+  return shoutedFacts
+}
+
+function iLoveTheBeatles(num) {
+  const array = []
+
+  do {
+      array.push('I love the Beatles!')
+      num++
+  } while (num < 15)
+  return array
 }
