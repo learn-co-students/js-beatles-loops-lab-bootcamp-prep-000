c97295bf49f595f6c5f94bce8d63b8b1f1b33a8c
Catherine Batsoula <cbatsoula@gmail.com>
2019-03-12 23:59:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 
-    var newArray = [];
 function theBeatlesPlay (musicians, instruments){
+var newArray = [];
   for (var i = 0; i < musicians.length ; i++){
     var musicianString = musicians[i] + ' plays ' + instruments[i];
     newArray[i] = musicianString ;
-return newArray;
-}
+    
+}return newArray;
 }
 
+function johnLennonFacts (array){
+  var i = 0;
+  var newArray = [];
+  while(array.length > i){
+  newArray.push(`${array[i]}` + "!!!");
+  i++;
+  } return newArray;
+}
