d935db843c0d9b98910bcf1961fa8688ea181c7b
Catherine Batsoula <cbatsoula@gmail.com>
2019-03-12 23:03:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// add solution here
+const musicians = ["John", "Paul", "George", "Ringo"];
+
+const instruments = ["guitar", "guitar", "bass", "drums"];
+
+
+function theBeatlesPlay (musicians, instruments){
+  for (var i = 0; i < musicians.length ; i++){
+    var musicianString = musicians[i] + ' plays ' + instruments[i];
+    var musicianInstrument = [];
+    musicianInstrument[i] = musicianString;
+    
+return musicianInstrument;
+}
+}
+
