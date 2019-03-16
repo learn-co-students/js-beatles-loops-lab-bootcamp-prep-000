1a2749d285223e4e1d19be19852d3b791584655e
Eva Keller <eva.keller1@gmail.com>
2019-03-16 09:22:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 function theBeatlesPlay(musicians, instruments) {
-  var phrases = [];
-  for (var i = 0; i < musicians.length; i++) {
-    phrases.push(musicians[i] + " plays " + instruments[i]);
-  }
-  return phrases;
-}
-
-function johnLennonFacts() {
-  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
   var array = [];
-  var i = 0;
-  while (array.length < facts.length) {
-    array.push(facts[i] + "!!!");
-    i++;
+  for (var i = 0; i < musicians.length; i++) {
+  	array.push(musicians[i] + ' plays ' + instruments[i]);
   }
   return array;
 }
 
-function iLoveTheBeatles(num) {
-  var phrases = [];
+ function johnLennonFacts(facts) {
+  var results = [];
+  var count = 0;
+  while (count < facts.length) {
+    results.push(facts[count] + "!!!");
+    count++;
+  }
+  return results;
+}
+
+ function iLoveTheBeatles(num) {
+  var array = [];
   do {
-    phrases.push("I love the Beatles!");
-    num++
+    array.push("I love the Beatles!")
+    num++;
   } while (num < 15);
-  return phrases;
-} 
+  return array;
+}
