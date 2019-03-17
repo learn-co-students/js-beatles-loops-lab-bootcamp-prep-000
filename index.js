1e3e9c6f8f6c2ae1d7f7162b658f7714b5a56b8f
Ay Has <ahashim983@yahoo.com>
2019-03-17 19:29:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+function theBeatlesPlay(musicians, instruments){
+  var musiciansAndInstruments = []
+  for (var i = 0; i < musicians.length; i++){
+    var sentence = musicians[i] + " plays " + instruments[i]
+    musiciansAndInstruments.push(sentence)
+  }
+  return musiciansAndInstruments
+}
+
+function johnLennonFacts(jLFacts){
+  var i = 0
+  while (jLFacts[i]){
+    jLFacts[i] = jLFacts[i].concat("!!!")
+    i++
+  }
+  return jLFacts
+
+}
+
+function iLoveTheBeatles(num){
+  var beatles = []
+  do {
+    beatles.push("I love the Beatles!")
+    num = num + 1
+  }  while (num < 15)
+
+  return beatles
+}
+
+
+
+
+
+
+
 // add solution here
