b65b123c91d36193b4bfde0b6b1e23643d691b0b
Jessica Pope <jessica.pope.co@gmail.com>
2019-03-20 23:22:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+ var newvar=[];
+ for(var i = 0; i < musicians.length; i++){
+  var musician=musicians[i];
+  var instrument=instruments[i];
+  newvar.push(musician + " plays " + instrument)
+ }
+  return newvar
+  
+}
+
+function johnLennonFacts(facts){
+  var newfacts = []
+  while(facts.length>0){
+    var secvar = facts.shift();
+    newfacts.push(secvar + "!!!")
+    
+  }
+return newfacts}
+
+function iLoveTheBeatles(num){
+  var empty = []
+  do {empty.push("I love the Beatles!")}
+  while(++num<15)
+  return empty
+}
+  
+  
+  
