75509a9b6fed1c398cfde25129d3cb0e6f6fe0d0
Josephine Feely <jojofeely@yahoo.co.uk>
2019-03-20 23:30:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// add solution here
+function theBeatlesPlay(musician, instrument){
+  var array = []
+  for (var i = 0; i < musician.length; i++){
+    array.push(musician[i] + ' plays ' + instrument[i]);
+  }
+  return array
+}
+
+function johnLennonFacts (facts){
+  var arrayFacts = [];
+  var countdown = 0;
+  while (countdown < facts.length){
+    arrayFacts.push(facts[countdown] + '!!!');
+    countdown++;
+  }
+  return arrayFacts
+}
+
+function iLoveTheBeatles(num){
+  var arrayBeatles = [];
+  do {
+    arrayBeatles.push("I love the Beatles!"); num++
+  } while (num < 15);
+  return arrayBeatles
+  }
+  
