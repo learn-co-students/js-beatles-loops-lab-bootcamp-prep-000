95095a54812b57c0cf787229b66482c01240fa64
Dikla Beauliere <dikla.beauliere@gmail.com>
2019-03-28 16:50:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+ var array = []
+  for (let i = 0; i < musicians.length; i++)
+ {
+    var string = `${musicians[i]} plays ${instruments[i]}`
+    array.push(string)
+  }
+  return array
+}
+
+
+function johnLennonFacts (facts){
+ var array = []
+  let i = 0
+  while (i < facts.length) {
+    array.push(`${facts[i]}!!!`)
+    i++
+  }
+  return array
+}
+
+function iLoveTheBeatles(num) {
+  var array = []
+  do {
+    array.push("I love the Beatles!")
+    num++
+  } while (num < 15)
+  return array
+}
