df71b0c5e283dcfbf37619c7629bb51c2e33bcbd
Leonardo Oliveira <leozartino@gmail.com>
2019-03-29 23:44:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+
+function theBeatlesPlay(artistMusicians, artistInstruments){
+  let beattles = []
+  for (let x = 0; x < artistMusicians.length; x++){
+    beattles.push(`${artistMusicians[x]} plays ${artistInstruments[x]}`)
+  }
+  return beattles;
+}
+
+function johnLennonFacts (facts){
+  let vetorFacts = []
+  for (let i = 0; i < facts.length; i++){
+    vetorFacts.push(`${facts[i]}!!!`)
+  }
+  return vetorFacts;
+}
+
+function iLoveTheBeatles(number){
+  let vetorFrases = []
+  do {
+    vetorFrases.push('I love the Beatles!')
+    number += 1
+  } while(number < 15)
+  
+  return vetorFrases;
+}
