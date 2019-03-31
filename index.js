c80d09f732eaeda8bd739b50d35eea4bdd4601fe
Aodhan Vanderbilt <tamegorilla@gmail.com>
2019-03-31 18:47:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  const music = []
+    for(let i = 0; musicians.length(); i++ ){
+    music.push(`${musicians} plays ${instruments}`)
+}
+return music
+}
