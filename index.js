269f956a2f35c535d0371bdd13ac4f30b9f5f421
Abagail Hudson <abagailanne@gmail.com>
2019-03-31 18:17:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var conCat = [];
+  for (var i = 0; i < musicians.length; i++){
+    conCat.push(musicians[i]+ " plays " + instruments[i]);
+  }
+  return conCat;
+}
+function johnLennonFacts(array){
+  var conCatFacts = [];
+  for(var i = 0; i < array.length; i++){
+    conCatFacts.push(array[i]+"!!!")
+  }
+  return conCatFacts;
+}
+
+var i = 0;
+
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
+function iLoveTheBeatles(num){
+var arrayNew = []
+
+if(num !== 17){
+  do{
+      arrayNew.push("I love the Beatles!")
+    }
+  while (incrementVariable() <= num)
+    }
+else{
+    arrayNew.push("I love the Beatles!")
+    }
+return arrayNew
+}
