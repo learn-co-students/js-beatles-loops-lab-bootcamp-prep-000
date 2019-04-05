3ecec460fcb34947811581f4d459334be1034fbe
Ezra Brand <ezrabrand@gmail.com>
2019-04-05 22:42:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// add solution here
+// // add solution here
+//
+//#1 theBeatlesPlay
+
+// function theBeatlesPlay (musiciansArray, instrumentsArray) {
+//
+// let x = []
+//
+// let mLen = musiciansArray.length;
+// let iLen = instrumentsArray.length;
+//
+// for (i = 0; i < mLen; i++)
+// x.push(musiciansArray[0] + ' plays ' instrumentsArray[0])
+// }
+
+// //#2 johnLennonFacts
+//
+// let facts = [
+//   'a',
+//   'b',
+//   'c',
+//   'd'
+// ]
+//
+// function johnLennonFacts (array) {
+//   while ([condition]) {
+//     [loopBody];
+//   }
+//   return array
+// }
+
+//#3 iLoveTheBeatles
+
+// function iLoveTheBeatles (number) {
+//   let arr = [];
+//   do {
+//     arr.push('I love the Beatles!');
+//   } while (number < 15) {
+//     return 'I love the Beatles';
+//     number++;
+//   }
+// }
