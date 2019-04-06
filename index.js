f811276b8a59724aa1aa9cb0f572b040a91f1d88
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-06 19:12:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  array = [];
+  for ( let i = 0; i < musicians.length; i++ ) {
+    array.push(`${musicians} play ${instruments}`);
+  } return array;
+}
+
+function iLoveTheBeatles(number) {
+  array = [];
+  
+}
