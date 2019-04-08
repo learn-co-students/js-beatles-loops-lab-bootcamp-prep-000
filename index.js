72e77376a059567c659cffe8c2560d5ed2b1c05d
Alexandre Silva Dias <alexdias53@gmail.com>
2019-04-08 14:55:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// add solution here
+var array = []
+
+function theBeatlesPlay(musicians, instruments) {
+
+
+for(let i = 0; i < musicians.length; i++){
+         array.push(`John Lennon ` + `plays` + ` Guitar`,'Paul McCartney plays Bass Guitar','George Harrison plays Lead Guitar','Ringo Starr plays Drums' );
+         
+  
+  return array
+  
+}
+}
+
+function johnLennonFacts(facts) {
+ 
+ var container = []
+ 
+  let i = 0
+  
+  while(facts.length > i) {
+    { 
+    
+    container.push(facts[i])  
+    console.log(`${facts[i]"!!!"`);
+    i++
+  }
+}
+}
+
+
+
+
