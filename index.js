83275a1cc91167077e121f2b7c0120ca97cc8fca
Alexandre Silva Dias <alexdias53@gmail.com>
2019-04-08 16:40:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
-var array = []
-
-function theBeatlesPlay(musicians, instruments) {
-
-
-for(let i = 0; i < musicians.length; i++){
-         array.push(`John Lennon ` + `plays` + ` Guitar`,'Paul McCartney plays Bass Guitar','George Harrison plays Lead Guitar','Ringo Starr plays Drums' );
-         
-  
-  return array
-  
-}
+function theBeatlesPlay(players, instruments) {
+    var allPlayers = [];
+    for (var i=0; i < players.length; i++) {
+        allPlayers.push(players[i] + " plays " + instruments[i]);
+    }
+    return allPlayers;
 }
 
-function johnLennonFacts(facts) {
- 
- var container = []
- 
-  let i = 0
-  
-  while(facts.length > i) {
-    { 
-    
-    container.push(`facts[i]"!!!"`)  
-    
-    i++
-  }
-}
+function johnLennonFacts() {
+    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+    var container = [];
+    var i = 0;
+    while (i < facts.length) {
+        container.push(facts[i] +"!!!");
+        i++;
+    }
+    return container;
 }
 
+function iLoveTheBeatles(num) {
+    var love = [];
+    do {
+        love.push("I love the Beatles!");
+        num++;
+    } while (num < 15);
 
+    return love;
+}
 
 
