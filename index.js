67d175bc75d789a6eb9e22c2295aa966777fd159
Ezra Brand <ezrabrand@gmail.com>
2019-04-09 16:15:31 +0000

    Done.

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ let arr = []
 let mLen = musiciansArray.length;
 let iLen = instrumentsArray.length;
 
-for (i = 0; i < mLen; i++)
-console.log(musiciansArray[i],' plays',instrumentsArray[i])
-console.log(arr)
+for (let i = 0; i < mLen; i++) {
+arr.push(`${musiciansArray[i]} plays ${instrumentsArray[i]}`)
+}
+return arr
 }
 
-// //getting the following error: "SyntaxError: missing ) after argument list"
 
 // //#2 johnLennonFacts
 //
@@ -24,12 +24,31 @@ console.log(arr)
 //   'c',
 //   'd'
 // ]
+
+function johnLennonFacts (arr) {
+  let factsWithExc = []
+  let arrLength = arr.length
+  let exc = '!!!'
+  let count = 0
+  while (count<arr.length) {
+       factsWithExc.push(arr[count] += exc);
+       count+=1
+  }
+  return arr
+}
+
+// WHAT FOLLOWS IS FOR LOOP, NOT WHILE LOOP, JUST FOR FUN!:
+
+// function johnLennonFacts(arr) {
+//   let factsWithExc = []
+//   let arrLength = arr.length
+//   let exc = '!!!'
 //
-// function johnLennonFacts (array) {
-//   while ([condition]) {
-//     [loopBody];
+//   for (i=0;i<arr.length;i++) {
+//     factsWithExc.push(arr[i] += exc);
+//
 //   }
-//   return array
+//   return factsWithExc;
 // }
 
 //#3 iLoveTheBeatles
