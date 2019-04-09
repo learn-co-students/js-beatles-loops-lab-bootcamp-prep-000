8cc02d6933745d815c37c206b8586e1796d46f1b
Alexandre Silva Dias <alexdias53@gmail.com>
2019-04-09 11:19:17 +0000

    Done.

--- index.js
+++ index.js
@@ -7,32 +7,18 @@ function theBeatlesPlay(players, instruments) {
 }
 
 
-function johnLennonFacts() {
-    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+function johnLennonFacts(facts) {
+    
+  
     var container = [];
     var i = 0;
     while (i < facts.length) {
-        container.push(facts[i] +"!!!");
+        container.push(`facts[i]` + `!!!`);
         i++;
     }
     return container;
-}
 
-function johnLennonFacts(facts) {
- 
- var container = []
- 
-  let i = 0
-  
-  while(facts.length > i) {
-    { 
-    
-    container.push(`facts[i]"!!!"`)  
-    
-    i++
-  }
-  return container.push
-}
+
 }
 
 function iLoveTheBeatles(num) {
