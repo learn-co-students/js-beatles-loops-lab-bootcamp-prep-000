d1796b87335d37636fbffb473415714435d779af
Alexandre Silva Dias <alexdias53@gmail.com>
2019-04-09 11:33:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(players, instruments) {
-    var allPlayers = [];
+    var allPlayers = []; // Create a variable with an empty array.
     for (var i=0; i < players.length; i++) {
-        allPlayers.push(players[i] + " plays " + instruments[i]);
+        allPlayers.push(players[i] + " plays " + instruments[i]); // The first time through the loop, it should create a string using the first index of the musicians array and the first index of the instruments array:
     }
     return allPlayers;
 }
@@ -9,7 +9,6 @@ function theBeatlesPlay(players, instruments) {
 
 function johnLennonFacts(facts) {
     
-  
     var container = [];
     var i = 0;
     while (i < facts.length) {
@@ -21,14 +20,14 @@ function johnLennonFacts(facts) {
 
 }
 
-function iLoveTheBeatles(num) {
+function iLoveTheBeatles(num) { //The loop should then increment the number passed in as a parameter
     var love = [];
     do {
         love.push("I love the Beatles!");
         num++;
-    } while (num < 15);
+    } while (num < 15); // The condition of the loop should check to see that the parameter number is less than 15
 
-    return love;
+    return love; // Return the array with the strings "I love the Beatles!".
 }
 
 
