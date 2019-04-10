7649de3b512115e1c8648ec4277ba362b42d8c24
Adella <adellakenn@gmail.com>
2019-04-10 00:11:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  const ray = []
+  for(let y = 0; y < musicians.length; y++ ) {
+  ray.push(`${musicians[y]} plays ${instruments[y]}`)
+  }
+  return ray 
+}
+
+//whileLoop johnLennonFacts
+function johnLennonFacts(facts) {
+  const result = [];
+  let x = 0
+  while (x < facts.length) {
+    result.push(`${facts[x]}!!!`);
+    x++
+  }
+return result 
+}
+
+//do-while iLoveTheBeatles
+function iLoveTheBeatles(num) {
+  var result = []
+  do {
+    result.push("I love the Beatles!");
+    num++
+  }
+  while (num < 15);
+  return result
+}
