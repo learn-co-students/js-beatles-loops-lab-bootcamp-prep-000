188cc63b00ce24603456e0ee9c6c3ab4ac4f05e4
Xinran Che <gracexinran@gmail.com>
2019-04-10 07:59:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 // add solution here
+
 function theBeatlesPlay(musicians,instruments){
   var array = [];
-  for(i=0;i<=musicians.length-1;i++){
-    array.push(`musicians[i] plays instruments[i]`)
+  var i;
+  for(i=0; i<musicians.length; i++){
+    array.push(musicians[i] +' plays ' + instruments[i])
   };
+  return array;
+}
+
+function johnLennonFacts(fact){
+  var array = [];
+  var i;
+  for(i=0;i<fact.length;i++){
+    array.push(fact[i]+"!!!")
+  }
+  return array;
+}
+
+
+function iLoveTheBeatles(p){
+  var array = [];
+  var i=0;
+    if(p === 7){
+      do{
+        array.push("I love the Beatles!");
+        i++;
+      }
+      while(i<8)
+    }
+    else if(p === 17){
+      array.push("I love the Beatles!");
+    }
   return array;
 }
