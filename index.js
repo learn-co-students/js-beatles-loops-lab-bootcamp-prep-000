1bf5615385a3fbce177038ab63f5acbe44e360eb
Xinran Che <gracexinran@gmail.com>
2019-04-10 06:09:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments){
+  var array = [];
+  for(i=0;i<=musicians.length-1;i++){
+    array.push(`musicians[i] plays instruments[i]`)
+  };
+  return array;
+}
