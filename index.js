6b7312cf79ecfb2c36e0e40d730b3117a41cc440
peaceful-state-9936 <efua.mensahbrown@gmail.com>
2019-04-16 16:12:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var newMusic= [];
+  for (let i = 0; i < musicians.length; i++) {
+  newMusic.push(musicians[i]+ " plays " + instruments[i]);
+ }
+ return newMusic
+ }
+ 
+ 
+ function johnLennonFacts(facts){
+ var newArray = [];
+  let counter = 0;
+  while (counter < facts.length){
+    newArray.push(facts[counter]+ "!!!");
+    counter++;
+  }
+    return newArray;
+  }
+ 
+
+ function iLoveTheBeatles(number){
+   var newVar=[];
+   
+   do{
+     newVar.push("I love the Beatles!");
+      number++;
+   }while(number < 15)
+  
+     return newVar
+   
+ }
