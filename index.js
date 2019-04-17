ae7f75d0919011c84aef06868897b1b6ccce5646
William Eck <billyeck@gmail.com>
2019-04-17 19:49:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // add solution here
+var a=[];
+function theBeatlesPlay(musicians, instruments){
+  for (var i=0; i<musicians.length; i++){
+    a.push(musicians[i]+" plays "+instruments[i]);
+  }
+  return a;
+}
+
+function johnLennonFacts(trivia){
+  var b=0;
+  while (b<trivia.length){
+    trivia[b]=trivia[b]+"!!!"
+    b++;
+  }
+  return trivia;
+}
+
+function iLoveTheBeatles(n){
+  var ary=[];
+  do {
+    ary.push("I love the Beatles!");
+    n++;
+  } while (n<15)
+  return ary;
+}
