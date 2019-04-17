802616b0996740bd9ad7635bf8c09e4daf94fbb6
Matthew Soto <matthewsoto95@gmail.com>
2019-04-17 15:56:58 +0000

    Done.

--- index.js
+++ index.js
@@ -13,11 +13,23 @@ function theBeatlesPlay(musicians,instruments){
 }
 
 function johnLennonFacts(facts) {
+    var newFacts = [];
     var count = 0;
     while (count < facts.length) {
-        newFacts.push(facts[i] +"!!!");
+        newFacts.push(facts[count] +"!!!");
         count++;
     }
     return newFacts;
 }
 
+function iLoveTheBeatles(n){
+  var empty = [];
+  
+  do{
+    empty.push("I love the Beatles!");
+    n++;
+  }
+  while(n < 15);
+  
+  return empty;
+}
