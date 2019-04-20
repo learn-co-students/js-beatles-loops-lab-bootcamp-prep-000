9f47adcefae89b188a1965a5ca48df609b9a8749
Lawson Hung <lhung1@babson.edu>
2019-04-20 03:16:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  var i;
+  
+  for (let i = 0; i < musicians.length; i++) {
+    array[i] = `${musicians[i]} plays ${instruments[i]}`;
+  }
+  
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  
+  while (i < facts.length) {
+    facts[i] = `${facts[i]}!!!`;
+    i++;
+  }
+  
+  return facts;
+}
+
+function iLoveTheBeatles(num) {
+  var array = [];
+  
+  do {
+    num++;
+    array.push("I love the Beatles!");
+  } 
+  while (num < 15);
+  
+  return array;
+}
