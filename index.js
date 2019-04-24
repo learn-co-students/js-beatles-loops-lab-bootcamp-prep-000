55bed76e05cad77d694863647236cee015132ae0
Danielle Martinoli <daniechris5@gmail.com>
2019-04-24 02:36:38 +0000

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(array) {  
   const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
   const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
-    for (let i= 1; i <4; i++) {
+    for (let i= 1; i <5; i++) {
       array.push(`${musicians} plays ${instruments}`)
   }
   return array
