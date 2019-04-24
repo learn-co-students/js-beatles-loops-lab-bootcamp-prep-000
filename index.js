693a2856b018b33e8afc5b5b4ee3fb5bbba1bb9b
Danielle Martinoli <daniechris5@gmail.com>
2019-04-24 00:44:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// add solution here
+var array = [element]
+function theBeatlesPlay(array) {
+  for() {
+    array.push()
+}
+
+function johnLennonFacts(array) {
+  const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+  while (array.push(`${array} '!!!`))
+  
+  return array
+}
+
+function iLoveTheBeatles(array) {
+ 
+  var i = 0;
+  function incrementVariable() {
+    i = i + 1;
+    return i;
+  }
+  do {
+    console.log("I love the Beatles!");
+  }
+  while (incrementVariable() < 15);
+}
+  
+}
