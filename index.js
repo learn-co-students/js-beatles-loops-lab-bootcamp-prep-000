7ccc9609882cf5088533747cae16d117a6803d1a
Lawrence Yolland <lawrence@sciencedisrupt.com>
2019-04-25 17:16:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // add solution here
+var newArray = [];
+
+function theBeatlesPlay(musicianArray, instrumentArray){
+  for (var i=0; i<musicianArray.length; i++) {
+    newArray[i] = `${musicianArray[i]} plays ${instrumentArray[i]}`
+  }
+  return newArray
+}
+
+function johnLennonFacts(facts){
+  var i=0
+  while (i < facts.length){
+    facts[i] = facts[i]+('!!!')
+    i++
+  }
+  return facts
+}
+
+
+function iLoveTheBeatles(num){
+  var array = [];
+  do{
+    array.push('I love the Beatles!');
+    num++;
+  }
+  while(num<15);
+  return array;
+}
+
+
+
+  
