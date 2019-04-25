903b483a3b13caf41f0980239f3e47eff0abbbaa
Alexander Schelchere <ascellie@gmail.com>
2019-04-25 15:17:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// add solution here
+function theBeatlesPlay(museArr, insArr) {
+  let arr = [];
+  for (let i in museArr) {
+    arr.push(`${museArr[i]} plays ${insArr[i]}`)
+  }
+  return arr;
+}
+
+function johnLennonFacts(arr) {
+  let x = 0;
+  while (x < arr.length) {
+    arr[x] += '!!!';
+    x++;
+  }
+  return arr;
+}
+
+
+function iLoveTheBeatles(num) {
+  let luv = [];
+  do {
+    luv.push("I love the Beatles!");
+    ++num;
+  } while (num < 15);
+  return luv;
+}
