5aa720c9351c8754497c11c2ecdbaaca0126cf44
Laina Karosic <lainakarosic@gmail.com>
2019-04-26 05:15:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // add solution here
+// add solution here
+function theBeatlesPlay (musicians, instruments) {
+  var emptyArray = []
+  for (var i = 0; i < musicians.length; i++) {
+      emptyArray.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return emptyArray;
+}
+
+
+//2)
+function johnLennonFacts(factsArray){
+  var i = 0
+  while (i < factsArray.length){
+factsArray[i]= factsArray[i] + "!!!";
+i++;
+}
+return factsArray;
+}
+
+//3
+function iLoveTheBeatles(number){
+var loveBeatlesArr = []
+do {
+  loveBeatlesArr.push ("I love the Beatles!");
+number++;
+}
+while (number < 15);
+return loveBeatlesArr;
+}
+
+//4
+//function iLoveTheBeatles(number){
+//var loveBeatlesArr = []//
+//do {loveBeatlesArr.push ("I love the Beatles!");
+//number++;
+//}
+//while (number + loveBeatlesArr.length === 15); //less than 15 or equal to number.length ?
+//return loveBeatlesArr;
+//}
