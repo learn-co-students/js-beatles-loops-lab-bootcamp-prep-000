d63e5aef76e8dcc733947a3d368782d61aba4d54
Eliot Howes <ejhowes@gmail.com>
2019-04-28 16:51:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
-  var newArray = [5]
-  for (i = 0, i < 5, i++)
-  newArray[i] = musicians[i].concat(instruments[i]);
+  var newArray = [4]
+  for (var i = 0; i < 4; i++)
+  newArray[i] = musicians[i].concat(" plays " + instruments[i]);
   return newArray
+};
+
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  while (i < facts.length) {
+    facts[i] = facts[i].concat("!!!");
+    i++;
+  }
+  return facts
+};
+
+function iLoveTheBeatles(num) {
+  var array = []
+  var i = 0
+  do {
+    array.push("I love the Beatles!");
+    i++;
+  } while (i <= num && num != 17);
+  return array
 };
