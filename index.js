010d829e625ebf25f6b0bb9f9b6a6fa7c122c07d
Eliot Howes <ejhowes@gmail.com>
2019-04-28 09:29:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var newArray = [5]
+  for (i = 0, i < 5, i++)
+  newArray[i] = musicians[i].concat(instruments[i]);
+  return newArray
+};
