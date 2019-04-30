ab57e7c4fd2f4d4b153f2515e12b49c4a75fb678
modern-channel-7516 <svetlana.rovinsky@gmail.com>
2019-04-30 02:04:51 +0000

    Done.

--- index.js
+++ index.js
@@ -29,13 +29,7 @@ let array=[];
   return array;
 }
 //Beatles Loops iLoveTheBeatles returns an array of 'I love the Beatles!' 8 times when passed the parameter 7
-//Create a function iLoveTheBeatles which accepts a number as a parameter.
-//The function should create a variable that stores an empty array.
-//Implement a do-while loop inside the function that adds "I love the Beatles!" to the empty array.
-//The loop should then increment the number passed in as a parameter.
-//The condition of the loop should check to see that the parameter number is less than 15.
-//It should contain a number of strings that represents the difference between the input number and 15.
-//Return the array with the strings "I love the Beatles!".
+
 function iLoveTheBeatles (num){
   let array= [];
   let i= num;
