7239efd45f518bb20cccffcd7abb9fbf188e4dd6
Esperanza Rodriguez <esperanza.rguezz@gmail.com>
2019-05-01 20:04:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+    var i=0;
+    var myArray = [];
+    var arrayLength = musicians.length;
+    for(i=0;i<arrayLength;i++){
+        myArray[i]=musicians[i]+ ' plays ' +instruments[i];
+    }
+    return myArray;
+}
+
+function johnLennonFacts(array){
+  var i=0;
+  var myArray = [];
+  var arrayLength = array.length;
+  for(i=0;i<arrayLength;i++){
+      myArray[i]=array[i]+ '!!!'
+  }
+  return myArray;
+}
+
+function iLoveTheBeatles(number){
+    var counter=15-number;
+    var i=0;
+    var myArray = [];
+    do{
+      myArray[i]= "I love the Beatles!";
+      i++;
+      counter--;
+    }while(counter>0);
+    return myArray;
+}
