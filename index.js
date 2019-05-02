c725bc2aee193da6522c63a89d0e95949a071710
Peter Sampson <petersampson010@gmail.com>
2019-05-02 18:32:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
-var array1 = ["John Lennon", "Michael Buble", "Peter Sampson"]
-var array2 = ["guitar", "voice", "nothing"]
-
-function theBeatlesPlay(array1, array2) {
-  var array3 = [];
-  for (let i = `0`; i < `4`; i++) {
-    array3.push(`${array1[i]} plays ${array2[i]}`);
-    }
-    return array3;
-}
-
-
-var array4 = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was choir boy and boy scout", "He hated the sound of his own voice"]
-
-function johnLennonFacts(array4) {
-  var i = `0`;
-  var array5 = [];
-  while (i < array4.length) {
-    array5.push(array4[i] + "!!!"); i++;
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  for (let i=0; i<musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
   }
-  return array5
+  return array
 }
 
-function iLoveTheBeatles(i) {
-  var array6 = [];
-  do {
-    array6.push("I love the Beatles!"); i++;
-  } while (i < `15`);
-  return array6;
+function johnLennonFacts(facts) {
+  var i=0
+  var newArray = []
+  while(facts.length>i) {
+    newArray.push(`${facts[i]}!!!`)
+    i++
+  }
+  return newArray
 }
+
+function iLoveTheBeatles(n) {
+  var thisArray = []
+  do { thisArray.push(`I love the Beatles!`)
+  n++
+  } while (n<15)
+  return thisArray
+}
+
+
+
