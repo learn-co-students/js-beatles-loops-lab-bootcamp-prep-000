044fe69596bec9abb718fe82182d337d19feff4a
Zach Parker <parkerz410@gmail.com>
2019-05-03 21:12:46 +0000

    Done.

--- index.js
+++ index.js
@@ -4,18 +4,11 @@ var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
 function theBeatlesPlay(musicians, instruments) {
   var newArray = [];
-  for (i=0; i < musicians.length; i++) {
+  for (let i=0; i < musicians.length; i++) {
     newArray.push(musicians[i] + " plays " + instruments[i])
   }
   return newArray
 };
-
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
 
 function johnLennonFacts(factsArray) {
   var excitedArray = [];
