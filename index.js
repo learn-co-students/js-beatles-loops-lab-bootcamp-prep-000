cba5432bedc66cfac70ea918d9622b70e25e4d42
Jason Leach <jlspursfan@gmail.com>
2019-05-04 01:54:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // add solution here
+// function theBeatlesPlay(musicians, instruments) {
+//   var array = [];
+//   for ( i = 0; i < musicians.length; i++) {
+//     str = ("$(musicians[i]) plays the $(instruments[i])")
+//     array.push(str);
+//   }
+//   return array;
+// }
+
+function iLoveTheBeatles(num) {
+  var array = [];
+  do {
+   num += 1;
+   array.push("I love the Beatles!");
+  } while (num < 15);
+
+  return array;
+}
