b5a93f8ba0d394a7a8b953756c15e483b713a5c2
Rafael Lima <drdverzola@gmail.com>
2019-05-05 22:10:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var mus = []
+  var str = ""
+  for (var i = 0; i < musicians.lenght; i++){
+    str = musicians[i] + "plays" + instruments[i]
+    mus = [...mus, str]
+    // mus = [...mus, musicians[i] + "plays" + instruments[i]]
+  }
+  
+}
