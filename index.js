0243811c6fa5e359c159a0522fdd0c34c79bb372
Jonathan Koss <jonathan_koss@yahoo.com>
2019-05-07 03:09:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+function theBeatlesPlay(musicians, instruments) {
+  let array = [];
+  for(let i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array;
+}
