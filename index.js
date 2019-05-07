be18bdf345bb2adeb2f6254f9f07e2ecafb201a6
salubrious-reference-8531 <philippetimothe@gmail.com>
2019-05-07 19:49:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// add solution here
+function theBeatlesPlay(musicians,instruments){
+var array = [musicians[0] + " plays " +instruments[0]]; 
+  for ( var i = 1; i < 4; i++){
+    if (i < 0){
+    array.push(musicians[0] +" plays "+ instruments[0]) 
+    }else{ 
+    array.push(musicians[i] +" plays "+ instruments[i])
+  }
+    }
+    return array
+}
+
+ 
+function johnLennonFacts(facts){
+ let x = 0;
+ var value = [];
+ while (x < facts.length) {
+  value.push(facts[x] + "!!!"); x++;
+}
+return value;
+}
+
+function iLoveTheBeatles(n){
+  var array = []
+ 
+do {
+  array.push("I love the Beatles!"); n++
+} while (n < 15); 
+
+  return array
+}
+
