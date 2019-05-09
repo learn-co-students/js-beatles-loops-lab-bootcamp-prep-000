7f590a1cd73fafd7ce48cc07a09c8ae95f8910a7
Rafael Lima <drdverzola@gmail.com>
2019-05-09 20:51:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 function theBeatlesPlay(musicians, instruments){
   var mus = []
   var str = ""
-  for (var i = 0; i < musicians.lenght; i++){
-    str = musicians[i] + "plays" + instruments[i]
+  for (var i = 0; i < musicians.length; i++){
+    str = musicians[i] + " plays " + instruments[i]
     mus = [...mus, str]
     // mus = [...mus, musicians[i] + "plays" + instruments[i]]
   }
-  
+  return mus
+}
+
+function johnLennonFacts(fac){
+ var i = 0
+ while (i < fac.length) {
+    fac[i]= fac[i] + "!!!"
+    i++
+  }
+ return fac
+}
+
+function iLoveTheBeatles(num){
+  var b = []
+  do {
+    num = num + 1; //This code will add value 1 to the num variable until reaches the condition to //stop the loop
+    b.unshift("I love the Beatles!");
+    } while (num < 15);
+ return b
 }
