338d2883a4e818d434996bab33b2bd9ad15bc8ff
Emeka, Alex Odiah <alexodiah@gmail.com>
2019-05-13 14:54:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // add solution here
+
+
+  function theBeatlesPlay(musicians, instrument) {
+var empty = [];
+musicians = ["John Lennon", "Spice girls", "Tinnie Tempah","The Beatles"];
+instrument = ['Guitar', 'Microphone', 'The Hood', 'Jazz and Soul'];
+
+for(let m = 0; m < musicians.length; m++)
+{
+
+console.log(musicians[m]+" plays"+" "+instrument[m])
+//return
+}
+
+}
+
+theBeatlesPlay()
