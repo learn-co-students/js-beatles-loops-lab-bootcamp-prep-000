cbffcb8031c2d0be8a6541ba207797dc90c6fabb
Jonathan Koss <jonathan_koss@yahoo.com>
2019-05-13 04:00:02 +0000

    Done.

--- index.js
+++ index.js
@@ -15,3 +15,15 @@ function johnLennonFacts(facts) {
   }
   return array;
 }
+
+function iLoveTheBeatles(number) {
+  let array = [];
+  do {
+    array.push("I love the Beatles!");
+    number++;
+  }
+  while(number < 15); 
+  return array;
+} 
+
+
