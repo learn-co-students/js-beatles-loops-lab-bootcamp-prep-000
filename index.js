479b69cc4eb5d86de1b3eb5fe5a8d804395f89cf
mitchell williams <mscottwilliams1994@gmail.com>
2019-05-23 22:49:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 // add solution here
 var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
-var random = []
-function theBeatlesPlay(musicians, instruments){
-  
+
+
+function theBeatlesPlay(musicians, instruments) {
+  var music = [];
+  for (let i = 0; i < 4; i++) {
+      music.push(`${musicians[i]} plays ${instruments[i]}`);
+    }
+  return music;
+}
+JLfacts = [
+  "H was the last Beatle to learn to Drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"];
+  function johnLennonFacts(JLfacts){
+    var FACTS = []
+    var i = 0 
+    while (i < JLfacts.length){
+      FACTS.push(JLfacts[i] + "!!!")
+      i ++;
+    }
+    return FACTS
+  }
+
+function iLoveTheBeatles(n) {
+  var array = [];
+  do {
+    n = array.push("I love the Beatles!");
+    n++;
+  } while (n < 15); {
+    
+  }
+  return array;
 }
