9d6113f3670c0b5b48bab78c198103970f217d43
mitchell williams <mscottwilliams1994@gmail.com>
2019-05-23 21:32:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // add solution here
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
+var random = []
+function theBeatlesPlay(musicians, instruments){
+  
+}
