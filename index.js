8ff41e3bf37ba6db185f02e137d89813a63555f5
Catherine O'Hara <catherine.ohara@outlook.com>
2019-05-26 23:51:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (let i = 0; i < 4; i++) {
+   array.push(console.log(`${musicians[i]} + " plays " + ${instruments[i]}`));
+  }
+  
+    return array;
+}
