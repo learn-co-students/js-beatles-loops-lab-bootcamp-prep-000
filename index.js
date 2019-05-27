17a1b98cdce66128c96390481ebe0e39932aa910
Catherine O'Hara <catherine.ohara@outlook.com>
2019-05-27 00:03:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
   for (let i = 0; i < 4; i++) {
-   array.push(console.log(`${musicians[i]} + " plays " + ${instruments[i]}`));
+   array.push(`${musicians[i]} plays ${instruments[i]}`);
   }
   
     return array;
