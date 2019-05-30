788eca83ed841ed9aed2d9a62e786ee626b6cfca
Andrew Tack <andrew.tack.recapllc@gmail.com>
2019-05-30 16:08:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments) {
+  var emptyArray = []
+
+  for (let i = 0; i < musicians.length; i++) {
+      emptyArray.push(musicians[i] + " plays " + instruments[i])
+  }
+    
+  return emptyArray
+}
+
+function johnLennonFacts (facts) {
+  var array = []
+  var i = 0
+  while (i < facts.length) {
+    array.push(facts[i] + "!!!")
+    i++
+  }
+  return array
+}
+
+function iLoveTheBeatles (number) {
+  var array = []
+  
+  do { array.push("I love the Beatles!")
+        number++
+  } while (number < 15)
+  
+  return array
+}
