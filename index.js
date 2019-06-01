593705073f6cc1bd6d15df754cbbd231ca5698ae
Andrew Davisson <akdavisson4@gmail.com>
2019-06-01 15:43:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var arr = [];
+  for (let i = 0; i < musicians.length; i++){
+    arr.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  
+  return arr;
+}
+
+function johnLennonFacts(array){
+  var i = 0;
+  var arr = [];
+  while(i < array.length){
+    arr[i] = array[i] + `!!!`;
+    i++;
+  }
+  return arr;
+}
+
+function iLoveTheBeatles(n){
+  var arr = [];
+  do{
+    arr.push('I love the Beatles!');
+    n++;
+  }while(n < 15);
+  
+  return arr;
+}
