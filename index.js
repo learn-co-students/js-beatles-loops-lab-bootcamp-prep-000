8faf5071b03ab923985a2e702172354c6a082165
Nausherwan Khiljee <nausherwan.khiljee@gmail.com>
2019-06-04 02:29:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  for (var i = 0; i < instruments.length; i++) {
+    array.push(musicians[i] + ' plays ' + instruments[i])
+    }
+    return array
+}
+
+function johnLennonFacts(facts) {
+  var sentence = [];
+  var i = 0;
+ 
+  while (i < facts.length) {
+  sentence.push(facts[i] + "!!!");
+  i++;
+  }
+return sentence;
+}
+
+
+
+function iLoveTheBeatles(num) {
+  var empty = []
+  do {
+    empty.push('I love the Beatles!')
+    num++;
+  }
+  while (num < 15)
+  
+  return empty
+}
