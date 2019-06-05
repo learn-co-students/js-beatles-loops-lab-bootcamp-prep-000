dca77f3476d101647e25ed82bcf192a8a456b49a
Emeka, Alex Odiah <alexodiah@gmail.com>
2019-06-05 15:58:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,63 @@
-// add solution here
+// function theBeatlesPlay(musicians, instrument) {
+//   var empty = []
+//     for(let m = 0; m < musicians.length; m++) {
+//       empty.push(`${musicians[m]} plays ${instrument[m]}`)
+//       }
+//   return empty
+   
+// }
+  
+// function johnLennonFacts(array) {
+// var array = array;
+//   var i = 0;
+//   while (i < array.length) {
+//     array[i] = array[i] + "!!!";
+//     i++;
+//   }
+// return(array)
+
+// }
 
 
-  function theBeatlesPlay(musicians, instrument) {
-var empty = [];
-musicians = ["John Lennon", "Spice girls", "Tinnie Tempah","The Beatles"];
-instrument = ['Guitar', 'Microphone', 'The Hood', 'Jazz and Soul'];
-
-for(let m = 0; m < musicians.length; m++)
+function iLoveTheBeatles(number)
 {
-
-console.log(musicians[m]+" plays"+" "+instrument[m])
-//return
-}
-
-}
-
-theBeatlesPlay()
+  var num = 0
+  var fault = 'out of range'
+  var x = 0;
+  var qoute = "I love the Beatles!"
+  function createVariable()
+  {
+            var empty = [];
+     
+            for(x = 0; x < number; x++)
+         { 
+             if(number < 15 )
+          {
+               do 
+         {
+           
+            empty.push(qoute)
+         
+         }                                            
+             while ( empty.length < num)
+            {
+       
+               }
+              } else if(number > 15)
+              
+              { 
+       
+                return qoute
+               
+              }else{
+                break;
+              }
+          
+              }
+         
+                console.log(empty)
+  
+             }
+  
+                  return createVariable()
+              }
