0505884534ca0fe6bf81262513e7a6fd4d559220
Emeka, Alex Odiah <alexodiah@gmail.com>
2019-06-05 17:14:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,63 +1,42 @@
-// function theBeatlesPlay(musicians, instrument) {
-//   var empty = []
-//     for(let m = 0; m < musicians.length; m++) {
-//       empty.push(`${musicians[m]} plays ${instrument[m]}`)
-//       }
-//   return empty
+function theBeatlesPlay(musicians, instrument) {
+  var empty = []
+    for(let m = 0; m < musicians.length; m++) {
+      empty.push(`${musicians[m]} plays ${instrument[m]}`)
+      }
+  return empty
    
-// }
+}
   
-// function johnLennonFacts(array) {
-// var array = array;
-//   var i = 0;
-//   while (i < array.length) {
-//     array[i] = array[i] + "!!!";
-//     i++;
-//   }
-// return(array)
+function johnLennonFacts(array) {
+var array = array;
+  var i = 0;
+  while (i < array.length) {
+    array[i] = array[i] + "!!!";
+    i++;
+  }
+return(array)
 
-// }
+}
 
 
 function iLoveTheBeatles(number)
 {
-  var num = 0
+  var num = number
   var fault = 'out of range'
   var x = 0;
   var qoute = "I love the Beatles!"
-  function createVariable()
-  {
-            var empty = [];
-     
-            for(x = 0; x < number; x++)
-         { 
-             if(number < 15 )
-          {
-               do 
-         {
-           
-            empty.push(qoute)
-         
-         }                                            
-             while ( empty.length < num)
-            {
-       
-               }
-              } else if(number > 15)
-              
-              { 
-       
-                return qoute
-               
-              }else{
-                break;
-              }
-          
-              }
-         
-                console.log(empty)
+  var empty = [];
+ 
+  do {
+  //
+  empty.push(qoute);
+  num++
   
-             }
-  
-                  return createVariable()
-              }
+ 
+} while (num < 15);
+return empty
+
+}
+
+
+
