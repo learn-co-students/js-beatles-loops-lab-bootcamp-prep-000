14fc915ccd8f7ab9167b9aae635602bfc5b01ad0
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-06-06 19:18:03 +0000

    Done.

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ function theBeatlesPlay(musicians, instruments){
   var empty = []
   for (let i = 0; i < musicians.length ; i++){
     empty.push(`${musicians[i]} plays ${instruments[i]}`)
-}
+  }
   return empty
 } 
 function johnLennonFacts(facts){
+  var newArray = []
+  var i = 0
+  while (i<facts.length){
+   newArray.push(`${facts[i]}!!!`)
+   i++
+  }
+  return newArray
+}
+function iLoveTheBeatles(num){
+  var boop = []
   
 }
