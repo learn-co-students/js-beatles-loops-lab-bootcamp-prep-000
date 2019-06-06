a0a0589c6e7aa20a5f01eb2151fcdce4a65d7934
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-06-06 17:04:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var empty = []
+  for (let i = 0; i < musicians.length ; i++){
+    `${musicians[0]} plays ${instruments}`
+}
+  return 
+} 
