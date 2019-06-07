34dbc50accc76c69707e7e260cb82d760e825e4e
verdantkyber <kingjalexa@gmail.com>
2019-06-07 23:12:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // add solution here
+
+function theBeatlesPlay(arr1, arr2){
+  let band = []
+  for (let i =0; i < arr1.length; i++){
+    band[i] = arr1[i] +  " plays " + arr2[i];
+  }
+  return band
+}
+
+function johnLennonFacts(arr){
+  let i =0
+  let newArr = []
+  while (i < arr.length){
+    newArr[i] = arr[i] + "!!!"
+    i++
+  }
+  return newArr
+}
+
+function iLoveTheBeatles(num){
+  let arr = []
+  let i = 0
+  do {
+    arr[i] = "I love the Beatles!"
+    i++
+    num ++
+  }
+  while(num < 15);
+  return arr
+}
