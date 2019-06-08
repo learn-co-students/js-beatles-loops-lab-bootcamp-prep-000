82a4826c6a6915b07b84d023337cf87d43bf6952
Jeremiah Tabb <tabbjeremiah@gmail.com>
2019-06-08 13:59:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var sul = [];
+  for (var i = 0; i < musicians.length; i++) {
+    sul.push(musicians[i] + " plays " +instruments[i]);
+  }
+  return sul;
+}
+
+function johnLennonFacts(facts){
+  var sul = [];
+  var count = 0;
+  while (count <= facts.length -1){
+    sul.push(facts[count] + "!!!");
+    count++;
+  }
+  return sul;
+}
+
+// function johnLennonFacts(facts){
+//   var sul = [];
+//   forEach(function(fact){
+//     sul.push(fact + "!!!");
+//   });
+//   return sul;
+// }
+// function johnLennonFacts(facts){
+//   var sul = [];
+//   var count = 0;
+//   while (count <= facts.length){
+//     sul.push("!!!");
+//     count++;
+//   }
+//   return sul;
+// }
+
+function iLoveTheBeatles(num){
+  var sul = [];
+  do {
+    sul.push("I love the Beatles!");
+    num += 1;
+  } while(num < 15);
+  return sul;
+}
