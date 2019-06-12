f9eb935afd625eb8fbac6e8767210109a428cf90
Christopher Kennedy <chris94kennedy@gmail.com>
2019-06-12 17:10:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var firstArray = [];
+  for (let i = 0; i < 5; i++) {
+    if (i === 0) {
+    firstArray.push(`${musicians[0]} plays ${instruments[0]}`);
+    } else {
+      firstArray.push(`${musicians${i}} plays ${instruments${i}}`);
+    }
+  } 
+  return firstArray;
+}
