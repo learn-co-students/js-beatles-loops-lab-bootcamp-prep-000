282ec857a1e22f744f9a1b542096d036907df1d1
Jordan Winslow <admin@jordanwinslow.me>
2019-07-03 21:42:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// add solution here
+var emptyArray = []
+
+function theBeatlesPlay(musicians, instruments) {
+  // receive 2 arrays
+  for (let arrayIndex = 0; arrayIndex < musicians.length; arrayIndex++){
+    // start at first index in the musicians array and iterate until the last element
+    emptyArray[arrayIndex] = musicians[arrayIndex] + ' plays ' + instruments[arrayIndex]
+    //update the array with the name of the current musician and the instrument they play 
+  }
+  return emptyArray
+}
+function johnLennonFacts(facts){
+  // receive an array of facts about John Lennon
+  let arrayIndex = 0
+  while (arrayIndex < facts.length){
+    facts[arrayIndex] += "!!!"
+    arrayIndex++
+  }
+  return facts
+}
+function iLoveTheBeatles(number){
+  let newArray = []
+  do {
+    newArray.push("I love the Beatles!")
+    number++
+  } while (number < 15)
+  return newArray
+}
