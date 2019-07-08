08b5bebcf65a4ca3ef50f3fc7c5da8d0372fea51
Bello Osagie <osagienoah04@gmail.com>
2019-07-08 19:15:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // add solution here
+function theBeatlesPlay(arrayMusicians, arrayInstruments){
+var results =[];
+for(var i=0; i<arrayMusicians.length; i++){
+
+    results.push(`${arrayMusicians[i]} plays ${arrayInstruments[i]}`);
+}
+
+return results;
+}
+
+theBeatlesPlay(["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"],["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]);
+
+function johnLennonFacts(array){
+  var i = 0;
+  var results =[];
+  while(array[i]){
+    results.push(array[i]+"!!!");
+    i++;
+  }
+  return results;
+}
+
+johnLennonFacts([ 'John Lennon plays Guitar ', 'PaulMcCartney plays Bass Guitar ', 'George Harrison plays LeadGuitar ', 'Ringo Starr plays Drums ' ] )
+
+function  iLoveTheBeatles (number){
+  var number = number + 1
+  var results =[];
+  var i = 0;
+
+  do{
+    i++;
+    results.push("I love the Beatles!");
+  }
+
+  while (i<number);
+  if (number<=8){
+  return (results);
+}
+  else {
+    return (["I love the Beatles!"]);
+  }
+}
+
+iLoveTheBeatles(7)
