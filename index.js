d5a1d90cc2f6696eaef37463ea95d72129978337
effective-role-2041 <steve.alex@hotmail.co.uk>
2019-07-10 11:24:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var array = [];
+  for (var i = 0; i < musicians.length; i++){
+    array[i] = musicians[i] + " plays "+ instruments[i];
+  }
+  return array
+}
+
+function johnLennonFacts(facts){
+  for (var i = 0; i < facts.length; i++){
+    facts[i] = facts[i] + "!!!";
+  }
+  return facts
+}
+
+function iLoveTheBeatles(n){
+  
+}
