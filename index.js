d7126059d37f5cc6dd533c3b7e7fc5cf3ffcd79c
Ahmed Abououf <aabuauf@yahoo.com>
2019-07-14 17:30:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+function theBeatlesPlay(musArray,instArray){
+  var array =[]
+  for (var counter = 0 ; counter < musArray.length ;counter++){
+   array.push( musArray[counter] + " plays "+instArray[counter])
+  }
+  return array
+}
+
+function johnLennonFacts(array){
+ var index = 0
+  while (index<array.length){
+   array[index] = array[index] + "!!!"
+   index = index+1
+  }
+  return array
+}
+
+function iLoveTheBeatles(num){
+  var array = []
+  if (num > 15){
+    return "I love the Beatles!"
+  }
+  do {
+    array.push("I love the Beatles!")
+    num = num+1
+  }while (num<15)
+  return array
+}
