88c0f6b23a6b5690d90182ddb8eff6276589fb24
Aaron Blum <blumaa@gmail.com>
2019-07-17 19:33:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var facts = []
+  for (let counter = 0; counter < 10; counter++){
+    facts.push(musicians, instruments);
+    return facts
+  }
+}
