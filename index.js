26c78dee4bd044e370475bf2869ee764ee7cb0cc
Jack OMalley <john.omalley@wework.com>
2019-07-21 19:32:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// add solution here
+var musicians = ["John Lennon","Paul McCartney","George Harrison","Ringo Starr"]
+var instruments = ["Guitar","Bass Guitar","Lead Guitar","Drums"]
+
+function theBeatlesPlay(musicians,instruments){
+  var answer = []
+  for(var i=0;i<musicians.length;i++){
+    answer.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return answer
+}
+
+function johnLennonFacts(facts){
+  var excitingFacts = [];
+  for(var i=0;i<facts.length;i++){
+    excitingFacts.push(`${facts[i]}!!!`)
+  }
+  return excitingFacts
+}
+
+function iLoveTheBeatles(integer){
+  var love = []
+  do{
+    love.push("I love the Beatles!")
+    integer++
+  }
+  while(integer < 15)
+  return love
+  }
