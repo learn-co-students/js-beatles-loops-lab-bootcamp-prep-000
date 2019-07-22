ef1d05a7ac18505d0837cce1c9f229a90f5d3872
alfedd Fedd <alfedd@crimson.ua.edu>
2019-07-22 04:59:04 +0000

    Done.

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ function theBeatlesPlay(players, instruments) {
     return allPlayers;
 }
  function johnLennonFacts() {
-    var facts = [ 'He was the last Beatle to learn to drive!!!', 'He was never a vegetarian!!!', 'He was a choir boy and boy scout!!!', 'He hated the sound of his own voice!!!' ];
+    const facts = [ "He was the last Beatle to learn to drive",
+  "He was never a vegetarian" ];
     var newFacts = [];
     var i = 0;
     while (i < facts.length) {
-        newFacts.push(facts[i] +"!!!");
+        newFacts.push(facts[i] + "!!!");
         i++;
     }
     return newFacts;
