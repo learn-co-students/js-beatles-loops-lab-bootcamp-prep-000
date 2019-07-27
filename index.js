a34c368dbc14421dce907715bc5c7f0b35e889a7
asynchronous-widget-3412 <rikeiwilliams@gmail.com>
2019-07-27 21:31:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // add solution here
+function theBeatlesPlay(arrayMusic, arrayInst) {
+  var beatlesmusic = [];
+  for (let i = 0; i < arrayMusic.length; i++) {
+    beatlesmusic.push(`${arrayMusic[i]} plays ${arrayInst[i]}`);
+  }
+  return beatlesmusic;
+}
+function johnLennonFacts(array) {
+  var i = 0;
+  var facts = [];
+  while(i < array.length) {
+    facts.push(`${array[i]}!!!`);
+    i++;
+  }
+  return facts;
+}
+function iLoveTheBeatles(num) {
+  var music = [];
+  do {
+    music.push('I love the Beatles!');
+    num++;
+   } while(num < 15);
+   return music;
+}
