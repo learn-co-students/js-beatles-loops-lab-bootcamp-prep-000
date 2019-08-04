516cdea31724a82dcc4f177dee00664e3ab03e2c
Niki Nedelchev <nedelchev.niki@gmail.com>
2019-08-04 09:53:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// add solution here
+var sentences = [];
+
+function theBeatlesPlay(arrayMusicians, arrayInstruments) {
+
+  for (var i = 0; i <arrayMusicians.length; i++) {
+
+    sentences.push(`${arrayMusicians[i]} plays ${arrayInstruments[i]}`);
+
+  }
+
+  return sentences;
+
+}
+
+function johnLennonFacts(arrayFacts) {
+
+  var i = 0;
+
+  while (i < arrayFacts.length) {
+
+    arrayFacts[i] = `${arrayFacts[i]}!!!`;
+    i++;
+
+  }
+
+  return arrayFacts;
+
+}
+
+function iLoveTheBeatles(number) {
+
+  var sentence = [];
+
+  do {
+
+    sentence.push("I love the Beatles!");
+    number++;
+
+  } while (number < 15);
+
+  return sentence;
+
+}
