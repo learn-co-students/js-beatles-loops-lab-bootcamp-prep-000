ea084d3989fa0d38c0eea081227b4ca6000557a8
Barbara Muesing <muesingb@gmail.com>
2019-08-07 15:50:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
+//returns an array of strings containing what instruments each musician plays
 function theBeatlesPlay(musicians, instruments) {
   var a = []
   for (var i=0; i<4; i++) {a[i] = (musicians[i] + " plays " + instruments[i])}
   return a
-  }
-
+};
+//returns an array of strings with exclamation points
 function johnLennonFacts(facts) {
   var b = []
   let x = 0
-  while (x < facts.length) {b.push(`${facts[x]}` + "!!!");
+  while (x < facts.length) {b.push(`${facts[x]}!!!`);
   x++;}
   return b
-  }
-
+};
+//returns an array of 'I love the Beatles!' 15 - num
 function iLoveTheBeatles(num) {
   var c = []
   var d = 0
@@ -20,4 +21,4 @@ function iLoveTheBeatles(num) {
     else {return "I love the Beatles!" }}
   while (d++ < (14-num))
   return c
-}
+};
