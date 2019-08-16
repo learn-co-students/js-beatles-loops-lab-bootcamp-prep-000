efdc81fe3d9fb9b3fc23818adf2ac6e71e6cb428
fortuitous-map-5512 <danielionita88@yahoo.com>
2019-08-16 18:20:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments) {
+  var cantareti = [];
+  for (let i = 0; i < musicians.length; i++) {
+    cantareti.push(musicians[i] + ' plays ' + instruments[i]);
+  }
+  return cantareti;
+}
+
+function johnLennonFacts(facts) {
+  var newfacts=[];
+  var i = 0;
+  while (i < facts.length){
+    newfacts.push(facts[i] + "!!!");
+    i++;
+  }
+  return newfacts;
+}
+
+function iLoveTheBeatles(int) {
+  var array=[];
+  do {int++;
+  array.push('I love the Beatles!');
+    
+  } while (int<15);
+  return array;
+}
