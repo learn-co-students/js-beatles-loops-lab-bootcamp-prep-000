579a0893413d37b8733e8d906571d3b9b446ce4d
Anthony Clark Perfecto <a1clark1a@gmail.com>
2019-08-17 01:02:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // add solution here
+function theBeatlesPlay(musArr, instArr) {
+  
+  let emptyArr = [];
+  let stringHolder = "";
+  for(let i = 0; i < musArr.length; i++)
+  {
+    stringHolder = musArr[i] + ' plays ' +instArr[i];
+    emptyArr[i] =stringHolder;
+  }
+  
+  return emptyArr;
+}
+
+function johnLennonFacts(factsArr){
+  
+let newFacts = [];
+let i = 0;
+ while(i < factsArr.length)
+ {
+   newFacts[i] = factsArr[i] + "!!!";
+   i++;
+ 
+ }
+ 
+ return newFacts;
+}
+
+function iLoveTheBeatles(num){
+  
+  let emptyArr = [];
+  let i = 0;
+  let check = 15 - num;
+  do{
+    emptyArr[i] = "I love the Beatles!";
+    i++;
+    
+  }while(i < check);
+  
+  return emptyArr;
+}
