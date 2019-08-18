39f764f3a35f464b5aebd0e8026be407179f35ac
TShipley <taneymail@gmail.com>
2019-08-18 18:55:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var x = []
+  for (var i = 0; i < musicians.length; i++){
+    x.push(musicians [i] + plays + instruments[i])
+  }
+  return x;
+}
