088ac9e0e4954d77a802ed219c21302c59e35fd8
Ioan Scurtu <ioan.scurtu120@gmail.com>
2019-09-09 14:25:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var arr = [];
+  for (let i = 0 ; i < musicians.length; i++){
+    var statement = musicians[i] + "plays" + instruments[i];
+    arr.push(statement);
+    }
+    return array;
+};
+
+function johnLennonFacts(facts){
+    var factCounter = 0;
+    while(factCounter < facts.length){
+      facts[factCounter] = facts[factCounter] +"!!!";
+      factCounter++;
+    }
+      return facts;
+   }
+
+function iLoveTheBeatles(num){
+  var arr = [];
+  do {
+    arr.push("I love the Beatles!");
+  }while (++num < 15)
+
+  return arr;
+}
