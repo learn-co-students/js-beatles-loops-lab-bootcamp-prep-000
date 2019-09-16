e7bbfc51d5a926b33fc931fb7188e7a14f3c0009
Ioan Scurtu <ioan.scurtu120@gmail.com>
2019-09-16 16:46:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-  function theBeatlesPlay(musicians, instruments){
-    let array = []
-    let n = 0;
-  while (n<4){
-    array.push(musicians[n] + " plays " + instruments[n])
-    n++
-  }
-  return array
-  }
+function theBeatlesPlay(musicians, instruments){
+  let array = []
+  let n = 0;
+while (n<4){
+  array.push(musicians[n] + " plays " + instruments[n])
+  n++
+}
+return array
+}
 
-  function johnLennonFacts(facts){
-    let n=0;
-    let factsLoud = []
-    while(n < facts.length){
-      factsLoud.push(facts[n]+"!!!")
-      n++
-    }
-    return factsLoud
+function johnLennonFacts(facts){
+  let n=0;
+  let factsLoud = []
+  while(n < facts.length){
+    factsLoud.push(facts[n]+"!!!")
+    n++
   }
+  return factsLoud
+}
 
-  function iLoveTheBeatles (n){
-    let loveArray = []
-    loveArray.push("I love the Beatles!");
-    n++
-    while (n<15){
-    loveArray.push("I love the Beatles!");
-    n++
-  }
-  return loveArray
+function iLoveTheBeatles (n){
+  let loveArray = []
+  loveArray.push("I love the Beatles!");
+  n++
+  while (n<15){
+  loveArray.push("I love the Beatles!");
+  n++
+}
+return loveArray
 } // this is a comment
