3e11e11cecd408fcabe2c886724cf7cc6e0fb2e6
keemo <keemowatson1@gmail.com>
2019-09-16 16:14:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments){
+ var one = ["John Lennon plays guitar"]
+  learn
+  
+}
+function theBeatlesPlay(musicians, instruments)
+{ var arr = [];
+for (let i = 0 ; i < musicians.length; i++){
+  var statement = musicians[i] + " plays " + instruments[i]; arr.push(statement); }
+  return arr; 
+  
+} 
+
+
+function johnLennonFacts(facts)
+{ var factCounter = 0; while(factCounter < facts.length){
+  
+  facts[factCounter] = facts[factCounter] +"!!!"; factCounter++; }
+  return facts;
+}
+
+
+
+function iLoveTheBeatles(a){
+  var name =[]
+  do {
+  name.push("I love the Beatles!");
+  a++
+} while (a < 15);
+return name
+  
+
+}
