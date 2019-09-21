b50c232fdd4f52cecee1c3ae153fa63dcdba8694
Mark Kauffman <markkauffman22@gmail.com>
2019-09-21 14:16:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 // add solution here
 
-function theBeatlesPlay(players, instruments) {
-    var allPlayers = [];
-    for (var i=0; i < players.length; i++) {
-        allPlayers.push(players[i] + " plays " + instruments[i]);
+function theBeatlesPlay(musicians, instruments) {
+    var myNewArray = [];
+    for (var i=0; i < musicians.length; i++) {
+        myNewArray.push(musicians[i] + " plays " + instruments[i]);
     }
-    return allPlayers;
+    return myNewArray;
 }
+////////////////////////////////////////////
+/* TEST INPUT ARRAY
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+*/  
 
-function johnLennonFacts() {
-    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+function johnLennonFacts(facts) {
     var newFacts = [];
     var i = 0;
     while (i < facts.length) {
