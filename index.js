89c7e00f8c1da4897f695ac5c732b17482b25808
remi.odufuye <remi.odufuye@gmail.com>
2019-10-03 14:24:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // add solution here
+
+//  Notes 
+//  musicians is the musicians array 
+//  instruments is the instruments array 
+
+
+function theBeatlesPlay(musicians, instruments) {
+  
+  const beatlesArray = [];
+
+  for(let i =0; i < musicians.length; i++) {
+    beatlesArray.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return beatlesArray;
+}
+
+function johnLennonFacts(facts) {
+  const loudFacts = [];
+  let i = 0;
+  while(i<facts.length){
+    loudFacts.push(`${facts[i]}!!!`)
+    i++
+  }
+  return loudFacts;
+}
+
+function iLoveTheBeatles(number) {
+  const iLoveTheBeatlesAarray = [];
+
+  do {
+    iLoveTheBeatlesAarray.push("I love the Beatles!")
+    number++
+  } while(number<15);
+  return iLoveTheBeatlesAarray;
+}
