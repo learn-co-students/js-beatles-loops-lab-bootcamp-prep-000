80fec94c8e8f71800066f11d7f8dcfe720fc6b53
free-writer-5650 <miles.higbie@gmail.com>
2019-10-11 00:29:26 +0000

    Done.

--- index.js
+++ index.js
@@ -8,17 +8,22 @@ function theBeatlesPlay(musicians,instruments){
   }
     return array;
 }
-function johnLennonFacts() {
-  let facts = [
-        "He was the last Beatle to learn to drive",
-        "He was never a vegetarian",
-        "He was a choir boy and boy scout",
-       "He hated the sound of his own voice"];
+function johnLennonFacts(facts) {
+ var newFacts = []
   var i = 0;
     while (i < facts.length) {
-       facts.push(facts[i]+"!!!");
+      newFacts.push(facts[i]+"!!!");
      i++;
     }
-    return facts
+    return newFacts
 }
 
+
+function iLoveTheBeatles(num) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!")
+    num++;
+  } while (num < 15);
+  return array;
+}
