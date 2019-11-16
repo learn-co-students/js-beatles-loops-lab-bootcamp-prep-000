1086276b87aabd336f0710727c633f7742e738a2
Jeff Simon <jeffsbillinfo@gmail.com>
2019-11-16 06:53:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments)
+{
+  var returnArray = [];  
+  for(let counter = 0; counter <= musicians.length-1; counter++)
+  {
+    returnArray.push(`${musicians[counter]} plays ${instruments[counter]}`);
+  }
+  return returnArray;
+}
+
+function johnLennonFacts(facts)
+{
+  let counter = 0;
+  while(facts[counter] !== undefined)
+  {
+    facts[counter] = facts[counter] + '!!!';
+    counter++;
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(number)
+{
+  let returnArray = [];
+  do
+  {
+    returnArray.push("I love the Beatles!");
+    number++;
+  } while(number < 15)
+  return returnArray;
+  
+}
