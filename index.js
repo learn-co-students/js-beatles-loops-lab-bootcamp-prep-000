201e4d8d98674057a2b2e4e9474057ad02158267
Amy Franz <amy.franz@gmail.com>
2019-12-01 17:58:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var newArray = [];
+  newArray.length = musicians.length;
+  for (var i = 0; i < newArray.length; i++) {
+    newArray[i] = `${musicians[i]} plays ${instruments[i]}`;
+  }
+  return newArray;
+}
+
+function johnLennonFacts(arrayFacts){
+  var newArray = [];
+  var i = 0;
+  newArray.length = arrayFacts.length;
+  while (i < newArray.length){
+    newArray[i] = arrayFacts[i] + '!!!';
+    i++;
+  }
+  return newArray;
+}
+
+function iLoveTheBeatles(num){
+  var newArray = [];
+  var i;
+  if (num < 15) {
+    i = 15 - num - 1;
+  }
+  else {
+    i = 0;
+  }
+  newArray.length = i;
+  do {
+    newArray[i] = 'I love the Beatles!';
+    i--;
+  }
+  while (i >= 0); 
+  return newArray;
+}
