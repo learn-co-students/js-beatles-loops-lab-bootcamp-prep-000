72bde59e2f9f5c6ea5ab52c787de0f4ea992b80b
global-domain-9186 <jasonscottkutner@gmail.com>
2019-12-19 17:46:47 +0000

    Done.

--- index.js
+++ index.js
@@ -20,18 +20,6 @@ function johnLennonFacts(facts) {
 }
 
 
-// var i = 0;
-// do {
-//   i = i + 1;
-//   result = result + i;
-// } while (i < 5);
-
-// 1) Beatles Loops iLoveTheBeatles returns an array of 'I love the Beatles!' 8 times when passed the parameter 7 :
-// ReferenceError: iLoveTheBeatles is not defined
-
-// 2) Beatles Loops iLoveTheBeatles returns an array of 'I love the Beatles!' once when passed the parameter 17: MEANING IT FAILS
-// ReferenceError: iLoveTheBeatles is not defined
-
 function iLoveTheBeatles(number) {
   var i = 0
   var newArray = []
