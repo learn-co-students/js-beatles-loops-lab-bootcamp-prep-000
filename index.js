fdb0b0d53f4f052873f03e4cabedb9bbb72ab2d2
Avinash Nepal <nepalavinash@gmail.com>
2019-12-28 17:21:42 +0000

    Done.

--- index.js
+++ index.js
@@ -6,3 +6,25 @@ for(var i = 0; i < musicians.length; i++) {
 }
 return array;
 }
+
+
+  function johnLennonFacts(facts) {
+  let counter = 0;
+  var johnLennonFacts = []
+
+  while(counter < facts.length) {
+  johnLennonFacts.push (`${facts[counter]}!!!`)
+		counter += 1;
+    }
+return johnLennonFacts;
+}
+
+function iLoveTheBeatles(number) {
+var array = []
+do (var i = 0; i < 15; i++) {
+  array.push(`I love the Beatles!`)
+}
+
+return array;
+
+}
