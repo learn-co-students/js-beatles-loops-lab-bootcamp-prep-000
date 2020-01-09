c446d70f9ddebd5c3ba69fac9a4d1f7e9d5ce23b
Von C <shervonc@gmail.com>
2020-01-09 20:21:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) { // add solution here
+var message = []
+for (var i = 0; i< musicians.length; i++) 
+{message.push(musicians[i] + " plays " + instruments[i])
+}
+return message
+}
+
+
+function johnLennonFacts(facts) {
+  var endresult = [];
+  var i = 0;
+  while (i < facts.length) {
+    endresult.push(facts[i] + "!!!");i++
+  }
+  return endresult;
+}
+
+function iLoveTheBeatles(num) {
+  var message = [];
+  
+  do {message.push("I love the Beatles!"); num++
+  }
+  while (num < 15)
+  return message
+}
