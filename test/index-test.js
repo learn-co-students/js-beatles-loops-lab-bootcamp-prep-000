1d7a39b7199232b5864c748b8bf530810df67b1a
Jonathan T Dean <jonathantomdean@gmail.com>
2020-01-11 06:53:32 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -14,21 +14,6 @@ describe('Beatles Loops', function() {
         "He was a choir boy and boy scout",
         "He hated the sound of his own voice"
       ])).toEqual(["He was the last Beatle to learn to drive!!!", "He was never a vegetarian!!!", "He was a choir boy and boy scout!!!", "He hated the sound of his own voice!!!"]);
-
-      expect(johnLennonFacts([
-        "foo",
-        "bar",
-      ])).toEqual(["foo!!!", "bar!!!"])
-    });
-  });
-
-  describe('iLoveTheBeatles', function() {
-    it("returns an array of 'I love the Beatles!' 8 times when passed the parameter 7 ", function() {
-      expect(iLoveTheBeatles(7)).toEqual(["I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!"]);
-    });
-
-    it("returns an array of 'I love the Beatles!' once when passed the parameter 17", function() {
-      expect(iLoveTheBeatles(17)).toEqual(["I love the Beatles!"]);
     });
   });
 
