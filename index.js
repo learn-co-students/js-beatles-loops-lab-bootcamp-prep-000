0ad7aa37f0105db605d903dcd3b98e1be96d8cf8
Andrew Dalmedo <andrewdalmedo@gmail.com>
2020-02-04 06:02:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  // creating an empty variable
+  var statements = [];
+  // for loop (iterating through musicians)
+  for(var i = 0; i < musicians.length; i++) {
+    statements.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+
+  return statements;
+
+}
+
+function johnLennonFacts(facts) {
+  var num = 0;
+  var formattedFacts = facts;
+  while (num < facts.length) {
+    formattedFacts[num] = facts[num].concat("!!!");
+    num++;
+  }
+
+  return formattedFacts;
+
+}
+
+function iLoveTheBeatles(num) {
+  var love = [];
+
+  var i = 0;
+  do {
+    love.push("I love the Beatles!");
+    num++;
+  } while (num < 15);
+
+  console.log(love.length)
+
+  return love;
+}
+
+iLoveTheBeatles(17);
