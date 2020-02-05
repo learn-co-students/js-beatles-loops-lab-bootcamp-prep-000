1a9536a8fd4e5ce2fc65a03308bf7ca47fb945e4
James Jensen <jnjensen@live.com>
2020-02-05 03:37:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var play = []
+  for (var i = 0; i < musicians.length; i++){
+    play.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return play
+}
+
+function johnLennonFacts(fact){
+  var i = 0
+  var factsList = []
+  while (i < fact.length){
+    factsList.push(`${fact[i]}!!!`)
+    i++
+  }
+  return factsList
+}
+
+function iLoveTheBeatles(num){
+  var list = []
+  
+  function incrementVariable(){
+    num++
+    return num
+  }
+  
+  do{
+    list.push(`I love the Beatles!`)
+  }while(incrementVariable() < 15)
+  return list
+}
