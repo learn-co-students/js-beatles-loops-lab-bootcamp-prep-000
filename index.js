2071b7ce8328da7831cc7f61a13728e8865298d9
Devin Benson <devinbenson@gmail.com>
2020-03-01 21:39:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
+/* 
+function should return array of new paired strings 
+accepts 2 parameters, an array of musicians and an array of instruments 
+2 arrays, musicians and instruments
+
+
+musicians[0] + 'plays the ' instruments[0] + '.'
+
+function forLoop(array) {
+   for ( let i = 0; i < 25; i++) {
+     if (i === 1) {
+       array.push(`I am 1 strange loop.`)
+     } else {
+       array.push(`I am ${i} strange loops.`)
+     }
+  }
+  return array;
+
+*/ 
+var musicians = [];
+var instruments = [];
+var music = []; // the function should add / creat this 
+
+ function theBeatlesPlay(musicians, instruments) {
+  for ( let i = 0; musicians.length < 5; i++) {
+    var string = musicians[i] + " plays " + instruments[i] + ".";
+    music.push(string);}
+    return music;
+}
+
+
+
+
+
+/* 
+
+*/ 
+function johnLennonFacts() {
+  
+}
+
+
+
+
+
+
+/* 
+
+*/ 
+function iLoveTheBeatles() {
+  
+  
+}
+
+
+
+
+
+
+
 // add solution here
