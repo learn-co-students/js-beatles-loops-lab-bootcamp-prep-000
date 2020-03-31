dfe660e7eb6b2d26ad6f02f9aa8b1d2e2a38f435
alpha-acrobat-9971 <qsmity@gmail.com>
2020-03-31 08:35:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// add solution here
+var music = []
+var musicians = ["justin", "carl", "chester"]
+var instruments = ["guitar", "flute", "piano"]
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+]
+
+
+
+var theBeatlesPlay = function(musicians, instruments){
+  for (let i = 0; i < musicians.length; i++){
+      music.push(musicians[i] + " plays " + instruments[i])
+  }
+    return music
+}
+
+
+
+var johnLennonFacts = function(facts){
+  let i = 0
+  let array = []
+  while(i < facts.length){
+    array.push(`${facts[i]}!!!`)
+    i++
+  }
+  return array
+}
+
+
+var iLoveTheBeatles = function(num){
+  const love = []
+  do {
+    love.push("I love the Beatles!")
+    num++
+  } while(num < 15)
+  return love
+}
+
+
+// console.log(theBeatlesPlay(musicians, instruments))
+// console.log(johnLennonFacts(facts))
+console.log(iLoveTheBeatles(3))
