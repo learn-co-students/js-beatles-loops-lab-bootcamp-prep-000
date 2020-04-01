c13e54bcb59928e49178111f2f8e6ee7794b4ebb
zaid85j <zaid85j@gmail.com>
2020-04-01 12:10:22 +0000

    Done.

--- index.js
+++ index.js
@@ -2,16 +2,20 @@
 var theBeatlesPlayArr=[];
 var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
     var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-function theBeatlesPlay(musicians,instruments){
-for (var z=0;z<=4;z++){
-  for(var i=0;i<=musicians.length;i++){
-    
-  }
-  for(var x=0;x<=instruments.length;x++){
-    
-  }
-theBeatlesPlayArr.push(`${musicians[i]} plays${instruments[x]}`)
-}
-  return theBeatlesPlayArr
 
+
+
+  for(var i=0;i<musicians.length;i++){
+    console.log(musicians[i])
+    var musician=musicians[i]
+console.log(instruments[i])
+var instrument=instruments[i]
+ theBeatlesPlay(musician,instrument)
+
+  }
+
+function theBeatlesPlay(musician,instrument){
+ 
+theBeatlesPlayArr.push(`${musician} plays ${instrument}`)
+    console.log(theBeatlesPlayArr)
 }
