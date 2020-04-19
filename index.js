693561a1b3b38f517f776d7013f3ef90b1c02065
casey bradford <casey@flatironschool.com>
2020-04-19 22:03:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// add solution here
+var arrayNew = []
+
+function theBeatlesPlay(arrayMusicians, arrayInstruments){
+  for (let i = 0; i < 4; i++){
+  arrayNew.push(`${arrayMusicians[i]} plays ${arrayInstruments[i]}`);
+  }
+  return arrayNew
+}
+
+function johnLennonFacts(array) {
+  var n = 0
+  var arrayNew2 =[]
+  while (n < array.length){
+  arrayNew2.push(`${array[n]}!!!`)
+  n++;
+}
+return arrayNew2
+}
+
+function iLoveTheBeatles(g){
+  let beatlesArray = []
+  var count = g
+  do {
+    beatlesArray.push("I love the Beatles!")
+    count++
+  }
+  while (count < 15);
+  return beatlesArray
+}
