cde8e3ba9aada1b086ef690df5ea7b0bc66cd1a0
Erin Shields <elshields92@gmail.com>
2020-04-19 02:28:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var n = 0; n < musicians.length; n++) {
+    array.push(`${musicians[n]} plays ${instruments[n]}`);
+  }
+  return array;
+}
+
+function johnLennonFacts(array) {
+  let n = 0;
+  var newArray = [];
+  while (n < array.length) {
+    newArray.push(`${array[n]}!!!`);
+    n += 1
+  }
+  return newArray;
+}
+
+function iLoveTheBeatles(n) {
+  var array = [];
+  do {
+  array.push("I love the Beatles!")
+  n += 1;
+} while (n < 15);
+  return array
+}
