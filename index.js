4a7ddbf07cbf8ea39cc4e1efb7ac1a0a1b64adcd
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-05-18 14:23:28 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+    var beatlesLineUp = new Array();
+    for(let i = 0; i < musicians.length; i++){
+        beatlesLineUp[i] = `${musicians[i]} plays ${instruments[i]}`
+    }
+    return beatlesLineUp;
+}
+
+function johnLennonFacts(facts){
+    var bigFacts = new Array();
+    let i = 0;
+    while(i < facts.length){
+        bigFacts[i] = `${facts[i]}!!!`
+        i++;
+    }
+    return bigFacts;
+}
+
+function iLoveTheBeatles(num){
+    var message = new Array();
+    do{
+        message.push('I love the Beatles!')
+        num++;
+    }while(num < 15)
+    return message;
+}
