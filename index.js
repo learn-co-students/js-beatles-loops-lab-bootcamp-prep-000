2d86459a691432a8c87330afc5e2566169826ddd
David MacPhail <david.macphail@icloud.com>
2020-05-21 21:48:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // add solution here
+
+function theBeatlesPlay (musicians, instruments){
+  const blank = []
+
+  for (let i = 0, l = musicians.length; i < l; i++){
+    blank.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return blank
+}
+
+function johnLennonFacts(facts){
+  const shoutedFacts = []
+  let i = 0
+  while (i < facts.length) {
+    shoutedFacts.push(`${facts[i]}!!!`)
+  }
+  return shoutedFacts
+}
+
+function iLoveTheBeatles(n) {
+  const blank = []
+  do {
+    blank.push('I love the Beatles!')
+    n++
+  } while (n < 15)
+  return blank
+}
