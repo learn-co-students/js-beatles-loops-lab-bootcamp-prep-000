1097db7c5734eef77ebe13af04203b2cf3d72d89
Mackenzie Bruns <mackenziebruns503@gmail.com>
2020-05-29 18:54:55 +0000

    Done.

--- index.js
+++ index.js
@@ -18,21 +18,51 @@ function theBeatlesPlay(musicians, instruments) {
  return whoPlaysWhat
 }
 
+
 var facts= [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"];
-var excitedFacts= []
+  "He hated the sound of his own voice",
+  "While on tour, he enjoyed playing Monopoly",
+  "Harmonica was the first instrument Lennon learned to play"];
+
 function johnLennonFacts(facts){
-  while (i<4) {
-  i++
-  excitedFacts.push(`${facts[i]}!!!`);
-  
+ let i=0;
+ var excitedFacts=[];
+ while (i<facts.length) {
+excitedFacts.push(`${facts[i]}!!!`);
+  i+=1
   }
    return excitedFacts
 }
-var empty=[]
+
+function doWhileLoop(num){
+  do { console.log("I run once regardless.")}
+  while (incrementVariable()<= num);
+}
+
+let i = 0;
+ function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
 function iLoveTheBeatles(num){
-  empty.push("I love the Beatles!") num++ num<15
+var empty=[]
+ do { empty.push("I love the Beatles!")}
+ while(incrementVariable()<15);
+ return empty
+}
+
+function iLoveTheBeatles(num){
+  var empty=[];
+  function incrementVariable() {
+    num = num + 1;
+    return num;
+  }
+ do { 
+   empty.push("I love the Beatles!")
+  } while(incrementVariable()<15);
+ return empty
 }
