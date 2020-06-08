b5de60de86e658bd6dd9e392abd51d6471c7bb10
thoughtful-bundle-9618 <evansst@icloud.com>
2020-06-08 07:28:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments) {
+  var array = [ ]
+ for (let i = 0; i < musicians.length; i += 1 ){
+    var string = musicians[i] + " plays " + instruments[i]
+    array.push(string)
+ }
+  return array
+ 
+}
+
+function johnLennonFacts(facts) {
+  let i = 0
+  while (i < facts.length) {
+    facts[i] = facts[i] + "!!!"
+    i += 1;
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(n) {
+  var array = [ ]
+  do {
+    array.push("I love the Beatles!")
+    n += 1;
+  } while (n < 15)
+  return array;
+}
