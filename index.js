5d230222c80bbac9102027a574ad4dfb8b4241e5
Samantha Lurio <samantha.lurio@gmail.com>
2020-06-13 03:51:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// add solution here
+function theBeatlesPlay(musicans, instruments) {
+  var array = [];
+  for(let i=0; i < musicans.length; i += 1) {
+    array.push(`${musicans[i]} plays ${instruments[i]}`);
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  let i = 0;
+  var newFacts = [];
+  while(i < facts.length) {
+    newFacts.push(`${facts[i]}!!!`);
+    i += 1;
+  }
+  return newFacts;
+}
+
+function iLoveTheBeatles(num) {
+  var array = [];
+  function incrementNum() {
+    return num +=1;
+  }
+  do {
+    array.push("I love the Beatles!")
+  } while(incrementNum() < 15);
+  return array;
+}
