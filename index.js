d84048eb7a44accdd8187d4f57a6665dee55673d
MuathAlawi Alawi <muathalawi1@gmail.com>
2020-07-05 23:55:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // add solution here
+
+
+function theBeatlesPlay(musician, instrument) {
+ var art = [];
+ for (let i = 0; i < musician.length; i++){
+    
+     art.push(`${musician[i]} plays ${instrument[i]}`);
+     
+  }
+  return art;
+}
+
+function johnLennonFacts(facts) {
+  var array = [];
+  var i = 0;
+  while(array.length < facts.length) {
+    array.push(facts[i] + "!!!");
+    i++;
+  }
+  
+  return array;
+}
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!");
+    number += 1;
+    
+  } while(number < 15)
+  return array;
+}
