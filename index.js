39d81e3809f6625d76fd374325f136a64bae7592
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-11 20:57:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+var empty = []
+
+function theBeatlesPlay(musicians, instruments) {
+  for (let i = 0; i < musicians.length; i++) {
+    empty.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return empty
+}
+
+
+function johnLennonFacts(array) {
+  let i = 0
+  var newArray = []
+  while (i < array.length) {
+    newArray.push(`${array[i]}!!!`)
+    i += 1
+  }
+  return newArray
+}
+
+function iLoveTheBeatles(number) {
+  var empty = []
+  do {
+    empty.push("I love the Beatles!")
+    ++number
+  } while (number < 15)
+  return empty
+}
