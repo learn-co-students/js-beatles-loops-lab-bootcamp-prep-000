2d913bab0be49c8a4427fdb6e103cc22fb187099
Clayton Pierce <bamer78@gmail.com>
2020-08-21 19:59:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  let band = [];
+  for (let i = 0; i < musicians.length; i++){
+    band.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return band;
+}
+function johnLennonFacts(facts){
+  var returnArray = [];
+  var f = []
+  while (facts.length > 0){
+    f = facts.pop();
+    returnArray.unshift(`${f}!!!`);
+  }
+  return returnArray;
+}
+function iLoveTheBeatles(n){
+  var returnArray = []
+  do{
+    if (n > 15){
+      returnArray.push(`I love the Beatles!`)
+    }else{
+      n = n + 1
+      returnArray.push(`I love the Beatles!`)
+    }
+  }while(n < 15)
+  return returnArray
+}
