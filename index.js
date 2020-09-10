7f117b7a1f23192aa469b2c073d90adf2f65052f
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-09-10 18:45:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments){
+  const array=[];
+  for(let i=0; i<musicians.length; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array;
+}
+
+function johnLennonFacts(facts){
+  let i = 0;
+  const newArray=[];
+  while(i<facts.length){
+    newArray.push(`${facts[i]}` + "!!!");
+    i++;
+  }
+  return newArray;
+}
+
+
+function iLoveTheBeatles(number){
+  const array=[];
+function incrementVariable(){
+  
+}do{
+  array.push("I love the Beatles!");
+  number++
+}while(number<15);
+  
+  return array;
+}
+
+
