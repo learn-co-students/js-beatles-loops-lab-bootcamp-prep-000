0790af42982a809c96a92db2e1ad7461b54f4a65
DariasKehl <neocount@gmail.com>
2020-10-06 15:11:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+    var returnArray = [];
+    if(musicians.length === instruments.length) {
+        console.log("Input array size mismatch.");
+    }
+    for(let i = 0; i < musicians.length; i++) { 
+        returnArray[i] = musicians[i] + ' plays ' + instruments[i];
+        console.log(returnArray[i]);
+    }
+    return returnArray;
+}
+
+function johnLennonFacts(factsArray) {
+    let index = 0;
+    while(index < factsArray.length) {
+        factsArray[index] = factsArray[index] + "!!!";
+        index++;
+    }
+    return factsArray;
+}
+
+function iLoveTheBeatles(inputNumber) {
+    var whiteAlbum = [];
+    do {
+        whiteAlbum.push("I love the Beatles!");
+        inputNumber++;
+
+    } while(inputNumber < 15);
+    return whiteAlbum;
+}
