fca4d5a06841fc4340a08fe999a2d2097513d816
Rachid Abu-Hassan <rachid@platform78.com>
2020-10-24 14:10:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // add solution here
+// theBeatlesPlay
+function theBeatlesPlay(musicians, instruments) {
+  var phrases = []
+  for (let i = 0; i < musicians.length; i++) {
+    phrases.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return phrases
+}
+
+
+// johnLennonFacts
+function johnLennonFacts(facts) {
+  var phrases = []
+  for (let i = 0; i < facts.length; i++) {
+    phrases.push(`${facts[i]}!!!`)
+  }
+  return phrases
+
+}
+
+
+// iLoveTheBeatles
+function iLoveTheBeatles(number) {
+  function increment() {
+    number += 1
+    return number
+  }
+  var phrases = []
+  do {
+    phrases.push('I love the Beatles!')
+  } while (increment() < 15)
+return phrases
+
+}
