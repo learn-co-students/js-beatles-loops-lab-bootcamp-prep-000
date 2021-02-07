c459c5d8ffe831ea50a344268a70211ac353f0d8
Peter Crawford <pcraw1027@gmail.com>
2021-02-07 21:04:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 // add solution here
-var initArray = new musicianArray()
+var initArray = []
+var i;
 
-for () {
-  
-} 
-
-function johnLennonFacts(lennonArray) {
-  
+function theBeatlesPlay(musicians,instruments) {
+  for (i = 0; i < musicians.length; i++) {
+    initArray.push(musicians[i] + " plays " + instruments[i])
+  } 
+  return initArray
 }
 
-function iLoveTheBeatles () {
-  
+
+function johnLennonFacts(lennonArray) {
+  i = 0
+  while (lennonArray[i]) {
+    lennonArray[i] = lennonArray[i] + "!!!"
+    i++
+  }
+  return lennonArray
+}
+
+function iLoveTheBeatles(num) {
+  var emptyArray = []
+  do {
+    emptyArray.push("I love the Beatles!")
+    num++
+  } while (num < 15) 
+  return emptyArray
 }
